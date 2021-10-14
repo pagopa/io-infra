b70b791525a8582ece46cc2b0fe95ee795fc5ed1
Emanuele De Cupis <ema.decu@gmail.com>
2021-10-14 22:37:50 +0200

    reference fn3-eucovidcert

--- src/core/data.tf
+++ src/core/data.tf
@@ -14,3 +14,18 @@ data "azurerm_storage_account" "storage_apievents" {
   name                = replace(format("%s-stapievents", local.project), "-", "")
   resource_group_name = format("%s-rg-internal", local.project)
 }
+
+
+#
+# EUCovicCert resources
+#
+
+data "azurerm_function_app" "fnapp_eucovidcert" {
+  name                = format("%s-fn3-eucovidcert", local.project)
+  resource_group_name = format("%s-rg-eucovidcert", local.project)
+}
+
+data "azurerm_key_vault_secret" "fnapp_eucovidcert_authtoken" {
+  name         = "funceucovidcert-KEY-IOEVENTDISPATCHER"
+  key_vault_id = module.key_vault.id
+}
