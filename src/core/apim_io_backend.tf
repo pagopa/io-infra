d312852cc256517471b773cbdde9c92266b50476
Michael Disaro <michael.disaro@gmail.com>
2021-10-14 11:10:35 +0200

    wip

--- src/core/apim_io_backend.tf
+++ src/core/apim_io_backend.tf
@@ -19,26 +19,24 @@ module "apim_io_backend_product" {
   policy_xml = file("./api_product/io_backend/_base_policy.xml")
 }
 
-/* module "apim_io_backend_api" {
-  count  = var.io_backend_enabled ? 1 : 0
+module "apim_io_backend_api_v1" {
   source = "git::https://github.com/pagopa/azurerm.git//api_management_api?ref=v1.0.16"
 
   name                  = format("%s-io-backend-api", var.env_short)
-  api_management_name   = module.apim.name
-  resource_group_name   = azurerm_resource_group.rg_api.name
+  api_management_name   = data.azurerm_api_management.apim.name
+  resource_group_name   = data.azurerm_api_management.apim.resource_group_name
   product_ids           = [module.apim_io_backend_product[0].product_id]
   subscription_required = false
 
-  description  = "mock ec api"
-  display_name = "mock ec api"
-  path         = "io-backend/api"
+  description  = "BACKEND API for IO app"
+  display_name = "IO BACKEND API"
+  path         = "app"
   protocols    = ["https"]
 
   service_url = format("https://%s/mockEcService", module.io_backend[0].default_site_hostname)
 
-  content_value = templatefile("./api/mockec_api/v1/_swagger.json.tpl", {
-    host = azurerm_api_management_custom_domain.api_custom_domain.proxy[0].host_name
-  })
+  content_format = "openapi-link"
+  content_value = "https://github.com/pagopa/io-backend/blob/v7.23.0-RELEASE/api_backend.yaml"
 
   xml_content = file("./api/mockec_api/v1/_base_policy.xml")
-} */
+}
