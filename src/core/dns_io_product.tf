d2b4938319c79d18b629e3662f5d0dd0929bf332
pasqualedevita <>
2021-10-14 10:25:44 +0200

    add dns_caa_record

--- src/core/dns_io_product.tf
+++ src/core/dns_io_product.tf
@@ -5,3 +5,24 @@ resource "azurerm_dns_zone" "io_public" {
 
   tags = var.tags
 }
+
+resource "azurerm_dns_caa_record" "example" {
+  name                = "@"
+  zone_name           = azurerm_dns_zone.io_public[0].name
+  resource_group_name = azurerm_resource_group.rg_vnet.name
+  ttl                 = var.dns_default_ttl_sec
+
+  record {
+    flags = 0
+    tag   = "issue"
+    value = "letsencrypt.org"
+  }
+
+  record {
+    flags = 0
+    tag   = "iodef"
+    value = "mailto:security+caa@pagopa.it"
+  }
+
+  tags = var.tags
+}
