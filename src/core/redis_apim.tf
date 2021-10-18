3d970c6bb4f5fbe788ce02f8b8666ff855d24c35
Michael Disaro <michael.disaro@gmail.com>
2021-10-18 15:48:39 +0200

    try fix

--- src/core/redis_apim.tf
+++ src/core/redis_apim.tf
@@ -1,6 +1,7 @@
 ## Database subnet
 module "redis_snet" {
   source               = "git::https://github.com/pagopa/azurerm.git//subnet?ref=v1.0.51"
+  count                = var.redis_sku_name == "Premium" && length(var.cidr_subnet_redis) > 0 ? 1 : 0
   name                 = format("%s-redis-apim-snet", local.project)
   address_prefixes     = var.cidr_subnet_redis
   resource_group_name  = data.azurerm_resource_group.vnet_common_rg.name
@@ -17,7 +18,6 @@ module "redis" {
   sku_name              = var.redis_sku_name
   enable_non_ssl_port   = false
   enable_authentication = true
-  subnet_id             = length(module.redis_snet) == 0 ? null : module.redis_snet[0].id
-
-  tags = var.tags
+  subnet_id             = length(module.redis_snet.*.id) == 0 ? null : module.redis_snet[0].id
+  tags                  = var.tags
 }
