8d5fe1e9232c904959b7575d695b5a6924e0bf9d
Michael Disaro <michael.disaro@gmail.com>
2021-10-19 16:59:10 +0200

    added api v1 backend

--- src/core/apim_io_backend.tf
+++ src/core/apim_io_backend.tf
@@ -19,24 +19,35 @@ module "apim_io_backend_product" {
   policy_xml = file("./api_product/io_backend/_base_policy.xml")
 }
 
+resource "azurerm_api_management_api_version_set" "io_backend_api" {
+  name                = format("%s-io-backend-api", var.env_short)
+  resource_group_name = module.apim.resource_group_name
+  api_management_name = module.apim.name
+  display_name        = "IO BACKEND API"
+  versioning_scheme   = "Segment"
+}
+
 module "apim_io_backend_api_v1" {
   source = "git::https://github.com/pagopa/azurerm.git//api_management_api?ref=v1.0.16"
 
   name                  = format("%s-io-backend-api", var.env_short)
-  api_management_name   = data.azurerm_api_management.apim.name
-  resource_group_name   = data.azurerm_api_management.apim.resource_group_name
+  api_management_name   = module.apim.name
+  resource_group_name   = module.apim.resource_group_name
   product_ids           = [module.apim_io_backend_product[0].product_id]
   subscription_required = false
+  version_set_id        = azurerm_api_management_api_version_set.io_backend_api.id
+  api_version           = "v1"
+  service_url           = null
 
   description  = "BACKEND API for IO app"
   display_name = "IO BACKEND API"
-  path         = "app"
+  path         = "api/v1"
   protocols    = ["https"]
 
-  service_url = format("https://%s/mockEcService", module.io_backend[0].default_site_hostname)
+  content_format = "openapi"
+  content_value = templatefile("./api/io_backend/v1/_openapi.yaml.tpl", {
+    host = local.apim_hostname_api_app_internal
+  })
 
-  content_format = "openapi-link"
-  content_value = "https://github.com/pagopa/io-backend/blob/v7.23.0-RELEASE/api_backend.yaml"
-
-  xml_content = file("./api/mockec_api/v1/_base_policy.xml")
+  xml_content = file("./api/io_backend/v1/_base_policy.xml")
 }
