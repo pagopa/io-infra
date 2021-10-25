c576e6997856d4ab4dd2e5ce2a2f5601e546ac12
pasqualedevita <>
2021-10-25 22:30:52 +0200

    add uai_kvreader kv policy

--- src/core/appgateway.tf
+++ src/core/appgateway.tf
@@ -239,7 +239,22 @@ resource "azurerm_key_vault_access_policy" "app_gateway_policy" {
   key_vault_id            = module.key_vault.id
   tenant_id               = data.azurerm_client_config.current.tenant_id
   object_id               = azurerm_user_assigned_identity.appgateway.principal_id
-  key_permissions         = ["Get", "List"]
+  key_permissions         = []
+  secret_permissions      = ["Get", "List"]
+  certificate_permissions = ["Get", "List"]
+  storage_permissions     = []
+}
+
+## user assined identity: (old application gateway) ##
+data "azuread_service_principal" "app_gw_uai_kvreader" {
+  display_name = format("%s-uai-kvreader", local.project)
+}
+
+resource "azurerm_key_vault_access_policy" "app_gw_uai_kvreader_common" {
+  key_vault_id            = data.azurerm_key_vault.common.id
+  tenant_id               = data.azurerm_client_config.current.tenant_id
+  object_id               = data.azuread_service_principal.app_gw_uai_kvreader.object_id
+  key_permissions         = []
   secret_permissions      = ["Get", "List"]
   certificate_permissions = ["Get", "List"]
   storage_permissions     = []
