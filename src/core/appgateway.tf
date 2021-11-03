2bd75771b170599a178558deea76ae85fd68ff8f
pasqualedevita <>
2021-11-03 15:18:17 +0100

    fix required service_endpoints

--- src/core/appgateway.tf
+++ src/core/appgateway.tf
@@ -16,6 +16,10 @@ module "appgateway_snet" {
   address_prefixes     = var.cidr_subnet_appgateway
   resource_group_name  = data.azurerm_resource_group.vnet_common_rg.name
   virtual_network_name = data.azurerm_virtual_network.vnet_common.name
+
+  service_endpoints = [
+    "Microsoft.Web",
+  ]
 }
 
 ## Application gateway ##
