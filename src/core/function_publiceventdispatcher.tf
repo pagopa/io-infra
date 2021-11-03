3befd90337f040bc3e0d2849e0b34349eeda273b
Emanuele De Cupis <ema.decu@gmail.com>
2021-11-03 10:54:52 +0100

    fix covicert webhook

--- src/core/function_publiceventdispatcher.tf
+++ src/core/function_publiceventdispatcher.tf
@@ -73,14 +73,14 @@ module "function_pblevtdispatcher" {
     webhooks = jsonencode([
       # EUCovidCert PROD
       {
-        url           = format("https://%s/api/v1/webhook", data.azurerm_function_app.fnapp_eucovidcert.default_hostname),
+        url           = format("https://%s/api/v1/io-events-webhook", data.azurerm_function_app.fnapp_eucovidcert.default_hostname),
         headers       = { "X-Functions-Key" = data.azurerm_key_vault_secret.fnapp_eucovidcert_authtoken.value },
         attributes    = { serviceId = "01F73DNTMJTCEZQKJDFNB53KEB" },
         subscriptions = ["service:subscribed"]
       },
       # EUCovidCert UAT
       {
-        url           = format("https://%s/api/v1/webhook", data.azurerm_function_app.fnapp_eucovidcert.default_hostname),
+        url           = format("https://%s/api/v1/io-events-webhook", data.azurerm_function_app.fnapp_eucovidcert.default_hostname),
         headers       = { "X-Functions-Key" = data.azurerm_key_vault_secret.fnapp_eucovidcert_authtoken.value },
         attributes    = { serviceId = "01F798T3NX5RARB38DVKPABKV2" },
         subscriptions = ["service:subscribed"]
