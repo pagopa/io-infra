f8df083e6913a1a46d4c0449ccf06b13e940d9f1
pasqualedevita <>
2021-11-08 12:59:45 +0100

    no change for azurerm_dns_caa_record.io_italia_it TTL

--- src/core/dns_io_italia_it.tf
+++ src/core/dns_io_italia_it.tf
@@ -9,7 +9,7 @@ resource "azurerm_dns_caa_record" "io_italia_it" {
   name                = "@"
   zone_name           = azurerm_dns_zone.io_italia_it.name
   resource_group_name = azurerm_resource_group.rg_external.name
-  ttl                 = var.dns_default_ttl_sec
+  ttl                 = 300
 
   record {
     flags = 0
