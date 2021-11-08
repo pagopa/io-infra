3fccec3f69271b03085c86149753aa171a5a6f32
pp-ps <63303456+pp-ps@users.noreply.github.com>
2021-11-08 13:10:16 +0100

    Use dns_default_ttl_sec for CAA record

--- src/core/dns_io_italia_it.tf
+++ src/core/dns_io_italia_it.tf
@@ -9,7 +9,7 @@ resource "azurerm_dns_caa_record" "io_italia_it" {
   name                = "@"
   zone_name           = azurerm_dns_zone.io_italia_it.name
   resource_group_name = azurerm_resource_group.rg_external.name
-  ttl                 = 300
+  ttl                 = var.dns_default_ttl_sec
 
   record {
     flags = 0
