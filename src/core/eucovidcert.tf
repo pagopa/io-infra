2b28d6cf802d4d769bd7abc01be9dc4771aa032e
pasqualedevita <>
2021-12-02 08:40:05 +0100

    remove todo

--- src/core/eucovidcert.tf
+++ src/core/eucovidcert.tf
@@ -7,16 +7,14 @@ resource "azurerm_monitor_scheduled_query_rules_alert" "eucovidcert_availability
     action_group = [azurerm_monitor_action_group.email.id, azurerm_monitor_action_group.slack.id]
   }
 
-  data_source_id = data.azurerm_log_analytics_workspace.monitor_rg.id
-  # todo when will send logs to sec workspace
-  # data_source_id = var.env_short == "p" ? data.azurerm_key_vault_secret.sec_workspace_id[0].value : data.azurerm_log_analytics_workspace.monitor_rg.id
+  data_source_id          = var.env_short == "p" ? data.azurerm_key_vault_secret.sec_workspace_id[0].value : data.azurerm_log_analytics_workspace.monitor_rg.id
   description             = "Availability for Get Certificate is less than or equal to 99%, check ${module.app_gw.name}"
   enabled                 = var.eucovidcert_alerts_enabled
   auto_mitigation_enabled = true
 
   query = <<-QUERY
     AzureDiagnostics
-    | where Resource == "IO-P-AG-APPGATEWAY" or Resource == "${upper(module.app_gw.name)}"
+    | where Resource == "${upper(module.app_gw.name)}"
     | where host_s == "app-backend.io.italia.it"
     | where requestUri_s == "/api/v1/eucovidcert/certificate"
     | summarize
