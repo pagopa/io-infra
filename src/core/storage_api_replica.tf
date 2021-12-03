0f5e71cef9507b581a60a970af0ee457539177cc
uolter <walter.traspadini@pagopa.it>
2021-12-03 16:38:32 +0100

    changed start replication date

--- src/core/storage_api_replica.tf
+++ src/core/storage_api_replica.tf
@@ -37,7 +37,7 @@ module "io_apist_replica" {
   rules = [{
     source_container_name      = "message-content"
     destination_container_name = "message-content"
-    copy_blobs_created_after   = "20021-04-01T00:00:00Z"
+    copy_blobs_created_after   = "2021-01-01T00:00:00Z"
   }]
 
 }
