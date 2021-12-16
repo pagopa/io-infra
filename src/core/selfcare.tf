6a006661b2f35d2e188bd2aebf62f3534983352b
Pasquale De Vita <59291437+pasqualedevita@users.noreply.github.com>
2021-12-16 19:07:27 +0100

    update cdn version

--- src/core/selfcare.tf
+++ src/core/selfcare.tf
@@ -17,7 +17,7 @@ resource "azurerm_resource_group" "selfcare_fe_rg" {
 
 ### Frontend resources
 module "selfcare_cdn" {
-  source = "git::https://github.com/pagopa/azurerm.git//cdn?ref=v2.0.2"
+  source = "git::https://github.com/pagopa/azurerm.git//cdn?ref=v2.0.13"
 
   name                  = "selfcare"
   prefix                = local.project
