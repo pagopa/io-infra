4d591dd4c7393ffc44ea7004746b3b3f753c2d5b
Walter Traspadini <walter.traspadini@pagopa.it>
2022-01-13 17:58:48 +0100

    Cosmos db Cgn (#78)
    * first draft cosmos cgn
    
    * cosmos db cgn

--- src/core/cgn.tf
+++ src/core/cgn.tf
@@ -55,3 +55,62 @@ module "redis_cgn" {
 
   tags = var.tags
 }
+
+data "azurerm_subnet" "fn3cgn" {
+  name                 = "fn3cgn"
+  virtual_network_name = data.azurerm_virtual_network.vnet_common.name
+  resource_group_name  = data.azurerm_resource_group.vnet_common_rg.name
+}
+
+##################
+## CosmosDB cgn ##
+##################
+
+module "cosmos_cgn" {
+  source   = "git::https://github.com/pagopa/azurerm.git//cosmosdb_account?ref=v2.1.1"
+  name     = format("%s-cosmos-cgn", local.project)
+  location = var.location
+
+  resource_group_name = data.azurerm_resource_group.cgn.name
+  offer_type          = "Standard"
+  kind                = "GlobalDocumentDB"
+
+  main_geo_location_zone_redundant = false
+
+  enable_free_tier          = false
+  enable_automatic_failover = true
+
+
+  consistency_policy = {
+    consistency_level       = "Strong"
+    max_interval_in_seconds = null
+    max_staleness_prefix    = null
+  }
+
+  main_geo_location_location = "westeurope"
+
+  additional_geo_locations = [
+    {
+      location          = "northeurope"
+      failover_priority = 1
+      zone_redundant    = true
+    }
+  ]
+
+  backup_continuous_enabled = true
+
+  is_virtual_network_filter_enabled = true
+
+  ip_range = ""
+
+  allowed_virtual_network_subnet_ids = [
+    data.azurerm_subnet.fn3cgn.id
+  ]
+
+  subnet_id            = null
+  private_dns_zone_ids = []
+
+  tags = var.tags
+
+}
+
