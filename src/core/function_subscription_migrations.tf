b45f2ec95530890d4db3bf7e0b478c350f93882c
Emanuele De Cupis <emanuele.decupis@pagopa.it>
2022-01-14 13:19:08 +0100

    Update src/core/function_subscription_migrations.tf
    Co-authored-by: Pasquale De Vita <59291437+pasqualedevita@users.noreply.github.com>

--- src/core/function_subscription_migrations.tf
+++ src/core/function_subscription_migrations.tf
@@ -43,9 +43,9 @@ module "function_subscriptionmigrations" {
   internal_storage = {
     "enable"                     = true,
     "private_endpoint_subnet_id" = data.azurerm_subnet.private_endpoints_subnet.id,
-    "private_dns_zone_blob_ids"  = [data.azurerm_dns_zone.privatelink_blob_core_windows_net.id],
-    "private_dns_zone_queue_ids" = [data.azurerm_dns_zone.privatelink_queue_core_windows_net.id],
-    "private_dns_zone_table_ids" = [data.azurerm_dns_zone.privatelink_table_core_windows_net.id],
+    "private_dns_zone_blob_ids"  = [data. azurerm_private_dns_zone.privatelink_blob_core_windows_net.id],
+    "private_dns_zone_queue_ids" = [data. azurerm_private_dns_zone.privatelink_queue_core_windows_net.id],
+    "private_dns_zone_table_ids" = [data. azurerm_private_dns_zone.privatelink_table_core_windows_net.id],
     "queues"                     = [],
     "containers"                 = [],
     "blobs_retention_days"       = 1,
