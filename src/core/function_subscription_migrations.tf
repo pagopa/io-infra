127269b590a13d91c12e90d095c816be28901098
Emanuele De Cupis <ema.decu@gmail.com>
2022-01-27 12:55:34 +0100

    update postgres moduel

--- src/core/function_subscription_migrations.tf
+++ src/core/function_subscription_migrations.tf
@@ -27,73 +27,73 @@ locals {
 
     metric_alerts = {
       db = {
-      cpu = {
-        aggregation = "Average"
-        metric_name = "cpu_percent"
-        operator    = "GreaterThan"
-        threshold   = 70
-        frequency   = "PT1M"
-        window_size = "PT5M"
-        dimension   = []
+        cpu = {
+          aggregation = "Average"
+          metric_name = "cpu_percent"
+          operator    = "GreaterThan"
+          threshold   = 70
+          frequency   = "PT1M"
+          window_size = "PT5M"
+          dimension   = []
+        }
+        memory = {
+          aggregation = "Average"
+          metric_name = "memory_percent"
+          operator    = "GreaterThan"
+          threshold   = 75
+          frequency   = "PT1M"
+          window_size = "PT5M"
+          dimension   = []
+        }
+        io = {
+          aggregation = "Average"
+          metric_name = "io_consumption_percent"
+          operator    = "GreaterThan"
+          threshold   = 55
+          frequency   = "PT1M"
+          window_size = "PT5M"
+          dimension   = []
+        }
+        # https://docs.microsoft.com/it-it/azure/postgresql/concepts-limits
+        # GP_Gen5_2 -| 145 / 100 * 80 = 116
+        # GP_Gen5_32 -| 1495 / 100 * 80 = 1196
+        max_active_connections = {
+          aggregation = "Average"
+          metric_name = "active_connections"
+          operator    = "GreaterThan"
+          threshold   = 1196
+          frequency   = "PT5M"
+          window_size = "PT5M"
+          dimension   = []
+        }
+        min_active_connections = {
+          aggregation = "Average"
+          metric_name = "active_connections"
+          operator    = "LessThanOrEqual"
+          threshold   = 0
+          frequency   = "PT5M"
+          window_size = "PT15M"
+          dimension   = []
+        }
+        failed_connections = {
+          aggregation = "Total"
+          metric_name = "connections_failed"
+          operator    = "GreaterThan"
+          threshold   = 10
+          frequency   = "PT5M"
+          window_size = "PT15M"
+          dimension   = []
+        }
+        replica_lag = {
+          aggregation = "Average"
+          metric_name = "pg_replica_log_delay_in_seconds"
+          operator    = "GreaterThan"
+          threshold   = 60
+          frequency   = "PT1M"
+          window_size = "PT5M"
+          dimension   = []
+        }
       }
-      memory = {
-        aggregation = "Average"
-        metric_name = "memory_percent"
-        operator    = "GreaterThan"
-        threshold   = 75
-        frequency   = "PT1M"
-        window_size = "PT5M"
-        dimension   = []
-      }
-      io = {
-        aggregation = "Average"
-        metric_name = "io_consumption_percent"
-        operator    = "GreaterThan"
-        threshold   = 55
-        frequency   = "PT1M"
-        window_size = "PT5M"
-        dimension   = []
-      }
-      # https://docs.microsoft.com/it-it/azure/postgresql/concepts-limits
-      # GP_Gen5_2 -| 145 / 100 * 80 = 116
-      # GP_Gen5_32 -| 1495 / 100 * 80 = 1196
-      max_active_connections = {
-        aggregation = "Average"
-        metric_name = "active_connections"
-        operator    = "GreaterThan"
-        threshold   = 1196
-        frequency   = "PT5M"
-        window_size = "PT5M"
-        dimension   = []
-      }
-      min_active_connections = {
-        aggregation = "Average"
-        metric_name = "active_connections"
-        operator    = "LessThanOrEqual"
-        threshold   = 0
-        frequency   = "PT5M"
-        window_size = "PT15M"
-        dimension   = []
-      }
-      failed_connections = {
-        aggregation = "Total"
-        metric_name = "connections_failed"
-        operator    = "GreaterThan"
-        threshold   = 10
-        frequency   = "PT5M"
-        window_size = "PT15M"
-        dimension   = []
-      }
-      replica_lag = {
-        aggregation = "Average"
-        metric_name = "pg_replica_log_delay_in_seconds"
-        operator    = "GreaterThan"
-        threshold   = 60
-        frequency   = "PT1M"
-        window_size = "PT5M"
-        dimension   = []
-      }
-    }
     }
   }
 }
@@ -185,7 +185,7 @@ data "azurerm_key_vault_secret" "subscriptionmigrations_db_server_adm_password" 
 
 
 module "subscriptionmigrations_db_server" {
-  source = "git::https://github.com/pagopa/azurerm.git//postgresql_server?ref=v2.0.5"
+  source = "git::https://github.com/pagopa/azurerm.git//postgresql_server?ref=v2.1.14"
 
   name                = format("%s-%s-db-postgresql", local.project, local.function_subscriptionmigrations.app_context.name)
   location            = var.location
@@ -194,8 +194,8 @@ module "subscriptionmigrations_db_server" {
   administrator_login          = data.azurerm_key_vault_secret.subscriptionmigrations_db_server_adm_username.value
   administrator_login_password = data.azurerm_key_vault_secret.subscriptionmigrations_db_server_adm_password.value
 
-  sku_name   = "GP_Gen5_2"
-  version    = 11
+  sku_name                     = "GP_Gen5_2"
+  version                      = 11
   geo_redundant_backup_enabled = false
 
   public_network_access_enabled = false
