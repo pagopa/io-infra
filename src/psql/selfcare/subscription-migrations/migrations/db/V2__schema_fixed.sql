ad794f3dd6eaa64e179a07d9599ed492aca0a4c7
Emanuele De Cupis <ema.decu@gmail.com>
2022-02-08 17:50:59 +0100

    use text

---
+++ src/psql/selfcare/subscription-migrations/migrations/db/V2__schema_fixed.sql
@@ -0,0 +1,8 @@
+ALTER TABLE "SelfcareIOSubscriptionMigrations".migrations
+    ALTER COLUMN "sourceName" TYPE text,
+    ALTER COLUMN "sourceSurname" TYPE text,
+    ALTER COLUMN "sourceEmail" TYPE text,
+    ALTER COLUMN status text TYPE
+    ALTER COLUMN note text TYPE ,
+    ALTER COLUMN "serviceName" TYPE text;
+)
