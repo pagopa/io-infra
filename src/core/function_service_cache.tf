7e2f6d30985a25b6406ca5a596e89346386dbc20
diegoitaliait <diego.lagosmorales@pagopa.it>
2022-03-03 18:04:26 +0100

    fix subnet endpoints

--- src/core/function_service_cache.tf
+++ src/core/function_service_cache.tf
@@ -1,31 +1,3 @@
-resource "azurerm_resource_group" "service_cache_rg" {
-  name     = "${local.project}-plan-be-rg"
-  location = var.location
-
-  tags = var.tags
-}
-
-module "fn_service_cache_subnet" {
-  source               = "git::https://github.com/pagopa/azurerm.git//subnet?ref=v2.3.0"
-  name                 = "${local.fn_service_cache_subnet_name}"
-  address_prefixes     = var.cidr_subnet_fnelt
-  resource_group_name  = data.azurerm_resource_group.vnet_common_rg.name
-  virtual_network_name = data.azurerm_virtual_network.vnet_common.name
-  service_endpoints = [
-    "Microsoft.EventHub",
-    "Microsoft.Storage",
-    "Microsoft.AzureCosmosDB",
-  ]
-
-  delegation = {
-    name = "default"
-    service_delegation = {
-      name    = "Microsoft.Web/serverFarms"
-      actions = ["Microsoft.Network/virtualNetworks/subnets/action"]
-    }
-  }
-}
-
 # # CDN Assets storage account
 # data "azurerm_storage_account" "cdnassets" {
 #   name                = "iopstcdnassets"
@@ -45,86 +17,124 @@ module "fn_service_cache_subnet" {
 # }
 # # KeyVault values - end
 
-# module "function_elt" {
-#   source = "git::https://github.com/pagopa/azurerm.git//function_app?ref=v1.0.65"
+resource "azurerm_resource_group" "fn_service_cache_rg" {
+  name     = "${local.project}-plan-be-rg"
+  location = var.location
 
-#   resource_group_name                      = azurerm_resource_group.service_cache_rg.name
-#   prefix                                   = var.prefix
-#   env_short                                = var.env_short
-#   name                                     = "elt"
-#   location                                 = var.location
-#   health_check_path                        = "api/v1/info"
-#   subnet_out_id                            = module.fn_service_cache_subnet.id
-#   runtime_version                          = "~3"
-#   application_insights_instrumentation_key = data.azurerm_application_insights.application_insights.instrumentation_key
+  tags = var.tags
+}
 
-#   app_settings = {
-#     FUNCTIONS_WORKER_RUNTIME       = "node"
-#     WEBSITE_NODE_DEFAULT_VERSION   = "14.16.0"
-#     FUNCTIONS_WORKER_PROCESS_COUNT = 4
-#     NODE_ENV                       = "production"
+module "fn_service_cache_subnet" {
+  source               = "git::https://github.com/pagopa/azurerm.git//subnet?ref=v2.3.0"
+  name                 = "${local.fn_service_cache_subnet_name}"
+  address_prefixes     = var.cidr_subnet_fnelt
+  resource_group_name  = data.azurerm_resource_group.vnet_common_rg.name
+  virtual_network_name = data.azurerm_virtual_network.vnet_common.name
+  service_endpoints = [
+    "Microsoft.Web",
+    "Microsoft.AzureCosmosDB",
+    "Microsoft.Storage",
+  ]
 
-#     // Keepalive fields are all optionals
-#     FETCH_KEEPALIVE_ENABLED             = "true"
-#     FETCH_KEEPALIVE_SOCKET_ACTIVE_TTL   = "110000"
-#     FETCH_KEEPALIVE_MAX_SOCKETS         = "40"
-#     FETCH_KEEPALIVE_MAX_FREE_SOCKETS    = "10"
-#     FETCH_KEEPALIVE_FREE_SOCKET_TIMEOUT = "30000"
-#     FETCH_KEEPALIVE_TIMEOUT             = "60000"
+  delegation = {
+    name = "default"
+    service_delegation = {
+      name    = "Microsoft.Web/serverFarms"
+      actions = ["Microsoft.Network/virtualNetworks/subnets/action"]
+    }
+  }
+}
 
-#     COSMOSDB_NAME                = "db"
-#     COSMOSDB_URI                 = data.azurerm_cosmosdb_account.cosmos_api.endpoint
-#     COSMOSDB_KEY                 = data.azurerm_cosmosdb_account.cosmos_api.primary_master_key
-#     COSMOS_API_CONNECTION_STRING = format("AccountEndpoint=%s;AccountKey=%s;", data.azurerm_cosmosdb_account.cosmos_api.endpoint, data.azurerm_cosmosdb_account.cosmos_api.primary_master_key)
+module "function_service_cache" {
+  source = "git::https://github.com/pagopa/azurerm.git//function_app?ref=v2.3.0"
 
-#     TARGETKAFKA_clientId            = "IO_FUNCTIONS_ELT"
-#     TARGETKAFKA_brokers             = local.event_hub.connection
-#     TARGETKAFKA_ssl                 = "true"
-#     TARGETKAFKA_sasl_mechanism      = "plain"
-#     TARGETKAFKA_sasl_username       = "$ConnectionString"
-#     TARGETKAFKA_sasl_password       = module.event_hub.keys["io-cosmosdb-services.io-fn-elt"].primary_connection_string
-#     TARGETKAFKA_maxInFlightRequests = "1"
-#     TARGETKAFKA_idempotent          = "true"
-#     TARGETKAFKA_transactionalId     = "IO_ELT"
-#     TARGETKAFKA_topic               = "io-cosmosdb-services"
-#     ERROR_STORAGE_ACCOUNT           = module.storage_account_elt.name
-#     ERROR_STORAGE_KEY               = module.storage_account_elt.primary_access_key
-#     ERROR_STORAGE_TABLE             = azurerm_storage_table.fnelterrors.name
-#     COMMAND_STORAGE                 = module.storage_account_elt.primary_connection_string
-#     COMMAND_STORAGE_TABLE           = azurerm_storage_table.fneltcommands.name
-#     IMPORT_TOPIC_NAME               = "import-command"
-#     IMPORT_TOPIC_CONNECTION_STRING  = module.event_hub.keys["import-command.io-fn-elt"].primary_connection_string
+  resource_group_name                      = azurerm_resource_group.fn_service_cache_rg.name
+  name                                     = "${local.project}-service-cache-fn"
+  location                                 = var.location
+  health_check_path                        = "api/v1/info"
+  subnet_id                                = module.fn_service_cache_subnet.id
+  application_insights_instrumentation_key = data.azurerm_application_insights.application_insights.instrumentation_key
 
-#     PROFILE_TOPIC_NAME              = "io-cosmosdb-profiles"
-#     PROFILE_TOPIC_CONNECTION_STRING = module.event_hub.keys["io-cosmosdb-profiles.io-fn-elt"].primary_connection_string
+  app_settings = {
+    FUNCTIONS_WORKER_RUNTIME       = "node"
+    WEBSITE_NODE_DEFAULT_VERSION   = "14.16.0"
+    WEBSITE_RUN_FROM_PACKAGE     = "1"
+    NODE_ENV                       = "production"
 
-#     COSMOSDB_REPLICA_NAME     = "db"
-#     COSMOSDB_REPLICA_URI      = replace(data.azurerm_cosmosdb_account.cosmos_api.endpoint, "io-p-cosmos-api", "io-p-cosmos-api-northeurope")
-#     COSMOSDB_REPLICA_KEY      = data.azurerm_cosmosdb_account.cosmos_api.primary_master_key
-#     COSMOSDB_REPLICA_LOCATION = "North Europe"
+    # COSMOSDB_URI                 = data.azurerm_cosmosdb_account.cosmos_api.endpoint
+    # COSMOSDB_KEY                 = data.azurerm_cosmosdb_account.cosmos_api.primary_master_key
+    # COSMOS_API_CONNECTION_STRING = format("AccountEndpoint=%s;AccountKey=%s;", data.azurerm_cosmosdb_account.cosmos_api.endpoint, data.azurerm_cosmosdb_account.cosmos_api.primary_master_key)
+    # COSMOSDB_NAME                = "db"
 
-#     MESSAGE_EXPORTS_COMMAND_TABLE       = azurerm_storage_table.fneltexports.name
-#     MESSAGE_EXPORT_STEP_1_CONTAINER     = azurerm_storage_container.container_messages_report_step1.name
-#     MESSAGE_EXPORT_STEP_FINAL_CONTAINER = azurerm_storage_container.container_messages_report_step_final.name
 
-#     COSMOS_CHUNK_SIZE            = "1000"
-#     COSMOS_DEGREE_OF_PARALLELISM = "2"
-#     MESSAGE_CONTENT_CHUNK_SIZE   = "200"
 
-#     SERVICEID_EXCLUSION_LIST = data.azurerm_key_vault_secret.services_exclusion_list.value
 
-#     MessageContentStorageConnection  = data.azurerm_storage_account.api_replica.primary_connection_string
-#     ServiceInfoBlobStorageConnection = data.azurerm_storage_account.cdnassets.primary_connection_string
-#   }
 
-#   allowed_subnets = [
-#     data.azurerm_subnet.azdoa_snet[0].id,
-#   ]
 
-#   allowed_ips = local.app_insights_ips_west_europe
 
-#   tags = var.tags
-# }
+
+
+
+
+    FUNCTIONS_WORKER_PROCESS_COUNT = 4
+
+    // Keepalive fields are all optionals
+    FETCH_KEEPALIVE_ENABLED             = "true"
+    FETCH_KEEPALIVE_SOCKET_ACTIVE_TTL   = "110000"
+    FETCH_KEEPALIVE_MAX_SOCKETS         = "40"
+    FETCH_KEEPALIVE_MAX_FREE_SOCKETS    = "10"
+    FETCH_KEEPALIVE_FREE_SOCKET_TIMEOUT = "30000"
+    FETCH_KEEPALIVE_TIMEOUT             = "60000"
+
+
+    TARGETKAFKA_clientId            = "IO_FUNCTIONS_ELT"
+    TARGETKAFKA_brokers             = local.event_hub.connection
+    TARGETKAFKA_ssl                 = "true"
+    TARGETKAFKA_sasl_mechanism      = "plain"
+    TARGETKAFKA_sasl_username       = "$ConnectionString"
+    TARGETKAFKA_sasl_password       = module.event_hub.keys["io-cosmosdb-services.io-fn-elt"].primary_connection_string
+    TARGETKAFKA_maxInFlightRequests = "1"
+    TARGETKAFKA_idempotent          = "true"
+    TARGETKAFKA_transactionalId     = "IO_ELT"
+    TARGETKAFKA_topic               = "io-cosmosdb-services"
+    ERROR_STORAGE_ACCOUNT           = module.storage_account_elt.name
+    ERROR_STORAGE_KEY               = module.storage_account_elt.primary_access_key
+    ERROR_STORAGE_TABLE             = azurerm_storage_table.fnelterrors.name
+    COMMAND_STORAGE                 = module.storage_account_elt.primary_connection_string
+    COMMAND_STORAGE_TABLE           = azurerm_storage_table.fneltcommands.name
+    IMPORT_TOPIC_NAME               = "import-command"
+    IMPORT_TOPIC_CONNECTION_STRING  = module.event_hub.keys["import-command.io-fn-elt"].primary_connection_string
+
+    PROFILE_TOPIC_NAME              = "io-cosmosdb-profiles"
+    PROFILE_TOPIC_CONNECTION_STRING = module.event_hub.keys["io-cosmosdb-profiles.io-fn-elt"].primary_connection_string
+
+    COSMOSDB_REPLICA_NAME     = "db"
+    COSMOSDB_REPLICA_URI      = replace(data.azurerm_cosmosdb_account.cosmos_api.endpoint, "io-p-cosmos-api", "io-p-cosmos-api-northeurope")
+    COSMOSDB_REPLICA_KEY      = data.azurerm_cosmosdb_account.cosmos_api.primary_master_key
+    COSMOSDB_REPLICA_LOCATION = "North Europe"
+
+    MESSAGE_EXPORTS_COMMAND_TABLE       = azurerm_storage_table.fneltexports.name
+    MESSAGE_EXPORT_STEP_1_CONTAINER     = azurerm_storage_container.container_messages_report_step1.name
+    MESSAGE_EXPORT_STEP_FINAL_CONTAINER = azurerm_storage_container.container_messages_report_step_final.name
+
+    COSMOS_CHUNK_SIZE            = "1000"
+    COSMOS_DEGREE_OF_PARALLELISM = "2"
+    MESSAGE_CONTENT_CHUNK_SIZE   = "200"
+
+    SERVICEID_EXCLUSION_LIST = data.azurerm_key_vault_secret.services_exclusion_list.value
+
+    MessageContentStorageConnection  = data.azurerm_storage_account.api_replica.primary_connection_string
+    ServiceInfoBlobStorageConnection = data.azurerm_storage_account.cdnassets.primary_connection_string
+  }
+
+  allowed_subnets = [
+    data.azurerm_subnet.azdoa_snet[0].id,
+  ]
+
+  allowed_ips = local.app_insights_ips_west_europe
+
+  tags = var.tags
+}
 
 # #tfsec:ignore:azure-storage-default-action-deny
 # module "storage_account_elt" {
@@ -135,7 +145,7 @@ module "fn_service_cache_subnet" {
 #   account_tier               = "Standard"
 #   account_replication_type   = "GRS"
 #   access_tier                = "Hot"
-#   resource_group_name        = azurerm_resource_group.service_cache_rg.name
+#   resource_group_name        = azurerm_resource_group.fn_service_cache_rg.name
 #   location                   = var.location
 #   advanced_threat_protection = true
 
