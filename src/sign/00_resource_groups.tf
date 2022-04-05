afe66bb75ddd31d68bb47fdc69b3725cd0a8d74f
pasqualedevita <>
2022-04-05 21:12:03 +0200

    fix readme, naming

---
+++ src/sign/00_resource_groups.tf
@@ -0,0 +1,13 @@
+resource "azurerm_resource_group" "data_rg" {
+  name     = "${local.project}-data-rg"
+  location = var.location
+
+  tags = var.tags
+}
+
+resource "azurerm_resource_group" "backend_rg" {
+  name     = "${local.project}-backend-rg"
+  location = var.location
+
+  tags = var.tags
+}
