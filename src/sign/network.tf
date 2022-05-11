4e16b116704cf40b228b12e5e7ab2e9517855743
Pasquale De Vita <59291437+pasqualedevita@users.noreply.github.com>
2022-05-11 19:19:20 +0200

    Update src/sign/network.tf

--- src/sign/network.tf
+++ src/sign/network.tf
@@ -6,7 +6,7 @@ data "azurerm_virtual_network" "vnet_common" {
 module "io_sign_snet" {
   source               = "git::https://github.com/pagopa/azurerm.git//subnet?ref=v2.13.1"
   name                 = "${local.project}-snet"
-  resource_group_name  = azurerm_resource_group.backend_rg.name
+  resource_group_name  = "io-p-rg-common"
   virtual_network_name = data.azurerm_virtual_network.vnet_common.name
   address_prefixes     = ["10.0.102.0/24"]
 
