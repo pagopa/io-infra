13c60d1aea4b2d0268f08536ad499d44e5902327
pasqualedevita <>
2022-05-23 13:11:58 +0200

    add spf for mail.zendesk.com

--- src/core/dns_io_italia_it.tf
+++ src/core/dns_io_italia_it.tf
@@ -75,8 +75,17 @@ resource "azurerm_dns_txt_record" "zendeskverification_io_italia_it" {
   record {
     value = "1da62a0d3c1426ec"
   }
+  tags = var.tags
+}
+
+# TXT for io.italia.it
+resource "azurerm_dns_txt_record" "io_italia_it" {
+  name                = "@"
+  zone_name           = azurerm_dns_zone.io_italia_it.name
+  resource_group_name = azurerm_resource_group.rg_external.name
+  ttl                 = var.dns_default_ttl_sec
   record {
-    value = "v=spf1 include:mail.zendesk.com -all"
+    value = "v=spf1 include:musvc.com include:_spf.google.com include:mail.zendesk.com -all"
   }
   tags = var.tags
 }
