c9a74898c0cd8967c2435bb4bbb65cc8c9c798e2
Pasquale De Vita <59291437+pasqualedevita@users.noreply.github.com>
2022-05-23 14:58:20 +0200

    Add dns spf for zendesk (#175)
    * add zendesk spf
    
    * add spf for mail.zendesk.com
    
    Co-authored-by: pasqualedevita <>

--- src/core/dns_io_italia_it.tf
+++ src/core/dns_io_italia_it.tf
@@ -77,3 +77,15 @@ resource "azurerm_dns_txt_record" "zendeskverification_io_italia_it" {
   }
   tags = var.tags
 }
+
+# TXT for io.italia.it
+resource "azurerm_dns_txt_record" "io_italia_it" {
+  name                = "@"
+  zone_name           = azurerm_dns_zone.io_italia_it.name
+  resource_group_name = azurerm_resource_group.rg_external.name
+  ttl                 = var.dns_default_ttl_sec
+  record {
+    value = "v=spf1 include:musvc.com include:_spf.google.com include:mail.zendesk.com -all"
+  }
+  tags = var.tags
+}
