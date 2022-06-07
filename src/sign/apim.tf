7d1514448f967ec79ffa3ec6c3473cb627dda626
pasqualedevita <>
2022-06-07 18:03:18 +0200

    style

--- src/sign/apim.tf
+++ src/sign/apim.tf
@@ -8,7 +8,7 @@ resource "azurerm_api_management_named_value" "io_fn_sign_url" {
   api_management_name = data.azurerm_api_management.apim_api.name
   resource_group_name = data.azurerm_api_management.apim_api.resource_group_name
   display_name        = "io-fn-sign-url"
-  value               = format("https://io-%s-sign-func.azurewebsites.net", var.env_short)
+  value               = format("https://%s-sign-func.azurewebsites.net", var.product)
 }
 
 /*resource "azurerm_api_management_named_value" "io_fn_sign_key" {
@@ -33,31 +33,39 @@ module "apim_io_sign_product" {
   subscription_required = true
   approval_required     = false
 
-  policy_xml = file("./io_sign_policy.xml")
+  policy_xml = file("./api_product/sign/_base_policy.xml")
+}
+
+resource "azurerm_api_management_api_version_set" "io_sign_issuer_api" {
+  name                = format("%s-sign-issuer-api", var.product)
+  resource_group_name = data.azurerm_api_management.apim_api.resource_group_name
+  api_management_name = data.azurerm_api_management.apim_api.name
+  display_name        = "IO Sign - Issuer API"
+  versioning_scheme   = "Segment"
 }
 
 module "apim_io_sign_issuer_api_v1" {
   source = "git::https://github.com/pagopa/azurerm.git//api_management_api?ref=v1.0.16"
 
-  name                  = format("%s-io-sign-issuer-api", var.env_short)
+  name                  = format("%s-sign-issuer-api", var.product)
   api_management_name   = data.azurerm_api_management.apim_api.name
   resource_group_name   = data.azurerm_api_management.apim_api.resource_group_name
   product_ids           = [module.apim_io_sign_product.product_id]
+  version_set_id        = azurerm_api_management_api_version_set.io_sign_issuer_api.id
   subscription_required = false
   api_version           = "v1"
   service_url           = null
 
   description  = "IO Sign - Issuer API"
   display_name = "IO Sign - Issuer API"
-  path         = "api/v1/sign"
+  path         = "api/sign"
   protocols    = ["https"]
 
   content_format = "swagger-json"
 
-  content_value = templatefile("./api/issuer/swagger.json.tpl", {
+  content_value = templatefile("./api/issuer/v1/_swagger.json.tpl", {
     host = "api.io.pagopa.it"
   })
 
-  xml_content = file("./api/issuer/policy.xml")
+  xml_content = file("./api/issuer/v1/_base_policy.xml")
 }
-
