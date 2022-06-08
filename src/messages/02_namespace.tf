ff211f7a21d703e2dbf18df2f4ee04dbd625683f
pasqualedevita <>
2022-06-08 16:33:24 +0200

    fix

--- src/messages/02_namespace.tf
+++ src/messages/02_namespace.tf
@@ -4,28 +4,6 @@ resource "kubernetes_namespace" "namespace" {
   }
 }
 
-# module "ingress" {
-#   source = "git::https://github.com/pagopa/azurerm.git//kubernetes_ingress?ref=v2.13.1"
-
-#   resource_group_name = local.aks_resource_group_name
-#   location            = var.location
-#   tenant_id           = data.azurerm_subscription.current.tenant_id
-#   cluster_name        = local.aks_name
-
-#   name      = kubernetes_namespace.namespace.metadata[0].name
-#   namespace = kubernetes_namespace.namespace.metadata[0].name
-#   key_vault = data.azurerm_key_vault.kv
-
-#   host = "${local.ingress_hostname}.${local.internal_dns_zone_name}"
-#   rules = [
-#     {
-#       path         = "/api/v1/email-validation/(.*)"
-#       service_name = "email-validation"
-#       service_port = 80
-#     }
-#   ]
-# }
-
 module "pod_identity" {
   source = "git::https://github.com/pagopa/azurerm.git//kubernetes_pod_identity?ref=v2.13.1"
 
@@ -45,7 +23,7 @@ resource "helm_release" "reloader" {
   name       = "reloader"
   repository = "https://stakater.github.io/stakater-charts"
   chart      = "reloader"
-  version    = "v0.0.110"
+  version    = var.reloader_helm_version
   namespace  = kubernetes_namespace.namespace.metadata[0].name
 
   set {
--- src/profile-common/02_security.tf
+++ src/profile-common/02_security.tf
@@ -22,12 +22,38 @@ module "key_vault" {
   tags = var.tags
 }
 
-## ad group policy ##
-resource "azurerm_key_vault_access_policy" "ad_group_policy" {
+## adgroup_admin group policy ##
+resource "azurerm_key_vault_access_policy" "adgroup_admin" {
   key_vault_id = module.key_vault.id
 
   tenant_id = data.azurerm_client_config.current.tenant_id
   object_id = data.azuread_group.adgroup_admin.object_id
+
+  key_permissions         = ["Get", "List", "Update", "Create", "Import", "Delete", ]
+  secret_permissions      = ["Get", "List", "Set", "Delete", "Restore", "Recover", ]
+  storage_permissions     = []
+  certificate_permissions = ["Get", "List", "Update", "Create", "Import", "Delete", "Restore", "Recover", ]
+}
+
+## adgroup_developers group policy ##
+resource "azurerm_key_vault_access_policy" "adgroup_contributors" {
+  key_vault_id = module.key_vault.id
+
+  tenant_id = data.azurerm_client_config.current.tenant_id
+  object_id = data.azuread_group.adgroup_contributors.object_id
+
+  key_permissions         = ["Get", "List", "Update", "Create", "Import", "Delete", ]
+  secret_permissions      = ["Get", "List", "Set", "Delete", "Restore", "Recover", ]
+  storage_permissions     = []
+  certificate_permissions = ["Get", "List", "Update", "Create", "Import", "Delete", "Restore", "Recover", ]
+}
+
+## adgroup_developers group policy ##
+resource "azurerm_key_vault_access_policy" "adgroup_developers" {
+  key_vault_id = module.key_vault.id
+
+  tenant_id = data.azurerm_client_config.current.tenant_id
+  object_id = data.azuread_group.adgroup_developers.object_id
 
   key_permissions         = ["Get", "List", "Update", "Create", "Import", "Delete", ]
   secret_permissions      = ["Get", "List", "Set", "Delete", "Restore", "Recover", ]
