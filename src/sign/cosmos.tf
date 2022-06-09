c4e672d37f99c1127450142024ccfac6b7a23de4
Luca Cavallaro <luca.cavallaro@pagopa.it>
2022-06-09 17:52:43 +0200

    add "products" collection

--- src/sign/cosmos.tf
+++ src/sign/cosmos.tf
@@ -37,3 +37,13 @@ module "cosmosdb_sql_container_signature-requests" {
   throughput          = var.io_sign_database.signature_requests.throughput
 }
 
+module "cosmosdb_sql_container_signature-requests" {
+  source              = "git::https://github.com/pagopa/azurerm.git//cosmosdb_sql_container?ref=v2.13.1"
+  name                = "products"
+  resource_group_name = azurerm_resource_group.data_rg.name
+  account_name        = module.cosmosdb_account.name
+  database_name       = module.cosmosdb_sql_database_db.name
+  partition_key_path  = "/subscriptionId"
+  throughput          = var.io_sign_database.signature_requests.throughput
+}
+
