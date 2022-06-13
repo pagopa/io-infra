3feb5e9754662ed7e319834ff6527708c601c09e
pasqualedevita <>
2022-06-13 13:11:41 +0200

    fix cosmos

--- src/payments-common/01_network.tf
+++ src/payments-common/01_network.tf
@@ -3,6 +3,11 @@ data "azurerm_virtual_network" "vnet_common" {
   resource_group_name = local.vnet_common_resource_group_name
 }
 
+data "azurerm_private_dns_zone" "privatelink_mongo_cosmos_azure_com" {
+  name                = "privatelink.mongo.cosmos.azure.com"
+  resource_group_name = format("%s-rg-common", local.product)
+}
+
 data "azurerm_subnet" "private_endpoints_subnet" {
   name                 = "pendpoints"
   virtual_network_name = local.vnet_common_name
--- src/payments-common/03_database.tf
+++ src/payments-common/03_database.tf
@@ -7,7 +7,6 @@ resource "azurerm_resource_group" "data_rg" {
 
 
 module "cosmosdb_account_mongodb" {
-
   source = "git::https://github.com/pagopa/azurerm.git//cosmosdb_account?ref=v2.15.1"
 
   name                 = "${local.product}-${var.domain}-mongodb-account"
@@ -17,31 +16,37 @@ module "cosmosdb_account_mongodb" {
   enable_free_tier     = false
   kind                 = "MongoDB"
   capabilities         = ["EnableMongo"]
-  mongo_server_version = "4.2"
+  mongo_server_version = "4.0"
 
   public_network_access_enabled     = false
   private_endpoint_enabled          = true
   subnet_id                         = data.azurerm_subnet.private_endpoints_subnet.id
-  private_dns_zone_ids              = [data.azurerm_private_dns_zone.cosmos_mongo.id]
+  private_dns_zone_ids              = [data.azurerm_private_dns_zone.privatelink_mongo_cosmos_azure_com.id]
   is_virtual_network_filter_enabled = false
 
   main_geo_location_location       = azurerm_resource_group.data_rg.location
   main_geo_location_zone_redundant = false
-  additional_geo_locations         = "northeurope"
+  additional_geo_locations = [{
+    location          = "northeurope"
+    failover_priority = 1
+    zone_redundant    = false
+  }]
+  consistency_policy = {
+    consistency_level = "Session"
+    max_interval_in_seconds = null
+    max_staleness_prefix = null
+  }
 
   tags = var.tags
 }
 
 resource "azurerm_cosmosdb_mongo_database" "db" {
-
   name                = "db"
   resource_group_name = azurerm_resource_group.data_rg.name
   account_name        = module.cosmosdb_account_mongodb.name
 
-  throughput = 400
-
-  autoscale_settings = {
-    max_throughput = 1000
+  autoscale_settings {
+    max_throughput = 4000
   }
 }
 
