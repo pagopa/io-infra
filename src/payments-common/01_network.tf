1c86688164bef8a3f37779394d78d6d43ef4a8ad
AleDore <alessio85.dore@gmail.com>
2022-06-13 12:03:15 +0200

    initial cosmos resources

--- src/payments-common/01_network.tf
+++ src/payments-common/01_network.tf
@@ -2,3 +2,9 @@ data "azurerm_virtual_network" "vnet_common" {
   name                = local.vnet_common_name
   resource_group_name = local.vnet_common_resource_group_name
 }
+
+data "azurerm_subnet" "private_endpoints_subnet" {
+  name                 = "pendpoints"
+  virtual_network_name = local.vnet_common_name
+  resource_group_name  = local.vnet_common_resource_group_name
+}
