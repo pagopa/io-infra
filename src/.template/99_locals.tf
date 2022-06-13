791e0f4097550b245fac1523929d3b2ace56a86c
pasqualedevita <>
2022-06-13 10:43:54 +0200

    add template

---
+++ src/.template/99_locals.tf
@@ -0,0 +1,32 @@
+locals {
+  project = "${var.prefix}-${var.env_short}-${var.domain}-${var.location_short}-${var.instance}"
+  product = "${var.prefix}-${var.env_short}"
+
+  app_insights_ips_west_europe = [
+    "51.144.56.96/28",
+    "51.144.56.112/28",
+    "51.144.56.128/28",
+    "51.144.56.144/28",
+    "51.144.56.160/28",
+    "51.144.56.176/28",
+  ]
+
+  monitor_action_group_slack_name = "SlackPagoPA"
+  monitor_action_group_email_name = "EmailPagoPA"
+
+  vnet_name                = "${local.product}-${var.location_short}-${var.instance}-vnet"
+  vnet_resource_group_name = "${local.product}-${var.location_short}-${var.instance}-vnet-rg"
+
+  vnet_common_name                = "${local.product}-vnet-common"
+  vnet_common_resource_group_name = "${local.product}-rg-common"
+
+  ingress_hostname                      = "${var.location_short}${var.instance}.${var.domain}"
+  internal_dns_zone_name                = "internal.${var.prefix}.pagopa.it"
+  internal_dns_zone_resource_group_name = "${local.product}-rg-internal"
+
+  acr_name                = replace("${local.product}commonacr", "-", "")
+  acr_resource_group_name = "${local.product}-container-registry-rg"
+
+  aks_name                = "${local.product}-${var.location_short}-${var.instance}-aks"
+  aks_resource_group_name = "${local.product}-${var.location_short}-${var.instance}-aks-rg"
+}
