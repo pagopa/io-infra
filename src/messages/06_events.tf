a48699860747666d5a8bdb23924fa0df4cc108a2
Alessio Dore <57567806+AleDore@users.noreply.github.com>
2022-06-21 14:28:20 +0200

    [ICC-102] Add Event Hub for CQRS (#194)
    * [ICC-102] Add Event Hub for CQRS
    
    * add eventhub connection
    
    Co-authored-by: Pasquale De Vita <59291437+pasqualedevita@users.noreply.github.com>
    Co-authored-by: pasqualedevita <>

---
+++ src/messages/06_events.tf
@@ -0,0 +1,70 @@
+resource "azurerm_resource_group" "event_rg" {
+  name     = format("%s-evt-rg", local.project)
+  location = var.location
+
+  tags = var.tags
+}
+
+module "event_hub" {
+  count                    = var.ehns_enabled ? 1 : 0
+  source                   = "git::https://github.com/pagopa/azurerm.git//eventhub?ref=v2.18.3"
+  name                     = format("%s-evh-ns", local.project)
+  location                 = var.location
+  resource_group_name      = azurerm_resource_group.event_rg.name
+  auto_inflate_enabled     = var.ehns_auto_inflate_enabled
+  sku                      = var.ehns_sku_name
+  capacity                 = var.ehns_capacity
+  maximum_throughput_units = var.ehns_maximum_throughput_units
+  zone_redundant           = var.ehns_zone_redundant
+
+  virtual_network_ids            = [data.azurerm_virtual_network.vnet_common.id]
+  subnet_id                      = data.azurerm_subnet.private_endpoints_subnet.id
+  private_dns_zone_record_A_name = null
+
+  eventhubs = var.eventhubs
+
+  network_rulesets = [
+    {
+      default_action       = "Deny",
+      ip_rule              = var.ehns_ip_rules
+      virtual_network_rule = []
+      # virtual_network_rule = [
+      #   {
+      #     subnet_id                                       = null,
+      #     ignore_missing_virtual_network_service_endpoint = false
+      #   }
+      # ]
+    }
+  ]
+
+  alerts_enabled = var.ehns_alerts_enabled
+  metric_alerts  = var.ehns_metric_alerts
+  action = [
+    {
+      action_group_id    = data.azurerm_monitor_action_group.slack.id
+      webhook_properties = null
+    },
+    {
+      action_group_id    = data.azurerm_monitor_action_group.email.id
+      webhook_properties = null
+    }
+  ]
+
+  private_dns_zones = {
+    id   = [data.azurerm_private_dns_zone.privatelink_servicebus_windows_net.id]
+    name = [data.azurerm_private_dns_zone.privatelink_servicebus_windows_net.name]
+  }
+
+  tags = var.tags
+}
+
+#tfsec:ignore:AZU023
+resource "azurerm_key_vault_secret" "event_hub_keys" {
+  for_each = var.ehns_enabled ? module.event_hub[0].key_ids : {}
+
+  name         = "${replace(each.key, ".", "-")}-${var.location_short}-${var.instance}-evh-key"
+  value        = module.event_hub[0].keys[each.key].primary_key
+  content_type = "text/plain"
+
+  key_vault_id = data.azurerm_key_vault.kv.id
+}
