0ed1f43c3cf7f3a70c18dd95851f7db6c0a9b7fb
Alessio Dore <57567806+AleDore@users.noreply.github.com>
2022-06-24 11:40:30 +0200

    Update src/messages-common/05_apim.tf
    Co-authored-by: Pasquale De Vita <59291437+pasqualedevita@users.noreply.github.com>

--- src/messages-common/05_apim.tf
+++ src/messages-common/05_apim.tf
@@ -3,7 +3,7 @@ data "azurerm_api_management" "apim_api" {
   resource_group_name = local.apim_resource_group_name
 }
 
-resource "azurerm_api_management_group" "third_party_group" {
+resource "azurerm_api_management_group" "apithirdpartymessagewrite" {
   name                = "apithirdpartymessagewrite"
   api_management_name = data.azurerm_api_management.apim_api.name
   resource_group_name = data.azurerm_api_management.apim_api.resource_group_name
