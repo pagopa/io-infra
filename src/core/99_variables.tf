c843b7229205cad11f70d88d10723d78bcc43d1e
pasqualedevita <>
2022-07-12 16:45:19 +0200

    Add apim autoscale

--- src/core/99_variables.tf
+++ src/core/99_variables.tf
@@ -271,6 +271,46 @@ variable "apim_publisher_name" {
 variable "apim_sku" {
   type = string
 }
+
+variable "apim_autoscale" {
+  type = object(
+    {
+      enabled                       = bool
+      default_instances             = number
+      minimum_instances             = number
+      maximum_instances             = number
+      scale_out_capacity_percentage = number
+      scale_out_time_window         = string
+      scale_out_value               = string
+      scale_out_cooldown            = string
+      scale_in_capacity_percentage  = number
+      scale_in_time_window          = string
+      scale_in_value                = string
+      scale_in_cooldown             = string
+    }
+  )
+  default = {
+    enabled                       = false
+    default_instances             = 1
+    minimum_instances             = 1
+    maximum_instances             = 5
+    scale_out_capacity_percentage = 60
+    scale_out_time_window         = "PT10M"
+    scale_out_value               = "2"
+    scale_out_cooldown            = "PT45M"
+    scale_in_capacity_percentage  = 30
+    scale_in_time_window          = "PT30M"
+    scale_in_value                = "1"
+    scale_in_cooldown             = "PT30M"
+  }
+  description = "Configure Apim autoscale on capacity metric"
+}
+
+variable "apim_alerts_enabled" {
+  type        = bool
+  description = "Enable alerts"
+  default     = true
+}
 ##
 
 ## Redis cache
