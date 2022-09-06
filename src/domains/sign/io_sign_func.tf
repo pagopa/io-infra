6a514a67a8326d6baef5d891ad4c9c9b563c026b
Francesco Grauso <grausof@gmail.com>
2022-09-06 09:10:45 +0200

    Add queue to internal "iosign" blob storage (#231)
    Co-authored-by: Pasquale De Vita <59291437+pasqualedevita@users.noreply.github.com>

--- src/domains/sign/io_sign_func.tf
+++ src/domains/sign/io_sign_func.tf
@@ -28,12 +28,12 @@ module "io_sign_func" {
   health_check_path = "api/v1/sign/info"
 
   internal_storage = {
-    enable                     = false,
+    enable                     = true,
     private_endpoint_subnet_id = data.azurerm_subnet.private_endpoints_subnet.id,
     private_dns_zone_blob_ids  = []
     private_dns_zone_queue_ids = []
     private_dns_zone_table_ids = []
-    queues                     = []
+    queues                     = ["waiting-for-message", "waiting-for-signature", "waiting-for-document"]
     containers                 = []
     blobs_retention_days       = 1
   }
