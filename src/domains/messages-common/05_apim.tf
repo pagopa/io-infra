9c328abe62222eabc617dc8a40268b8d8842dea8
AleDore <alessio85.dore@gmail.com>
2022-09-06 14:17:02 +0200

    Expose Service Messages' API

--- src/domains/messages-common/05_apim.tf
+++ src/domains/messages-common/05_apim.tf
@@ -26,3 +26,42 @@ resource "azurerm_api_management_group" "apimessagereadadvanced" {
   display_name        = "ApiMessageReadAdvanced"
   description         = "A group that enables to send Advanced Read Messages"
 }
+
+module "apim_product_notifications" {
+  source = "git::https://github.com/pagopa/azurerm.git//api_management_product?ref=v1.0.16"
+
+  product_id   = "io-notifications-api"
+  display_name = "IO NOTIFICATIONS API"
+  description  = "Product for IO notifications"
+
+  api_management_name = data.azurerm_api_management.apim_api.name
+  resource_group_name = data.azurerm_api_management.apim_api.resource_group_name
+
+  published             = true
+  subscription_required = true
+  approval_required     = false
+
+  policy_xml = file("./api_product/messages/_base_policy.xml")
+}
+
+module "apim_service_messages_api_v1" {
+  source = "git::https://github.com/pagopa/azurerm.git//api_management_api?ref=v1.0.16"
+
+  name                  = format("%s-service-messages-api", local.product)
+  api_management_name   = data.azurerm_api_management.apim_api.name
+  resource_group_name   = data.azurerm_api_management.apim_api.resource_group_name
+  product_ids           = [module.apim_product_notifications.product_id]
+  subscription_required = true
+  service_url           = null
+
+  description  = "IO Service - Messages API"
+  display_name = "IO Service - Messages API"
+  path         = "api/v1"
+  protocols    = ["https"]
+
+  content_format = "openapi"
+
+  content_value = file("./api/service-messages/v1/_openapi.yaml")
+
+  xml_content = file("./api/service-messages/v1/_base_policy.xml")
+}
