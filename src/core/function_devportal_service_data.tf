4d94fbceee9467e0b7da83fbc7fa2c2cefc4d188
Emanuele De Cupis <ema.decu@gmail.com>
2022-09-07 13:00:45 +0200

    set db sku

--- src/core/function_devportal_service_data.tf
+++ src/core/function_devportal_service_data.tf
@@ -243,7 +243,7 @@ module "devportalservicedata_db_server" {
   administrator_login    = data.azurerm_key_vault_secret.devportalservicedata_db_server_adm_username.value
   administrator_password = data.azurerm_key_vault_secret.devportalservicedata_db_server_adm_password.value
 
-  sku_name                     = "GP_Gen5_2"
+  sku_name                     = "GP_Standard_D2ds_v4"
   db_version                   = 14
   geo_redundant_backup_enabled = false
 
