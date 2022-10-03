8bb4fc71e9a0748dde725c42b1bb88cfe3d4fe42
Emanuele De Cupis <ema.decu@gmail.com>
2022-10-03 09:49:21 +0200

    remove comments on columns

--- src/psql/selfcare/devportal-service-data/db/V1__schema.sql
+++ src/psql/selfcare/devportal-service-data/db/V1__schema.sql
@@ -5,11 +5,11 @@ CREATE SCHEMA "DeveloperPortalServiceData";
 --  enriched and denormalized in order to help queries for admin and statitstics purposes
 CREATE TABLE IF NOT EXISTS "DeveloperPortalServiceData".services
 (
-    "id" character(50)  NOT NULL, 
-    "organizationFiscalCode" character(11)  NOT NULL,
-    "version" integer NOT NULL,
+    "id" character(50)  NOT NULL, -- 'the unique identifier for the Service'
+    "organizationFiscalCode" character(11)  NOT NULL, -- 'identifies the Organization the Service belongs to'
+    "version" integer NOT NULL, -- 'revision number of the Service this record is built upon'
     "name" character varying ,
-    "isVisible" BOOLEAN NOT NULL,
+    "isVisible" BOOLEAN NOT NULL, -- 'visibility attribute for the Service'
 
     -- data related to the APIM account that owns the related subscription
     "subscriptionAccountId" character(26)  NOT NULL,
@@ -20,9 +20,3 @@ CREATE TABLE IF NOT EXISTS "DeveloperPortalServiceData".services
     "updateAt" time without time zone DEFAULT CURRENT_TIMESTAMP,
     CONSTRAINT migrations_pkey PRIMARY KEY ("id")
 );
-
-COMMENT ON COLUMN "DeveloperPortalServiceData".services.id IS 'the unique identifier for the Service';
-COMMENT ON COLUMN "DeveloperPortalServiceData".services.organizationFiscalCode IS 'identifies the Organization the Service belongs to';
-COMMENT ON COLUMN "DeveloperPortalServiceData".services.version IS 'revision number of the Service this record is built upon';
-COMMENT ON COLUMN "DeveloperPortalServiceData".services.isVisible IS 'visibility attribute for the Service';
-
