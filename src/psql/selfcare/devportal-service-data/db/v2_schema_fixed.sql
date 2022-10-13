4253232878081fc4f65333a3ebcabe26b8125473
LoFrance <lorenzo.franceschini@pagopa.it>
2022-10-13 16:47:58 +0200

    Refactor requireSecureChannels constraint to not null

--- src/psql/selfcare/devportal-service-data/db/v2_schema_fixed.sql
+++ src/psql/selfcare/devportal-service-data/db/v2_schema_fixed.sql
@@ -1,2 +1,2 @@
 ALTER TABLE "DeveloperPortalServiceData".services
-     ADD COLUMN "requireSecureChannels" BOOLEAN DEFAULT FALSE; -- if true the message will be available only in the IOApp instead of both e-mail and IOApp
+     ADD COLUMN "requireSecureChannels" NOT NULL; -- if true the message will be available only in the IOApp instead of both e-mail and IOApp
