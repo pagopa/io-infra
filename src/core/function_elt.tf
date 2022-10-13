bd05f00f44a2125ea960475b7216b33c78bf9707
fabriziopapi <fabrizio.papi@pagopa-it>
2022-10-13 10:40:25 +0200

    fix typo

--- src/core/function_elt.tf
+++ src/core/function_elt.tf
@@ -1,7 +1,7 @@
 locals {
   function_elt = {
     app_settings = {
-      AzureWebJobs.CosmosApiServicesChangeFeed.Disabled" = "1"
+      AzureWebJobs.CosmosApiServicesChangeFeed.Disabled = "1"
 
       FUNCTIONS_WORKER_RUNTIME       = "node"
       WEBSITE_NODE_DEFAULT_VERSION   = "14.16.0"
