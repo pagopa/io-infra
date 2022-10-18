3cdc48fe837186be651c5c5d9be97245074dbad3
Pasquale De Vita <59291437+pasqualedevita@users.noreply.github.com>
2022-10-18 12:52:43 +0200

    Upgrade app service module (#266)
    Co-authored-by: pasqualedevita <>

--- src/core/devportal.tf
+++ src/core/devportal.tf
@@ -65,7 +65,7 @@ resource "azurerm_app_service_virtual_network_swift_connection" "devportal_be" {
 #tfsec:ignore:azure-appservice-authentication-enabled:exp:2022-05-01 # already ignored, maybe a bug in tfsec
 #tfsec:ignore:azure-appservice-require-client-cert:exp:2022-05-01 # already ignored, maybe a bug in tfsec
 module "appservice_devportal_be" {
-  source = "git::https://github.com/pagopa/azurerm.git//app_service?ref=v2.9.1"
+  source = "git::https://github.com/pagopa/azurerm.git//app_service?ref=v3.2.1"
 
   name                = format("%s-app-devportal-be", local.project)
   resource_group_name = azurerm_resource_group.selfcare_be_rg.name
