b918a1998fe4fa75fea4817f186d71d62dc4289a
Emanuele De Cupis <ema.decu@gmail.com>
2022-10-27 15:46:00 +0200

    document choice

--- src/psql/selfcare/subscription-migrations/migrations/db/V3__visibility_fileds.sql
+++ src/psql/selfcare/subscription-migrations/migrations/db/V3__visibility_fileds.sql
@@ -1,3 +1,7 @@
 ALTER TABLE "SelfcareIOSubscriptionMigrations".migrations
+    -- In order to avoid null fileds, we set FALSE as default
+    -- This is harmless as
+    --  * we will ensure this field to be set at insert time
+    --  * the field is not considered yet by the application (at the time of writing)
     ADD COLUMN "isVisibile" TYPE boolean NOT NULL default false,
     ADD COLUMN "hasBeenVisibleOnce" TYPE boolean NOT NULL default false;
