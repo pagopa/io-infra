fd9360af33a59e82a40c94b5bd674fba736eb7b1
fabriziopapi <fabrizio.papi@pagopa.it>
2022-10-27 18:36:22 +0200

    fix lint

--- src/core/function_elt.tf
+++ src/core/function_elt.tf
@@ -151,8 +151,8 @@ module "function_elt" {
 
   app_settings = merge(
     local.function_elt.app_settings, {
-      "AzureWebJobs.CosmosApiServicesChangeFeed.Disabled"       = "1"
-      "AzureWebJobs.CosmosApiMessageStatusChangeFeed.Disabled"  = "1"
+      "AzureWebJobs.CosmosApiServicesChangeFeed.Disabled"      = "1"
+      "AzureWebJobs.CosmosApiMessageStatusChangeFeed.Disabled" = "1"
     }
   )
 
