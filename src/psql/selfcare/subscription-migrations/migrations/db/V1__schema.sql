f67b4c5e8d74582dedfe44685d2c8c13765431ef
Emanuele De Cupis <ema.decu@gmail.com>
2022-10-28 18:25:21 +0200

    reset schema to a correct shape

--- src/psql/selfcare/subscription-migrations/migrations/db/V1__schema.sql
+++ src/psql/selfcare/subscription-migrations/migrations/db/V1__schema.sql
@@ -3,16 +3,25 @@ CREATE SCHEMA "SelfcareIOSubscriptionMigrations";
 
 CREATE TABLE IF NOT EXISTS "SelfcareIOSubscriptionMigrations".migrations
 (
-    "subscriptionId" character(26)  NOT NULL,
+    "subscriptionId" character(50)  NOT NULL,
     "organizationFiscalCode" character(11)  NOT NULL,
-    "sourceId" character(26)  NOT NULL,
-    "sourceName" character(1) ,
-    "sourceSurname" character(1) ,
-    "sourceEmail" character(1) ,
-    status character(1)  NOT NULL DEFAULT 'INITIAL'::bpchar,
-    note text ,
     "serviceVersion" integer NOT NULL,
-    "serviceName" character varying ,
-    "updateAt" time without time zone DEFAULT CURRENT_TIMESTAMP,
+    "serviceName" text varying ,
+
+    "sourceId" character(26)  NOT NULL,
+    "sourceName" text ,
+    "sourceSurname" text ,
+    "sourceEmail" text ,
+    
+    -- In order to avoid null fileds, we set FALSE as default
+    -- This is harmless as
+    --  * we will ensure this field to be set at insert time
+    --  * the field is not considered yet by the application (at the time of writing)
+    ADD COLUMN "isVisible" boolean NOT NULL default false,
+    ADD COLUMN "hasBeenVisibleOnce"  boolean NOT NULL default false;
+
+    status text  NOT NULL DEFAULT 'INITIAL'::bpchar,
+    note text ,
+    "updateAt" timestamp without time zone DEFAULT CURRENT_TIMESTAMP,
     CONSTRAINT migrations_pkey PRIMARY KEY ("subscriptionId")
 )
--- src/psql/selfcare/subscription-migrations/migrations/db/V3__visibility_fileds.sql
+++
@@ -1,7 +0,0 @@
-ALTER TABLE "SelfcareIOSubscriptionMigrations".migrations
-    -- In order to avoid null fileds, we set FALSE as default
-    -- This is harmless as
-    --  * we will ensure this field to be set at insert time
-    --  * the field is not considered yet by the application (at the time of writing)
-    ADD COLUMN "isVisibile" boolean NOT NULL default false,
-    ADD COLUMN "hasBeenVisibleOnce"  boolean NOT NULL default false;
