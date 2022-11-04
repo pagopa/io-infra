aaef92dd748be554a9f050be7f71aaa2d71c90b1
fabriziopapi <fabrizio.papi@pagopa-it>
2022-11-04 11:23:49 +0100

    add payment update topic jaas to secret

--- src/domains/messages-common/06_events.tf
+++ src/domains/messages-common/06_events.tf
@@ -45,3 +45,19 @@ resource "azurerm_key_vault_secret" "messages_io-p-message-reminder-send-weu-pro
 
   key_vault_id = module.key_vault.id
 }
+
+data "azurerm_eventhub_authorization_rule" "io-p-payments-weu-prod01-evh-ns_payment-updates_io-p-reminder" {
+  name                = "io-p-reminder"
+  namespace_name      = "${local.product}-payments-weu-prod01-evh-ns"
+  eventhub_name       = "payment-updates"
+  resource_group_name = "${local.product}-payments-weu-prod01-evt-rg"
+}
+
+#tfsec:ignore:AZU023
+resource "azurerm_key_vault_secret" "payments_io-p-payment-updates-weu-prod01-evh-reminder_jaas-connection-string" {
+  name         = "payments-io-p-payment-updates-weu-prod01-evh-reminder-jaas-connection-string"
+  value        = "org.apache.kafka.common.security.plain.PlainLoginModule required username=\"$ConnectionString\" password=\"${data.azurerm_eventhub_authorization_rule.io-p-payments-weu-prod01-evh-ns_payment-updates_io-p-reminder.primary_connection_string}\";"
+  content_type = "full connection string for java"
+
+  key_vault_id = module.key_vault.id
+}
