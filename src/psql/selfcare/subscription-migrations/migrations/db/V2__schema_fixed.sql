8d31471223f3637d4dbee4a624ef6eba901ec26f
Emanuele De Cupis <ema.decu@gmail.com>
2022-11-08 10:56:44 +0100

    [#IOPSC-105] Remove schema files for `subscrioptions-migration` (#282)

--- src/psql/selfcare/subscription-migrations/migrations/db/V2__schema_fixed.sql
+++
@@ -1,8 +0,0 @@
-ALTER TABLE "SelfcareIOSubscriptionMigrations".migrations
-    ALTER COLUMN "subscriptionId" TYPE character(50),
-    ALTER COLUMN "sourceName" TYPE text,
-    ALTER COLUMN "sourceSurname" TYPE text,
-    ALTER COLUMN "sourceEmail" TYPE text,
-    ALTER COLUMN "status" TYPE text,
-    ALTER COLUMN "note" TYPE text,
-    ALTER COLUMN "serviceName" TYPE text;
