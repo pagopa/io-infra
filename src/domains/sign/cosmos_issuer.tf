7347f3baed8c9aada53d1f34d56e19b88a77562a
Manuel Coppotelli <manuel.coppotelli@pagopa.it>
2022-11-23 15:10:44 +0100

    sign: handle manual cosmos output
    On-behalf-of: @pagopa

--- src/domains/sign/cosmos_issuer.tf
+++ src/domains/sign/cosmos_issuer.tf
@@ -1,45 +1,34 @@
 module "cosmosdb_sql_database_issuer" {
-  source              = "git::https://github.com/pagopa/azurerm.git//cosmosdb_sql_database?ref=v2.13.1"
+  source              = "git::https://github.com/pagopa/azurerm.git//cosmosdb_sql_database?ref=v2.7.2"
   name                = "issuer"
   resource_group_name = azurerm_resource_group.data_rg.name
   account_name        = module.cosmosdb_account.name
-  throughput          = var.io_sign_database_issuer.throughput
 }
 
 module "cosmosdb_sql_container_issuer-dossiers" {
-  source              = "git::https://github.com/pagopa/azurerm.git//cosmosdb_sql_container?ref=v2.13.1"
+  source              = "git::https://github.com/pagopa/azurerm.git//cosmosdb_sql_container?ref=v3.2.5"
   name                = "dossiers"
   resource_group_name = azurerm_resource_group.data_rg.name
   account_name        = module.cosmosdb_account.name
   database_name       = module.cosmosdb_sql_database_issuer.name
   partition_key_path  = "/issuerId"
-
-  autoscale_settings = {
-    max_throughput = var.io_sign_database_issuer.dossiers.max_throughput
-  }
 }
 
 module "cosmosdb_sql_container_issuer-signature-requests" {
-  source              = "git::https://github.com/pagopa/azurerm.git//cosmosdb_sql_container?ref=v2.13.1"
+  source              = "git::https://github.com/pagopa/azurerm.git//cosmosdb_sql_container?ref=v3.2.5"
   name                = "signature-requests"
   resource_group_name = azurerm_resource_group.data_rg.name
   account_name        = module.cosmosdb_account.name
   database_name       = module.cosmosdb_sql_database_issuer.name
   partition_key_path  = "/issuerId"
-  autoscale_settings = {
-    max_throughput = var.io_sign_database_issuer.signature_requests.max_throughput
-  }
 }
 
 module "cosmosdb_sql_container_issuer-uploads" {
-  source              = "git::https://github.com/pagopa/azurerm.git//cosmosdb_sql_container?ref=v2.13.1"
+  source              = "git::https://github.com/pagopa/azurerm.git//cosmosdb_sql_container?ref=v3.2.5"
   name                = "uploads"
   resource_group_name = azurerm_resource_group.data_rg.name
   account_name        = module.cosmosdb_account.name
   database_name       = module.cosmosdb_sql_database_issuer.name
   partition_key_path  = "/id"
   default_ttl         = var.io_sign_database_issuer.uploads.ttl
-  autoscale_settings = {
-    max_throughput = var.io_sign_database_issuer.uploads.max_throughput
-  }
 }
