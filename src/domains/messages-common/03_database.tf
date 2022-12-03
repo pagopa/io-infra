58d90216ae692190cc0557d3347eb0950829be42
Alessio Dore <57567806+AleDore@users.noreply.github.com>
2022-12-03 12:16:00 +0100

    [#IOCIT-204] Add MySQL Connection config (#322)

--- src/domains/messages-common/03_database.tf
+++ src/domains/messages-common/03_database.tf
@@ -269,6 +269,13 @@ resource "azurerm_mysql_flexible_database" "reminder_mysql_db" {
   collation           = "utf8_unicode_ci"
 }
 
+resource "azurerm_mysql_flexible_server_configuration" "max_connections" {
+  name                = "max_connections"
+  resource_group_name = azurerm_resource_group.data_rg.name
+  server_name         = azurerm_mysql_flexible_server.reminder_mysql_server.name
+  value               = "341"
+}
+
 resource "azurerm_key_vault_secret" "reminder_mysql_db_server_url" {
   name = "${azurerm_mysql_flexible_server.reminder_mysql_server.name}-REMINDER-MYSQL-DB-URL"
   value = format("jdbc:mysql://%s:%s/%s",
