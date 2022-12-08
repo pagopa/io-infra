676b967509c5b7144bcb31d485711a0c2c178070
Manuel Coppotelli <manuel.coppotelli@pagopa.it>
2022-12-08 14:07:07 +0100

    sign: Allow function access from VPN

--- src/domains/sign/network.tf
+++ src/domains/sign/network.tf
@@ -24,6 +24,8 @@ module "io_sign_snet" {
 
   enforce_private_link_endpoint_network_policies = true
 
+  # network_security_group_id = azurerm_network_security_group.io_sign_issuer_nsg.id
+
   service_endpoints = [
     "Microsoft.Web",
     "Microsoft.AzureCosmosDB",
@@ -39,6 +41,22 @@ module "io_sign_snet" {
   }
 }
 
+resource "azurerm_network_security_group" "io_sign_issuer_nsg" {
+  name                = format("%s-issuer-nsg", local.project)
+  location            = data.azurerm_virtual_network.vnet_common.location
+  resource_group_name = data.azurerm_virtual_network.vnet_common.resource_group_name
+
+  tags = var.tags
+}
+
+resource "azurerm_network_security_group" "io_sign_user_nsg" {
+  name                = format("%s-user-nsg", local.project)
+  location            = data.azurerm_virtual_network.vnet_common.location
+  resource_group_name = data.azurerm_virtual_network.vnet_common.resource_group_name
+
+  tags = var.tags
+}
+
 module "io_sign_user_snet" {
   source               = "git::https://github.com/pagopa/azurerm.git//subnet?ref=v2.7.2"
   name                 = format("%s-user-snet", local.project)
@@ -47,6 +65,8 @@ module "io_sign_user_snet" {
   address_prefixes     = ["10.0.103.0/24"]
 
   enforce_private_link_endpoint_network_policies = true
+
+  # network_security_group_id = azurerm_network_security_group.io_sign_user_nsg.id
 
   service_endpoints = [
     "Microsoft.Web",
@@ -75,6 +95,11 @@ data "azurerm_private_dns_zone" "privatelink_blob_core_windows_net" {
 
 data "azurerm_private_dns_zone" "privatelink_queue_core_windows_net" {
   name                = "privatelink.queue.core.windows.net"
+  resource_group_name = "io-p-rg-common"
+}
+
+data "azurerm_private_dns_zone" "privatelink_azurewebsites_net" {
+  name                = "privatelink.azurewebsites.net"
   resource_group_name = "io-p-rg-common"
 }
 
@@ -132,19 +157,44 @@ resource "azurerm_private_endpoint" "queue" {
   tags = var.tags
 }
 
-# TODO(SFEQS-1191) Allow function access from VPN
-# resource "azurerm_private_endpoint" "function" {
-#   name                = format("%s-function", module.io_sign_user_func.name)
-#   location            = azurerm_resource_group.data_rg.location
-#   resource_group_name = azurerm_resource_group.data_rg.name
-#   subnet_id           = data.azurerm_subnet.private_endpoints_subnet.id
+resource "azurerm_private_endpoint" "io_sign_user_func" {
+  name                = format("%s-endpoint", module.io_sign_user_func.name)
+  location            = azurerm_resource_group.data_rg.location
+  resource_group_name = azurerm_resource_group.data_rg.name
+  subnet_id           = data.azurerm_subnet.private_endpoints_subnet.id
 
-#   private_service_connection {
-#     name                           = format("%s-function", module.io_sign_user_func.name)
-#     private_connection_resource_id = module.io_sign_user_func.id
-#     is_manual_connection           = false
-#     subresource_names              = ["sites"]
-#   }
+  private_service_connection {
+    name                           = format("%s-endpoint", module.io_sign_user_func.name)
+    private_connection_resource_id = module.io_sign_user_func.id
+    is_manual_connection           = false
+    subresource_names              = ["sites"]
+  }
 
-#   tags = var.tags
-# }
+  private_dns_zone_group {
+    name                 = "private-dns-zone-group"
+    private_dns_zone_ids = [data.azurerm_private_dns_zone.privatelink_azurewebsites_net.id]
+  }
+
+  tags = var.tags
+}
+
+resource "azurerm_private_endpoint" "io_sign_issuer_func" {
+  name                = format("%s-endpoint", module.io_sign_issuer_func.name)
+  location            = azurerm_resource_group.data_rg.location
+  resource_group_name = azurerm_resource_group.data_rg.name
+  subnet_id           = data.azurerm_subnet.private_endpoints_subnet.id
+
+  private_service_connection {
+    name                           = format("%s-endpoint", module.io_sign_issuer_func.name)
+    private_connection_resource_id = module.io_sign_issuer_func.id
+    is_manual_connection           = false
+    subresource_names              = ["sites"]
+  }
+
+  private_dns_zone_group {
+    name                 = "private-dns-zone-group"
+    private_dns_zone_ids = [data.azurerm_private_dns_zone.privatelink_azurewebsites_net.id]
+  }
+
+  tags = var.tags
+}
