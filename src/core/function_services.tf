8bb300e7bccb87c83686f213b6a94780430e3d89
AleDore <alessio85.dore@gmail.com>
2022-12-09 13:45:33 +0100

    upgrade fn_app provider ref

--- src/core/function_services.tf
+++ src/core/function_services.tf
@@ -164,7 +164,7 @@ resource "azurerm_resource_group" "services_rg" {
 #tfsec:ignore:azure-storage-queue-services-logging-enabled:exp:2022-05-01 # already ignored, maybe a bug in tfsec
 module "function_services" {
   count  = var.function_services_count
-  source = "git::https://github.com/pagopa/azurerm.git//function_app?ref=fix-fn-module-container"
+  source = "git::https://github.com/pagopa/azurerm.git//function_app?ref=v3.6.1"
 
   resource_group_name = azurerm_resource_group.services_rg[count.index].name
   name                = format("%s-services-fn-%d", local.project, count.index + 1)
@@ -233,7 +233,7 @@ module "function_services" {
 
 module "function_services_staging_slot" {
   count  = var.function_services_count
-  source = "git::https://github.com/pagopa/azurerm.git//function_app_slot?ref=v3.4.0"
+  source = "git::https://github.com/pagopa/azurerm.git//function_app_slot?ref=v3.6.1"
 
   name                = "staging"
   location            = var.location
