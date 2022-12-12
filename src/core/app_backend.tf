04b8b29ed58a099751fc037d739f86210b357409
gquadrati <greta.quadrati@pagopa.it>
2022-12-12 09:52:37 +0100

    fix format

--- src/core/app_backend.tf
+++ src/core/app_backend.tf
@@ -1072,7 +1072,7 @@ resource "azurerm_monitor_metric_alert" "too_many_http_5xx" {
   }
 
   action {
-    action_group_id = azurerm_monitor_action_group.error_action_group.id
+    action_group_id    = azurerm_monitor_action_group.error_action_group.id
     webhook_properties = null
   }
 
