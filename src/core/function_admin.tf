81a554445a8cb5c20e23d32cf94c4816a71d6caa
Daniele Manni <BurnedMarshal@users.noreply.github.com>
2022-12-14 11:59:42 +0100

    Remove WEBSITE_NODE_DEFAULT_VERSION env
    Co-authored-by: Pasquale De Vita <59291437+pasqualedevita@users.noreply.github.com>

--- src/core/function_admin.tf
+++ src/core/function_admin.tf
@@ -76,7 +76,6 @@ locals {
   function_admin = {
     app_settings_common = {
       FUNCTIONS_WORKER_RUNTIME       = "node"
-      WEBSITE_NODE_DEFAULT_VERSION   = "14.16.0"
       WEBSITE_RUN_FROM_PACKAGE       = "1"
       NODE_ENV                       = "production"
       FUNCTIONS_WORKER_PROCESS_COUNT = 4
