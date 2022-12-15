5bcfbae93abbed04157cc5093b5136b9f0caebca
Gabriele Mendolia <arcogabbo@gmail.com>
2022-12-15 16:32:07 +0100

    [#IOCIT-183] added apim values for cgn merchant (#341)
    Co-authored-by: Pasquale De Vita <59291437+pasqualedevita@users.noreply.github.com>

--- src/core/cgn.tf
+++ src/core/cgn.tf
@@ -282,6 +282,29 @@ module "api_cgn_merchant" {
   xml_content = file("./api/cgn/v1/_base_policy.xml")
 }
 
+# Named Values function-cgn-merchant
+resource "azurerm_api_management_named_value" "io_fn_cgnmerchant_url" {
+  name                = "io-fn-cgnmerchant-url"
+  api_management_name = module.apim.name
+  resource_group_name = module.apim.resource_group_name
+  display_name        = "io-fn-cgnmerchant-url"
+  value               = "https://${module.function_cgn_merchant.default_hostname}"
+}
+
+data "azurerm_key_vault_secret" "io_fn_cgnmerchant_key_secret" {
+  name         = "io-fn-cgnmerchant-KEY-APIM"
+  key_vault_id = data.azurerm_key_vault.common.id
+}
+
+resource "azurerm_api_management_named_value" "io_fn_cgnmerchant_key" {
+  name                = "io-fn-cgnmerchant-key"
+  api_management_name = module.apim.name
+  resource_group_name = module.apim.resource_group_name
+  display_name        = "io-fn-cgnmerchant-key"
+  value               = data.azurerm_key_vault_secret.io_fn_cgnmerchant_key_secret.value
+  secret              = "true"
+}
+
 ## App registration for cgn backend portal ##
 
 /*
