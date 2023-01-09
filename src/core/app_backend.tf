90a26cfde170b6335c78af5f49aea3f147a078f2
gquadrati <greta.quadrati@pagopa.it>
2023-01-09 17:34:44 +0100

    set regex for canary users

--- src/core/app_backend.tf
+++ src/core/app_backend.tf
@@ -139,7 +139,8 @@ locals {
 
       FF_ROUTING_PUSH_NOTIF                        = "BETA" # possible values are: BETA, CANARY, PROD, NONE
       FF_ROUTING_PUSH_NOTIF_BETA_TESTER_SHA_LIST   = data.azurerm_key_vault_secret.app_backend_APP_MESSAGES_BETA_FISCAL_CODES.value
-      FF_ROUTING_PUSH_NOTIF_CANARY_SHA_USERS_REGEX = "XYZ"
+      # ~6,25% of users
+      FF_ROUTING_PUSH_NOTIF_CANARY_SHA_USERS_REGEX = "^([(0-9)|(a-f)|(A-F)]{63}0)$"
 
       FF_PN_ACTIVATION_ENABLED = "1"
 
