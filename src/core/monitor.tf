7c52b21074c0952b7b4ec133f37f31b13849c2c3
gquadrati <greta.quadrati@pagopa.it>
2023-01-13 10:34:43 +0100

    add quarantine action group

--- src/core/monitor.tf
+++ src/core/monitor.tf
@@ -61,6 +61,20 @@ resource "azurerm_monitor_action_group" "error_action_group" {
   tags = var.tags
 }
 
+resource "azurerm_monitor_action_group" "quarantine_error_action_group" {
+  resource_group_name = data.azurerm_resource_group.monitor_rg.name
+  name                = "${var.prefix}${var.env_short}quarantineerror"
+  short_name          = "${var.prefix}${var.env_short}quarantineerror"
+
+  email_receiver {
+    name                    = "slack"
+    email_address           = data.azurerm_key_vault_secret.alert_quarantine_error_notification_slack.value
+    use_common_alert_schema = true
+  }
+
+  tags = var.tags
+}
+
 resource "azurerm_monitor_action_group" "email" {
   name                = "EmailPagoPA"
   resource_group_name = data.azurerm_resource_group.monitor_rg.name
--- src/domains/messages-common/01_monitor.tf
+++ src/domains/messages-common/01_monitor.tf
@@ -44,3 +44,8 @@ data "azurerm_monitor_action_group" "error_action_group" {
   resource_group_name = var.monitor_resource_group_name
   name                = "${var.prefix}${var.env_short}error"
 }
+
+data "azurerm_monitor_action_group" "error_action_group" {
+  resource_group_name = var.monitor_resource_group_name
+  name                = "${var.prefix}${var.env_short}quarantineerror"
+}
