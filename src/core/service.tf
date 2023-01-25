42572d066a8daf20831669b31a0f0cb0cc1e0075
Ivan Diana <iwo.diana@gmail.com>
2023-01-25 14:41:39 +0100

    Update src/core/service.tf
    no need of discriminate between the two datasource
    
    Co-authored-by: Greta Quadrati <75862507+gquadrati@users.noreply.github.com>

--- src/core/service.tf
+++ src/core/service.tf
@@ -7,7 +7,7 @@ resource "azurerm_monitor_scheduled_query_rules_alert" "service_availability_man
     action_group = [azurerm_monitor_action_group.quarantine_error_action_group]
   }
 
-  data_source_id          = var.env_short == "p" ? data.azurerm_key_vault_secret.sec_workspace_id[0].value : data.azurerm_log_analytics_workspace.monitor_rg.id
+  data_source_id          = data.azurerm_log_analytics_workspace.monitor_rg.id
   description             = "Availability for get and manage services is less than or equal to 99%, check ${module.app_gw.name}."
   enabled                 = var.service_alerts_enabled
   auto_mitigation_enabled = true
