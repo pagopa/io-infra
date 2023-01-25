be7453f1590e9afe4520e349e84a860eb7996f1e
pasqualedevita <>
2023-01-25 20:06:15 +0100

    feat: Migrate to azurerm v3

--- src/domains/profile-common/99_main.tf
+++ src/domains/profile-common/99_main.tf
@@ -2,15 +2,15 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "= 2.99.0"
+      version = "<= 3.40.0"
     }
     azuread = {
       source  = "hashicorp/azuread"
-      version = "= 2.21.0"
+      version = "<= 2.33.0"
     }
     null = {
       source  = "hashicorp/null"
-      version = "= 3.1.1"
+      version = "<= 3.2.1"
     }
   }
 
