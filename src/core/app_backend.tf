c2686739371e6dd5c46af6650c9259e39b008f1a
pasqualedevita <>
2023-01-30 23:24:40 +0100

    Increase minimum instances app backend

--- src/core/app_backend.tf
+++ src/core/app_backend.tf
@@ -538,7 +538,7 @@ resource "azurerm_monitor_autoscale_setting" "appservice_app_backendl1" {
 
     capacity {
       default = var.app_backend_autoscale_default
-      minimum = var.app_backend_autoscale_minimum
+      minimum = 2 # var.app_backend_autoscale_minimum
       maximum = var.app_backend_autoscale_maximum
     }
 
