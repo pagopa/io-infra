81867027978a1fb337fc7db8d5eaf7731945445f
pasqualedevita <>
2023-02-08 16:47:20 +0100

    subnet private_endpoint_network_policies_enabled

--- src/core/function_devportal_service_data.tf
+++ src/core/function_devportal_service_data.tf
@@ -258,7 +258,7 @@ module "devportalservicedata_db_server_snet" {
   address_prefixes                          = var.cidr_subnet_devportalservicedata_db_server
   resource_group_name                       = data.azurerm_resource_group.vnet_common_rg.name
   virtual_network_name                      = data.azurerm_virtual_network.vnet_common.name
-  private_endpoint_network_policies_enabled = true
+  private_endpoint_network_policies_enabled = false
   service_endpoints                         = ["Microsoft.Sql"]
 
   delegation = {
--- src/core/vpn.tf
+++ src/core/vpn.tf
@@ -11,7 +11,7 @@ module "vpn_snet" {
   resource_group_name                       = data.azurerm_resource_group.vnet_common_rg.name
   virtual_network_name                      = data.azurerm_virtual_network.vnet_common.name
   service_endpoints                         = []
-  private_endpoint_network_policies_enabled = true
+  private_endpoint_network_policies_enabled = false
 }
 
 module "vpn" {
