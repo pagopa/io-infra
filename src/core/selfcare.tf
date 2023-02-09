66b2335ec7f8c996428bccc381b6633fe549c1c5
pasqualedevita <>
2023-02-09 09:52:05 +0100

    try

--- src/core/selfcare.tf
+++ src/core/selfcare.tf
@@ -115,7 +115,7 @@ data "azurerm_key_vault_secret" "selfcare_subsmigrations_apikey" {
 # JWT
 #tfsec:ignore:azure-keyvault-ensure-secret-expiry:exp:2022-05-01 # already ignored, maybe a bug in tfsec
 module "selfcare_jwt" {
-  source = "git::https://github.com/pagopa/terraform-azurerm-v3.git//jwt_keys?ref=v4.1.10"
+  source = "git::https://github.com/pagopa/terraform-azurerm-v3.git//jwt_keys?ref=fix-jwt-keys"
 
   jwt_name         = "selfcare-jwt"
   key_vault_id     = data.azurerm_key_vault.common.id
