5eb28e6cf653479d1441ac47b9635a13d70e4e55
AleDore <alessio85.dore@gmail.com>
2023-02-17 17:33:56 +0100

    add permission to GH on key vault

--- src/domains/citizen-auth-common/02_key_vault.tf
+++ src/domains/citizen-auth-common/02_key_vault.tf
@@ -75,3 +75,32 @@ resource "azurerm_key_vault_access_policy" "azdevops_platform_iac_policy" {
   storage_permissions     = []
   certificate_permissions = ["SetIssuers", "DeleteIssuers", "Purge", "List", "Get", "ManageContacts", ]
 }
+
+
+data "azuread_service_principal" "github_action_iac_cd" {
+  display_name = "github-pagopa-io-infra-prod-cd"
+}
+
+resource "azurerm_key_vault_access_policy" "github_action_iac_cd_kv" {
+  key_vault_id = module.key_vault.id
+  tenant_id    = data.azurerm_client_config.current.tenant_id
+  object_id    = data.azuread_service_principal.github_action_iac_cd.object_id
+
+  secret_permissions      = ["Get", "List", "Set", ]
+  storage_permissions     = []
+  certificate_permissions = ["SetIssuers", "DeleteIssuers", "Purge", "List", "Get", "ManageContacts", ]
+}
+
+data "azuread_service_principal" "github_action_iac_ci" {
+  display_name = "github-pagopa-io-infra-prod-ci"
+}
+
+resource "azurerm_key_vault_access_policy" "github_action_iac_ci_kv" {
+  key_vault_id = module.key_vault.id
+  tenant_id    = data.azurerm_client_config.current.tenant_id
+  object_id    = data.azuread_service_principal.github_action_iac_ci.object_id
+
+  secret_permissions      = ["Get", "List", ]
+  storage_permissions     = []
+  certificate_permissions = ["Get", "List", ]
+}
