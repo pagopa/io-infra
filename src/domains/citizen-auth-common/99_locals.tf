6387ec0cc595e73c6ffc36a04b522283d721e9f6
fabriziopapi <fabrizio.papi@pagopa.it>
2023-02-20 14:55:10 +0100

    fix lint

--- src/domains/citizen-auth-common/99_locals.tf
+++ src/domains/citizen-auth-common/99_locals.tf
@@ -1,6 +1,6 @@
 locals {
-  project        = "${var.prefix}-${var.env_short}-${var.location_short}-${var.domain}"
-  product        = "${var.prefix}-${var.env_short}"
+  project = "${var.prefix}-${var.env_short}-${var.location_short}-${var.domain}"
+  product = "${var.prefix}-${var.env_short}"
 
   monitor_action_group_slack_name = "SlackPagoPA"
   monitor_action_group_email_name = "EmailPagoPA"
