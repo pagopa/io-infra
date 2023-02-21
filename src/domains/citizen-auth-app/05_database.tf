df9c4a8ab35cffbe8f0f2f012db03aec6b027f76
AleDore <alessio85.dore@gmail.com>
2023-02-21 10:42:28 +0100

    fix cosmos data rg

--- src/domains/citizen-auth-app/05_database.tf
+++ src/domains/citizen-auth-app/05_database.tf
@@ -1,4 +1,4 @@
 data "azurerm_cosmosdb_account" "cosmos_citizen_auth" {
   name                = format("%s-%s-account", local.product, var.domain)
-  resource_group_name = format("%s-rg-internal", local.product)
+  resource_group_name = format("%s-%s-data-rg", local.product, var.domain)
 }
