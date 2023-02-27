df154d1037694400bed96c3d9aa659fba3c3e64d
Greta Quadrati <75862507+gquadrati@users.noreply.github.com>
2023-02-27 15:55:56 +0100

    [#IOCIT-302]  Add jwt certificate v1 (#460)

--- src/domains/citizen-auth-app/02_security.tf
+++ src/domains/citizen-auth-app/02_security.tf
@@ -2,3 +2,46 @@ data "azurerm_key_vault" "kv" {
   name                = "${local.product}-${var.domain}-kv"
   resource_group_name = "${local.product}-${var.domain}-sec-rg"
 }
+
+
+resource "azurerm_key_vault_certificate" "lollipop_certificate_v1" {
+  name         = "lollipop-certificate-v1"
+  key_vault_id = data.azurerm_key_vault.kv.id
+
+  certificate_policy {
+    issuer_parameters {
+      name = "Self"
+    }
+
+    key_properties {
+      exportable = true
+      key_size   = 2048
+      key_type   = "RSA"
+      reuse_key  = false
+    }
+
+    secret_properties {
+      content_type = "application/x-pem-file"
+    }
+
+    x509_certificate_properties {
+      key_usage = [
+        "cRLSign",
+        "dataEncipherment",
+        "digitalSignature",
+        "keyAgreement",
+        "keyCertSign",
+        "keyEncipherment",
+      ]
+
+      subject            = local.lollipop_jwt_host
+      validity_in_months = 1200
+    }
+  }
+}
+
+
+data "azurerm_key_vault_certificate_data" "lollipop_certificate_v1" {
+  name         = resource.azurerm_key_vault_certificate.lollipop_certificate_v1.name
+  key_vault_id = data.azurerm_key_vault.kv.id
+}
