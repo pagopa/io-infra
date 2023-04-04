c3c31c1611d2676b18e653916da93d37962285d0
Pasquale De Vita <59291437+pasqualedevita@users.noreply.github.com>
2023-04-04 18:55:02 +0200

    Update 05_database.tf

--- src/domains/citizen-auth-common/05_database.tf
+++ src/domains/citizen-auth-common/05_database.tf
@@ -92,7 +92,7 @@ resource "azurerm_monitor_metric_alert" "cosmosdb_sql_database_citizen_auth_norm
   # Metric info
   # https://learn.microsoft.com/en-us/azure/azure-monitor/essentials/metrics-supported#microsoftdocumentdbdatabaseaccounts
   criteria {
-    metric_namespace       = "Microsoft.DocumentDB/databaseAccounts/sqlDatabases"
+    metric_namespace       = "microsoft.documentdb/databaseaccounts"
     metric_name            = "NormalizedRUConsumption"
     aggregation            = "Maximum"
     operator               = "GreaterThan"
