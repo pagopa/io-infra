fd68b0f1e1f0dccd83643400094f46d555eff3bd
Garma <64153597+Garma00@users.noreply.github.com>
2023-04-12 16:40:30 +0200

    [ICC-327] Update Node version for fn-assets prod (#528)

--- src/core/function_assets_cdn.tf
+++ src/core/function_assets_cdn.tf
@@ -66,7 +66,7 @@ module "function_assets_cdn" {
 
   os_type                                  = "linux"
   runtime_version                          = "~4"
-  linux_fx_version                         = "NODE|14"
+  linux_fx_version                         = "NODE|18"
   always_on                                = true
   application_insights_instrumentation_key = azurerm_application_insights.application_insights.instrumentation_key
 
