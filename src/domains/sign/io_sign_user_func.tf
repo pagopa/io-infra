08db8f4e0d18fc264075057272d659e2f86b2c4b
Luca Cavallaro <4357400+lucacavallaro@users.noreply.github.com>
2023-04-12 10:36:25 +0200

    fix vars

--- src/domains/sign/io_sign_user_func.tf
+++ src/domains/sign/io_sign_user_func.tf
@@ -17,7 +17,7 @@ locals {
       userValidatedBlobContainerName    = azurerm_storage_container.validated_documents.name
       IoServicesApiBasePath             = "https://api.io.pagopa.it"
       IoServicesSubscriptionKey         = module.key_vault_secrets.values["IoServicesSubscriptionKey"].value
-      PdvTokenizerApiBasePath           = "https://api.uat.tokenizer.pdv.pagopa.it"
+      PdvTokenizerApiBasePath           = "https://api.tokenizer.pdv.pagopa.it"
       PdvTokenizerApiKey                = module.key_vault_secrets.values["PdvTokenizerApiKey"].value
       NamirialApiBasePath               = "https://pagopa.namirial.com"
       NamirialUsername                  = "api"
