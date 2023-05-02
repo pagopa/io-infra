175236ea7e58680ae5d681b4398970247dce4d81
Greta Quadrati <75862507+gquadrati@users.noreply.github.com>
2023-05-02 10:32:11 +0200

    [#IOPID-106] Add alert on PubKey revoke failures (#536)

--- src/domains/citizen-auth-app/01_monitor.tf
+++ src/domains/citizen-auth-app/01_monitor.tf
@@ -17,6 +17,11 @@ data "azurerm_monitor_action_group" "error_action_group" {
   name                = "${var.prefix}${var.env_short}error"
 }
 
+data "azurerm_monitor_action_group" "quarantine_error_action_group" {
+  resource_group_name = var.monitor_resource_group_name
+  name                = "${var.prefix}${var.env_short}quarantineerror"
+}
+
 data "azurerm_monitor_action_group" "slack" {
   resource_group_name = var.monitor_resource_group_name
   name                = local.monitor_action_group_slack_name
