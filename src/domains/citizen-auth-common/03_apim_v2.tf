5c8fd9056bc5030c5816f5373bfb798b8361e59f
AleDore <alessio85.dore@gmail.com>
2023-07-05 11:46:36 +0200

    fix citizen-auth apim v2 duplicate names

--- src/domains/citizen-auth-common/03_apim_v2.tf
+++ src/domains/citizen-auth-common/03_apim_v2.tf
@@ -6,7 +6,7 @@ data "azurerm_api_management" "apim_v2_api" {
 ####################################################################################
 # Lollipop APIM Product
 ####################################################################################
-resource "azurerm_api_management_group" "api_lollipop_assertion_read" {
+resource "azurerm_api_management_group" "api_lollipop_assertion_read_v2" {
   name                = "apilollipopassertionread"
   api_management_name = data.azurerm_api_management.apim_v2_api.name
   resource_group_name = data.azurerm_api_management.apim_v2_api.resource_group_name
@@ -14,7 +14,7 @@ resource "azurerm_api_management_group" "api_lollipop_assertion_read" {
   description         = "A group that enables LC to retrieve user's assertion on a Lollipop flow"
 }
 
-module "apim_product_lollipop" {
+module "apim_v2_product_lollipop" {
   source = "git::https://github.com/pagopa/terraform-azurerm-v3//api_management_product?ref=v4.1.5"
 
   product_id   = "io-lollipop-api"
@@ -31,13 +31,13 @@ module "apim_product_lollipop" {
   policy_xml = file("./api_product/io_lollipop/_base_policy.xml")
 }
 
-module "apim_lollipop_api_v1" {
+module "apim_v2_lollipop_api_v1" {
   source = "git::https://github.com/pagopa/terraform-azurerm-v3//api_management_api?ref=v4.1.5"
 
   name                  = format("%s-lollipop-api", local.product)
   api_management_name   = data.azurerm_api_management.apim_v2_api.name
   resource_group_name   = data.azurerm_api_management.apim_v2_api.resource_group_name
-  product_ids           = [module.apim_product_lollipop.product_id]
+  product_ids           = [module.apim_v2_product_lollipop.product_id]
   subscription_required = true
   service_url           = null
 
@@ -54,7 +54,7 @@ module "apim_lollipop_api_v1" {
 }
 
 # Named Value fn-lollipop
-resource "azurerm_api_management_named_value" "io_fn_weu_lollipop_url" {
+resource "azurerm_api_management_named_value" "io_fn_weu_lollipop_url_v2" {
   name                = "io-fn-weu-lollipop-url"
   api_management_name = data.azurerm_api_management.apim_v2_api.name
   resource_group_name = data.azurerm_api_management.apim_v2_api.resource_group_name
@@ -62,24 +62,24 @@ resource "azurerm_api_management_named_value" "io_fn_weu_lollipop_url" {
   value               = "https://io-p-weu-lollipop-fn.azurewebsites.net"
 }
 
-data "azurerm_key_vault_secret" "io_fn_weu_lollipop_key_secret" {
+data "azurerm_key_vault_secret" "io_fn_weu_lollipop_key_secret_v2" {
   name         = "io-fn-weu-lollipop-KEY-APIM"
   key_vault_id = module.key_vault.id
 }
 
-resource "azurerm_api_management_named_value" "io_fn_weu_lollipop_key" {
+resource "azurerm_api_management_named_value" "io_fn_weu_lollipop_key_v2" {
   name                = "io-fn-weu-lollipop-key"
   api_management_name = data.azurerm_api_management.apim_v2_api.name
   resource_group_name = data.azurerm_api_management.apim_v2_api.resource_group_name
   display_name        = "io-fn-weu-lollipop-key"
-  value               = data.azurerm_key_vault_secret.io_fn_weu_lollipop_key_secret.value
+  value               = data.azurerm_key_vault_secret.io_fn_weu_lollipop_key_secret_v2.value
   secret              = "true"
 }
 
 ####################################################################################
 # PagoPA General Lollipop User
 ####################################################################################
-resource "azurerm_api_management_user" "pagopa_user" {
+resource "azurerm_api_management_user" "pagopa_user_v2" {
   user_id             = "iolollipoppagopauser"
   api_management_name = data.azurerm_api_management.apim_v2_api.name
   resource_group_name = data.azurerm_api_management.apim_v2_api.resource_group_name
@@ -89,27 +89,27 @@ resource "azurerm_api_management_user" "pagopa_user" {
   state               = "active"
 }
 
-resource "azurerm_api_management_group_user" "pagopa_group" {
-  user_id             = azurerm_api_management_user.pagopa_user.user_id
+resource "azurerm_api_management_group_user" "pagopa_group_v2" {
+  user_id             = azurerm_api_management_user.pagopa_user_v2.user_id
   api_management_name = data.azurerm_api_management.apim_v2_api.name
   resource_group_name = data.azurerm_api_management.apim_v2_api.resource_group_name
-  group_name          = azurerm_api_management_group.api_lollipop_assertion_read.name
+  group_name          = azurerm_api_management_group.api_lollipop_assertion_read_v2.name
 }
 
-resource "azurerm_api_management_subscription" "pagopa" {
-  user_id             = azurerm_api_management_user.pagopa_user.id
+resource "azurerm_api_management_subscription" "pagopa_v2" {
+  user_id             = azurerm_api_management_user.pagopa_user_v2.id
   api_management_name = data.azurerm_api_management.apim_v2_api.name
   resource_group_name = data.azurerm_api_management.apim_v2_api.resource_group_name
-  product_id          = module.apim_product_lollipop.id
+  product_id          = module.apim_v2_product_lollipop.id
   display_name        = "Lollipop API"
   state               = "active"
 }
 
-resource "azurerm_api_management_subscription" "pagopa_fastlogin" {
-  user_id             = azurerm_api_management_user.pagopa_user.id
+resource "azurerm_api_management_subscription" "pagopa_fastlogin_v2" {
+  user_id             = azurerm_api_management_user.pagopa_user_v2.id
   api_management_name = data.azurerm_api_management.apim_v2_api.name
   resource_group_name = data.azurerm_api_management.apim_v2_api.resource_group_name
-  product_id          = module.apim_product_lollipop.id
+  product_id          = module.apim_v2_product_lollipop.id
   display_name        = "Fast Login LC"
   state               = "active"
 }
@@ -117,9 +117,9 @@ resource "azurerm_api_management_subscription" "pagopa_fastlogin" {
 ####################################################################################
 # PagoPA General Lollipop Secret
 ####################################################################################
-resource "azurerm_key_vault_secret" "first_lollipop_consumer_subscription_key" {
+resource "azurerm_key_vault_secret" "first_lollipop_consumer_subscription_key_v2" {
   name         = "first-lollipop-consumer-pagopa-subscription-key"
-  value        = azurerm_api_management_subscription.pagopa.primary_key
+  value        = azurerm_api_management_subscription.pagopa_v2.primary_key
   key_vault_id = module.key_vault.id
 }
 
@@ -128,8 +128,8 @@ resource "azurerm_key_vault_secret" "first_lollipop_consumer_subscription_key" {
 ####################################################################################
 
 # subscription key used for assertion retrieval
-resource "azurerm_key_vault_secret" "fast_login_subscription_key" {
+resource "azurerm_key_vault_secret" "fast_login_subscription_key_v2" {
   name         = "fast-login-subscription-key"
-  value        = azurerm_api_management_subscription.pagopa_fastlogin.primary_key
+  value        = azurerm_api_management_subscription.pagopa_fastlogin_v2.primary_key
   key_vault_id = module.key_vault.id
 }
