9ace452f4cb8785554dc4b6bbbe0127dc8bc1a67
Gabriele Mendolia <arcogabbo@gmail.com>
2023-07-13 09:51:09 +0200

    [#IOPID-384] add required env variables for function_app (#614)
    This PR also updated continua http_status to reflect current value on azure

--- src/core/function_app.tf
+++ src/core/function_app.tf
@@ -108,6 +108,12 @@ locals {
 
       VISIBLE_SERVICE_BLOB_ID = "visible-services-national.json"
 
+      # Login Email variables
+      # TODO: change those variables once the service has been created
+      MAGIC_LINK_SERVICE_PUBLIC_URL = "https://example.com"
+      HELP_DESK_REF                 = "#"
+      #
+
       MAILUP_USERNAME      = data.azurerm_key_vault_secret.common_MAILUP_USERNAME.value
       MAILUP_SECRET        = data.azurerm_key_vault_secret.common_MAILUP_SECRET.value
       PUBLIC_API_KEY       = trimspace(data.azurerm_key_vault_secret.fn_app_PUBLIC_API_KEY.value)
--- src/core/monitor.tf
+++ src/core/monitor.tf
@@ -313,7 +313,7 @@ locals {
       name                              = trimsuffix(azurerm_dns_a_record.continua_io_pagopa_it.fqdn, "."),
       host                              = trimsuffix(azurerm_dns_a_record.continua_io_pagopa_it.fqdn, "."),
       path                              = "",
-      http_status                       = 200,
+      http_status                       = 302,
       ssl_cert_remaining_lifetime_check = 7,
     },
   ]
