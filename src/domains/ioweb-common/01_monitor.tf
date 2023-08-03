1d26cf7745094a6245aae67dcdbb425daf520462
gquadrati <greta.quadrati@pagopa.it>
2023-08-03 15:41:58 +0200

    add new ioweb domain

---
+++ src/domains/ioweb-common/01_monitor.tf
@@ -0,0 +1,51 @@
+data "azurerm_log_analytics_workspace" "log_analytics" {
+  name                = var.log_analytics_workspace_name
+  resource_group_name = var.log_analytics_workspace_resource_group_name
+}
+
+data "azurerm_application_insights" "application_insights" {
+  name                = var.application_insights_name
+  resource_group_name = var.monitor_resource_group_name
+}
+
+data "azurerm_resource_group" "monitor_rg" {
+  name = var.monitor_resource_group_name
+}
+
+data "azurerm_monitor_action_group" "slack" {
+  resource_group_name = var.monitor_resource_group_name
+  name                = local.monitor_action_group_slack_name
+}
+
+data "azurerm_monitor_action_group" "email" {
+  resource_group_name = var.monitor_resource_group_name
+  name                = local.monitor_action_group_email_name
+}
+
+data "azurerm_monitor_action_group" "error_action_group" {
+  resource_group_name = var.monitor_resource_group_name
+  name                = "${var.prefix}${var.env_short}error"
+}
+
+data "azurerm_monitor_action_group" "quarantine_error_action_group" {
+  resource_group_name = var.monitor_resource_group_name
+  name                = "${var.prefix}${var.env_short}quarantineerror"
+}
+
+#tfsec:ignore:AZU023
+resource "azurerm_key_vault_secret" "appinsights_instrumentation_key" {
+  name         = "appinsights-instrumentation-key"
+  value        = data.azurerm_application_insights.application_insights.instrumentation_key
+  content_type = "only instrumentation key"
+
+  key_vault_id = module.key_vault.id
+}
+
+#tfsec:ignore:AZU023
+resource "azurerm_key_vault_secret" "appinsights_connection_string" {
+  name         = "appinsights-connection-string"
+  value        = data.azurerm_application_insights.application_insights.connection_string
+  content_type = "full connection string, example InstrumentationKey=XXXXX"
+
+  key_vault_id = module.key_vault.id
+}
---
+++ src/domains/ioweb-common/02_security.tf
@@ -0,0 +1,44 @@
+resource "azurerm_resource_group" "sec_rg" {
+  name     = "${local.product}-${var.domain}-sec-rg"
+  location = var.location
+
+  tags = var.tags
+}
+
+module "key_vault" {
+  source = "git::https://github.com/pagopa/terraform-azurerm-v3.git//key_vault?ref=v4.1.3"
+
+  name                       = "${local.product}-${var.domain}-kv"
+  location                   = azurerm_resource_group.sec_rg.location
+  resource_group_name        = azurerm_resource_group.sec_rg.name
+  tenant_id                  = data.azurerm_client_config.current.tenant_id
+  soft_delete_retention_days = 90
+
+  tags = var.tags
+}
+
+## adgroup_admin group policy ##
+resource "azurerm_key_vault_access_policy" "adgroup_admin" {
+  key_vault_id = module.key_vault.id
+
+  tenant_id = data.azurerm_client_config.current.tenant_id
+  object_id = data.azuread_group.adgroup_admin.object_id
+
+  key_permissions         = ["Get", "List", "Update", "Create", "Import", "Delete", ]
+  secret_permissions      = ["Get", "List", "Set", "Delete", "Restore", "Recover", ]
+  storage_permissions     = []
+  certificate_permissions = ["Get", "List", "Update", "Create", "Import", "Delete", "Restore", "Recover", ]
+}
+
+## adgroup_developers group policy ##
+resource "azurerm_key_vault_access_policy" "adgroup_developers" {
+  key_vault_id = module.key_vault.id
+
+  tenant_id = data.azurerm_client_config.current.tenant_id
+  object_id = data.azuread_group.adgroup_developers.object_id
+
+  key_permissions         = ["Get", "List", "Update", "Create", "Import", "Delete", ]
+  secret_permissions      = ["Get", "List", "Set", "Delete", "Restore", "Recover", ]
+  storage_permissions     = []
+  certificate_permissions = ["Get", "List", "Update", "Create", "Import", "Delete", "Restore", "Recover", ]
+}
