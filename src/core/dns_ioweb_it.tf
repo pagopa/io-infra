9475cbbbcb93bdaef368d8f7006526893ad9de22
Greta Quadrati <75862507+gquadrati@users.noreply.github.com>
2023-08-04 15:46:32 +0200

    [#IOPID-598]  Add `ioweb-common` domain (#643)

--- src/core/dns_ioweb_it.tf
+++ src/core/dns_ioweb_it.tf
@@ -25,29 +25,3 @@ resource "azurerm_dns_caa_record" "ioweb_it" {
 
   tags = var.tags
 }
-
-
-# application gateway records
-
-# api.ioapp.it (api)
-resource "azurerm_dns_a_record" "api_ioweb_it" {
-  name                = "api"
-  zone_name           = azurerm_dns_zone.ioweb_it.name
-  resource_group_name = azurerm_resource_group.rg_external.name
-  ttl                 = var.dns_default_ttl_sec
-  records             = [azurerm_public_ip.appgateway_public_ip.ip_address]
-
-  tags = var.tags
-}
-
-
-# auth.ioapp.it (hub-spid-login)
-resource "azurerm_dns_a_record" "app_backend_ioweb_it" {
-  name                = "auth"
-  zone_name           = azurerm_dns_zone.ioweb_it.name
-  resource_group_name = azurerm_resource_group.rg_external.name
-  ttl                 = var.dns_default_ttl_sec
-  records             = [azurerm_public_ip.appgateway_public_ip.ip_address]
-
-  tags = var.tags
-}
---
+++ src/domains/ioweb-common/env/prod/terraform.tfvars
@@ -0,0 +1,22 @@
+prefix         = "io"
+env_short      = "p"
+env            = "prod"
+domain         = "ioweb"
+location       = "westeurope"
+location_short = "weu"
+instance       = "common"
+
+tags = {
+  CreatedBy   = "Terraform"
+  Environment = "Prod"
+  Owner       = "IO"
+  Source      = "https://github.com/pagopa/io-infra/tree/main/src/domains/ioweb-common"
+  CostCenter  = "TS310 - PAGAMENTI & SERVIZI"
+}
+
+### External resources
+
+monitor_resource_group_name                 = "io-p-rg-common"
+log_analytics_workspace_name                = "io-p-law-common"
+log_analytics_workspace_resource_group_name = "io-p-rg-common"
+application_insights_name                   = "io-p-ai-common"
