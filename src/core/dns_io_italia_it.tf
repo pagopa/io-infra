1fb5e8d1f8d7aa6a1f7f36481e45e07cd194082a
pasqualedevita <>
2023-08-07 18:25:54 +0200

    switch dns to api management v2

--- src/core/dns_io_italia_it.tf
+++ src/core/dns_io_italia_it.tf
@@ -72,7 +72,7 @@ resource "azurerm_dns_a_record" "api_internal_io_italia_it" {
   zone_name           = azurerm_dns_zone.io_italia_it.name
   resource_group_name = azurerm_resource_group.rg_external.name
   ttl                 = "60" # var.dns_default_ttl_sec # TODO rollback after apim-v2 migration
-  records             = module.apim.*.private_ip_addresses[0]
+  records             = module.apim_v2.*.private_ip_addresses[0]
 
   tags = var.tags
 }
