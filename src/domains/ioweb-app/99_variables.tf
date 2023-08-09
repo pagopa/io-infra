f22645966667eb0dff73cda5b81a3992661578ea
Greta Quadrati <75862507+gquadrati@users.noreply.github.com>
2023-08-09 11:19:36 +0200

    [#IOPID-598] Add `ioweb-app` domain (#644)
    Co-authored-by: Gabriele Mendolia <arcogabbo@gmail.com>

---
+++ src/domains/ioweb-app/99_variables.tf
@@ -0,0 +1,125 @@
+# general
+
+variable "prefix" {
+  type = string
+  validation {
+    condition = (
+      length(var.prefix) < 6
+    )
+    error_message = "Max length is 6 chars."
+  }
+}
+
+variable "env" {
+  type = string
+}
+
+variable "env_short" {
+  type = string
+  validation {
+    condition = (
+      length(var.env_short) == 1
+    )
+    error_message = "Length must be 1 chars."
+  }
+}
+
+variable "domain" {
+  type = string
+  validation {
+    condition = (
+      length(var.domain) <= 12
+    )
+    error_message = "Max length is 12 chars."
+  }
+}
+
+variable "location" {
+  type        = string
+  description = "One of westeurope, northeurope"
+}
+
+variable "location_short" {
+  type = string
+  validation {
+    condition = (
+      length(var.location_short) == 3
+    )
+    error_message = "Length must be 3 chars."
+  }
+  description = "One of weu, neu"
+}
+
+variable "location_string" {
+  type        = string
+  description = "One of West Europe, North Europe"
+}
+
+variable "instance" {
+  type        = string
+  description = "One of beta, prod01, prod02"
+}
+
+variable "lock_enable" {
+  type        = bool
+  default     = false
+  description = "Apply locks to block accedentaly deletions."
+}
+
+variable "tags" {
+  type = map(any)
+  default = {
+    CreatedBy = "Terraform"
+  }
+}
+
+### External resources
+
+variable "monitor_resource_group_name" {
+  type        = string
+  description = "Monitor resource group name"
+}
+
+variable "log_analytics_workspace_name" {
+  type        = string
+  description = "Specifies the name of the Log Analytics Workspace."
+}
+
+variable "log_analytics_workspace_resource_group_name" {
+  type        = string
+  description = "The name of the resource group in which the Log Analytics workspace is located in."
+}
+
+variable "application_insights_name" {
+  type        = string
+  description = "Specifies the name of the Application Insights."
+}
+
+### Aks
+
+variable "k8s_kube_config_path_prefix" {
+  type    = string
+  default = "~/.kube"
+}
+
+variable "ingress_load_balancer_ip" {
+  type = string
+}
+
+variable "reloader_helm" {
+  type = object({
+    chart_version = string,
+    image_name    = string,
+    image_tag     = string
+  })
+  description = "reloader helm chart configuration"
+}
+
+variable "tls_cert_check_helm" {
+  type = object({
+    chart_version = string,
+    image_name    = string,
+    image_tag     = string
+  })
+  description = "tls cert helm chart configuration"
+}
---
+++ src/domains/ioweb-app/env/weu-prod01/terraform.tfvars
@@ -0,0 +1,44 @@
+prefix          = "io"
+env_short       = "p"
+env             = "prod"
+domain          = "ioweb"
+location        = "westeurope"
+location_short  = "weu"
+location_string = "West Europe"
+instance        = "prod01"
+
+tags = {
+  CreatedBy   = "Terraform"
+  Environment = "Prod"
+  Owner       = "IO"
+  Source      = "https://github.com/pagopa/io-infra/tree/main/src/domains/ioweb-app"
+  CostCenter  = "TS310 - PAGAMENTI & SERVIZI"
+}
+
+### External resources
+
+monitor_resource_group_name                 = "io-p-rg-common"
+log_analytics_workspace_name                = "io-p-law-common"
+log_analytics_workspace_resource_group_name = "io-p-rg-common"
+application_insights_name                   = "io-p-ai-common"
+
+### External tools
+
+# chart releases: https://github.com/stakater/Reloader/releases
+# image tags: https://hub.docker.com/r/stakater/reloader/tags
+reloader_helm = {
+  chart_version = "v0.0.118"
+  image_name    = "stakater/reloader"
+  image_tag     = "v0.0.118@sha256:2d423cab8d0e83d1428ebc70c5c5cafc44bd92a597bff94007f93cddaa607b02"
+}
+# chart releases: https://github.com/pagopa/aks-microservice-chart-blueprint/releases
+# image tags: https://github.com/pagopa/infra-ssl-check/releases
+tls_cert_check_helm = {
+  chart_version = "1.21.0"
+  image_name    = "ghcr.io/pagopa/infra-ssl-check"
+  image_tag     = "v1.3.4@sha256:c3d45736706c981493b6216451fc65e99a69d5d64409ccb1c4ca93fef57c921d"
+}
+
+### Aks
+
+ingress_load_balancer_ip = "10.11.100.250"
