f74be21316fb60e3a5ba7bf2e6da733b592c34af
arcogabbo <arcogabbo@gmail.com>
2023-08-28 11:51:31 +0200

    [#IOPID-583] created cdn

---
+++ src/domains/ioweb-common/06_cdn.tf
@@ -0,0 +1,56 @@
+# core domain external rg
+data "azurerm_resource_group" "core_ext" {
+  name = format("%s-rg-external", local.product)
+}
+
+data "azurerm_dns_zone" "ioapp_it" {
+  name                = "ioapp.it"
+  resource_group_name = data.azurerm_resource_group.core_ext.name
+}
+
+module "landing_cdn" {
+  source = "git::https://github.com/pagopa/terraform-azurerm-v3.git//cdn?ref=v7.2.1"
+
+  name                  = "portal"
+  prefix                = local.project
+  resource_group_name   = azurerm_resource_group.common_rg.name
+  location              = azurerm_resource_group.common_rg.location
+  hostname              = "ioapp.it"
+  https_rewrite_enabled = true
+
+  index_document     = "index.html"
+  error_404_document = "404.html"
+
+  dns_zone_name                = data.azurerm_dns_zone.ioapp_it.name
+  dns_zone_resource_group_name = data.azurerm_resource_group.core_ext.name
+
+  # keyvault where the certificate is stored
+  keyvault_vault_name          = format("%s-profile-kv", local.product)
+  keyvault_resource_group_name = format("%s-profile-sec-rg", local.product)
+  keyvault_subscription_id     = data.azurerm_subscription.current.id
+
+  querystring_caching_behaviour = "BypassCaching"
+
+  global_delivery_rule = {
+    cache_expiration_action       = []
+    cache_key_query_string_action = []
+    modify_request_header_action  = []
+
+    # HSTS
+    modify_response_header_action = [
+      {
+        action = "Overwrite"
+        name   = "Strict-Transport-Security"
+        value  = "max-age=31536000"
+      },
+      # Content-Security-Policy (in Report mode)
+      {
+        action = "Append"
+        name   = "Content-Security-Policy"
+        value  = "script-src 'self' 'unsafe-inline'; script-src-elem 'self' 'unsafe-inline' https://cdn.matomo.cloud/pagopa.matomo.cloud/ https://pagopa.matomo.cloud/ https://recaptcha.net/ https://www.recaptcha.net/recaptcha/ https://www.gstatic.com/recaptcha/; style-src 'self' 'unsafe-inline' recaptcha.net; worker-src 'none'; font-src data: 'self'; img-src data: 'self' recaptcha.net; object-src 'none'; "
+      }
+    ]
+  }
+
+  tags = var.tags
+}
