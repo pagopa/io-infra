3feb47014cc37548bf8fe687b51ea4dcfa0d3737
Luca Cavallaro <4357400+lucacavallaro@users.noreply.github.com>
2023-09-05 12:48:24 +0200

    add new collections

--- src/domains/sign/cosmos_backoffice.tf
+++ src/domains/sign/cosmos_backoffice.tf
@@ -19,3 +19,33 @@ module "cosmosdb_sql_container_backoffice-api-keys" {
 
   default_ttl = var.io_sign_database_backoffice.api_keys.ttl
 }
+
+module "cosmosdb_sql_container_backoffice-issuers" {
+  source              = "git::https://github.com/pagopa/terraform-azurerm-v3.git//cosmosdb_sql_container?ref=v6.20.2"
+  name                = "issuers"
+  resource_group_name = azurerm_resource_group.data_rg.name
+  account_name        = module.cosmosdb_account.name
+  database_name       = module.cosmosdb_sql_database_backoffice.name
+  partition_key_path  = "/institutionId"
+
+  autoscale_settings = {
+    max_throughput = var.io_sign_database_backoffice.issuers.max_throughput
+  }
+
+  default_ttl = var.io_sign_database_backoffice.issuers.ttl
+}
+
+module "cosmosdb_sql_container_backoffice-consents" {
+  source              = "git::https://github.com/pagopa/terraform-azurerm-v3.git//cosmosdb_sql_container?ref=v6.20.2"
+  name                = "consents"
+  resource_group_name = azurerm_resource_group.data_rg.name
+  account_name        = module.cosmosdb_account.name
+  database_name       = module.cosmosdb_sql_database_backoffice.name
+  partition_key_path  = "/institutionId"
+
+  autoscale_settings = {
+    max_throughput = var.io_sign_database_backoffice.consents.max_throughput
+  }
+
+  default_ttl = var.io_sign_database_backoffice.consents.ttl
+}
