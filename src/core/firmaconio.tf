57760b0f32520bc2549b0752e39504cb42deba4a
Luca Cavallaro <4357400+lucacavallaro@users.noreply.github.com>
2023-09-06 15:00:55 +0200

    add role assigment

--- src/core/firmaconio.tf
+++ src/core/firmaconio.tf
@@ -12,3 +12,9 @@ data "azurerm_linux_web_app" "firmaconio_selfcare_web_app" {
   name                = format("%s-backoffice-app", local.firmaconio_project)
   resource_group_name = local.firmaconio.resource_group_names.backend
 }
+
+resource "azurerm_role_assignment" "firmaconio_selfcare_apim_contributor_role" {
+  scope                = module.apim.id
+  role_definition_name = "Contributor"
+  principal_id         = data.azurerm_linux_web_app.firmaconio_selfcare_web_app.identity.principal_id
+}
