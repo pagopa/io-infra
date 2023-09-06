ec96a8be52419fbf05c4442e3bd8fa7d9b768e74
Luca Cavallaro <4357400+lucacavallaro@users.noreply.github.com>
2023-09-06 14:32:00 +0200

    fmt

--- src/domains/sign/io_sign_backoffice_app.tf
+++ src/domains/sign/io_sign_backoffice_app.tf
@@ -3,7 +3,7 @@ locals {
     AZURE_SUBSCRIPTION_ID          = data.azurerm_subscription.current.subscription_id
     COSMOS_DB_CONNECTION_STRING    = module.cosmosdb_account.connection_strings[0],
     COSMOS_DB_NAME                 = module.cosmosdb_sql_database_backoffice.name
-    COSMOS_CONTAINER_NAME       = module.cosmosdb_sql_container_backoffice-api-keys.name
+    COSMOS_CONTAINER_NAME          = module.cosmosdb_sql_container_backoffice-api-keys.name
     APIM_RESOURCE_GROUP_NAME       = data.azurerm_api_management.apim_api.resource_group_name,
     APIM_SERVICE_NAME              = data.azurerm_api_management.apim_api.name,
     APIM_PRODUCT_NAME              = module.apim_io_sign_product.product_id
