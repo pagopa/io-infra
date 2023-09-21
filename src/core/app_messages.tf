4ce1e79b9229229598265ba0bc4fd82b80da59a4
Michael Disaro <michael.disaro@gmail.com>
2023-09-21 17:37:51 +0200

    moved to canary users, taking 0.4% of fiscal codes in production

--- src/core/app_messages.tf
+++ src/core/app_messages.tf
@@ -39,9 +39,10 @@ locals {
 
       // View Features Flag
       USE_FALLBACK          = false
-      FF_TYPE               = "beta"
+      FF_TYPE               = "canary"
       FF_BETA_TESTER_LIST   = data.azurerm_key_vault_secret.fn_messages_APP_MESSAGES_BETA_FISCAL_CODES.value
-      FF_CANARY_USERS_REGEX = "XYZ"
+      # Takes ~0,4% of users
+      FF_CANARY_USERS_REGEX = "^([(0-9)|(a-f)|(A-F)]{62}00)$"
 
     }
     app_settings_1 = {
