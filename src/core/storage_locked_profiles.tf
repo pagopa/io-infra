c2222594cfa8485582fc8f9aa8b4be8a05c450dd
gquadrati <greta.quadrati@pagopa.it>
2023-09-23 19:08:57 +0200

    fix

--- src/core/storage_locked_profiles.tf
+++ src/core/storage_locked_profiles.tf
@@ -26,7 +26,7 @@ resource "azurerm_private_endpoint" "locked_profiles_storage_table" {
   name                = "${module.locked_profiles_storage.name}-table-endpoint"
   location            = azurerm_resource_group.rg_internal.location
   resource_group_name = azurerm_resource_group.rg_internal.location
-  subnet_id           = var.internal_storage.private_endpoint_subnet_id
+  subnet_id           =  module.private_endpoints_subnet.id
 
   private_service_connection {
     name                           = "${module.locked_profiles_storage.name}-table"
