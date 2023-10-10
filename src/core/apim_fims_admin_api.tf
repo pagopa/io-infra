0136ddb4abe8f4f4d23811717e7ef279f90d4f96
Andrea Grillo <andrea.grillo@pagopa.it>
2023-10-10 10:41:48 +0200

    remove named values, useless policy and unexisting keyvault key

--- src/core/apim_fims_admin_api.tf
+++ src/core/apim_fims_admin_api.tf
@@ -13,29 +13,6 @@ module "apim_product_fims_admin" {
   policy_xml = file("./api_product/fims/_base_policy.xml")
 }
 
-# Named Value fn3-admin
-resource "azurerm_api_management_named_value" "fims_admin_url" {
-  name                = "fims-admin-url"
-  api_management_name = module.apim.name
-  resource_group_name = module.apim.resource_group_name
-  display_name        = "fims-admin-url"
-  value               = "https://io-p-citizen-auth-weu-prod01-app-fims.azurewebsites.net"
-}
-
-data "azurerm_key_vault_secret" "fims_admin_key_secret" {
-  name         = "fimsadmin-KEY-APIM"
-  key_vault_id = module.key_vault_common.id
-}
-
-resource "azurerm_api_management_named_value" "fims_admin_key" {
-  name                = "fims-admin-key"
-  api_management_name = module.apim.name
-  resource_group_name = module.apim.resource_group_name
-  display_name        = "fims-admin-key"
-  value               = data.azurerm_key_vault_secret.fims_admin_key_secret.value
-  secret              = "true"
-}
-
 module "api_fims_admin" {
   source = "git::https://github.com/pagopa/terraform-azurerm-v3.git//api_management_api?ref=v4.1.15"
 
@@ -50,7 +27,7 @@ module "api_fims_admin" {
   protocols   = ["https"]
   product_ids = [module.apim_product_fims_admin.product_id]
 
-  service_url = null
+  service_url = module.appservice_fims.domain
 
   subscription_required = true
 
--- src/core/apim_fims_public_api.tf
+++ src/core/apim_fims_public_api.tf
@@ -12,30 +12,6 @@ module "apim_product_fims_public" {
 
   policy_xml = file("./api_product/fims/_base_policy.xml")
 }
-
-# Named Value io_fn3_public_url
-resource "azurerm_api_management_named_value" "fims_public_url" {
-  name                = "fims-public-url"
-  api_management_name = module.apim.name
-  resource_group_name = module.apim.resource_group_name
-  display_name        = "fims-public-url"
-  value               = "https://io-p-citizen-auth-weu-prod01-app-fims.azurewebsites.net"
-}
-
-data "azurerm_key_vault_secret" "fims_public_key_secret" {
-  name         = "fimspublic-KEY-APIM"
-  key_vault_id = module.key_vault_common.id
-}
-
-resource "azurerm_api_management_named_value" "fims_public_key" {
-  name                = "fims-public-key"
-  api_management_name = module.apim.name
-  resource_group_name = module.apim.resource_group_name
-  display_name        = "fims-public-key"
-  value               = data.azurerm_key_vault_secret.fims_public_key_secret.value
-  secret              = "true"
-}
-
 module "api_fims_public" {
   source = "git::https://github.com/pagopa/terraform-azurerm-v3.git//api_management_api?ref=v4.1.15"
 
@@ -50,7 +26,7 @@ module "api_fims_public" {
   protocols   = ["https"]
   product_ids = [module.apim_product_fims_public.product_id]
 
-  service_url = null
+  service_url = module.appservice_fims.domain
 
   subscription_required = false
 
