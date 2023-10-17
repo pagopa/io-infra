99851011a9bcbe387c83dc0ea5aac8d33c5dc2ad
Luca Cavallaro <4357400+lucacavallaro@users.noreply.github.com>
2023-10-17 16:30:57 +0200

    move to network

--- src/domains/sign/io_sign_backoffice_func.tf
+++ src/domains/sign/io_sign_backoffice_func.tf
@@ -105,24 +105,3 @@ module "io_sign_backoffice_func_staging_slot" {
 
   tags = var.tags
 }
-
-resource "azurerm_private_endpoint" "io_sign_backoffice_func_staging_slot" {
-  name                = format("%s-backoffice-func-staging-endpoint", local.project)
-  location            = azurerm_resource_group.data_rg.location
-  resource_group_name = azurerm_resource_group.data_rg.name
-  subnet_id           = data.azurerm_subnet.private_endpoints_subnet.id
-
-  private_service_connection {
-    name                           = format("%s-backoffice-func-staging-endpoint", local.project)
-    private_connection_resource_id = module.io_sign_backoffice_func.id
-    is_manual_connection           = false
-    subresource_names              = ["sites-staging"]
-  }
-
-  private_dns_zone_group {
-    name                 = "private-dns-zone-group"
-    private_dns_zone_ids = [data.azurerm_private_dns_zone.privatelink_azurewebsites_net.id]
-  }
-
-  tags = var.tags
-}
