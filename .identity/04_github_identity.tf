20c1ffa6355ea1e75d3401fb089072821e6bb5ec
Andrea Grillo <andrea.grillo@pagopa.it>
2024-01-03 10:34:44 +0100

    fix wrong merge

--- .identity/04_github_identity.tf
+++ .identity/04_github_identity.tf
@@ -29,7 +29,7 @@ module "identity_ci" {
 }
 
 module "identity_cd" {
-  source = "github.com/pagopa/terraform-azurerm-v3//github_federated_identity?ref=v7.28.0"
+  source = "github.com/pagopa/terraform-azurerm-v3//github_federated_identity?ref=v7.35.0"
 
   prefix    = var.prefix
   env_short = var.env_short
