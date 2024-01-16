fa6ea2d2db7b70cf1215aafbcea4d033b4ff3483
Andrea Grillo <andrea.grillo@pagopa.it>
2024-01-16 13:30:59 +0100

    [Ec-88] Upgrade io-sign TF modules (#835)

--- src/domains/sign/io_sign_backoffice_app.tf
+++ src/domains/sign/io_sign_backoffice_app.tf
@@ -15,7 +15,7 @@ locals {
 }
 
 module "io_sign_backoffice_snet" {
-  source               = "git::https://github.com/pagopa/terraform-azurerm-v3.git//subnet?ref=v6.20.2"
+  source               = "github.com/pagopa/terraform-azurerm-v3.git//subnet?ref=v7.46.0"
   name                 = format("%s-backoffice-snet", local.project)
   resource_group_name  = data.azurerm_virtual_network.vnet_common.resource_group_name
   virtual_network_name = data.azurerm_virtual_network.vnet_common.name
@@ -44,7 +44,7 @@ data "azurerm_subnet" "appgateway_snet" {
 }
 
 module "io_sign_backoffice_app" {
-  source = "git::https://github.com/pagopa/terraform-azurerm-v3.git//app_service?ref=v6.20.2"
+  source = "github.com/pagopa/terraform-azurerm-v3.git//app_service?ref=v7.46.0"
 
   name                = format("%s-backoffice-app", local.project)
   location            = azurerm_resource_group.backend_rg.location
@@ -116,7 +116,7 @@ resource "azurerm_private_endpoint" "io_sign_backoffice_app" {
 }
 
 module "io_sign_backoffice_app_staging_slot" {
-  source = "git::https://github.com/pagopa/terraform-azurerm-v3.git//app_service_slot?ref=v7.7.0"
+  source = "github.com/pagopa/terraform-azurerm-v3.git//app_service_slot?ref=v7.46.0"
 
   name                = "staging"
   location            = azurerm_resource_group.backend_rg.location
--- src/domains/sign/io_sign_issuer_func.tf
+++ src/domains/sign/io_sign_issuer_func.tf
@@ -34,7 +34,7 @@ locals {
 }
 
 module "io_sign_issuer_func" {
-  source = "git::https://github.com/pagopa/terraform-azurerm-v3.git//function_app?ref=v6.2.1"
+  source = "github.com/pagopa/terraform-azurerm-v3.git//function_app?ref=v7.46.0"
 
   name                = format("%s-issuer-func", local.project)
   location            = azurerm_resource_group.backend_rg.location
@@ -50,6 +50,8 @@ module "io_sign_issuer_func" {
     kind                         = "Linux"
     sku_size                     = var.io_sign_issuer_func.sku_size
     maximum_elastic_worker_count = 0
+    worker_count                 = 1
+    zone_balancing_enabled       = false
   }
 
   app_settings = merge(
@@ -61,7 +63,7 @@ module "io_sign_issuer_func" {
     }
   )
 
-  sticky_settings = [
+  sticky_app_setting_names = [
     # Sticky the settings enabling triggered by queue and timer
     for to_disable in local.io_sign_issuer_func.staging_disabled :
     format("AzureWebJobs.%s.Disabled", to_disable)
@@ -87,7 +89,7 @@ resource "azurerm_role_assignment" "issuer_func_api_keys_queue_processor_role" {
 
 module "io_sign_issuer_func_staging_slot" {
   count  = var.io_sign_issuer_func.sku_tier == "PremiumV3" ? 1 : 0
-  source = "git::https://github.com/pagopa/terraform-azurerm-v3.git//function_app_slot?ref=v6.2.1"
+  source = "github.com/pagopa/terraform-azurerm-v3.git//function_app_slot?ref=v7.46.0"
 
   name                = "staging"
   location            = azurerm_resource_group.backend_rg.location
