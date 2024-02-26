8147b888f2130065bb2ae8739b4de00a1e2e2ccd
gquadrati <greta.quadrati@pagopa.it>
2024-02-26 12:45:01 +0100

    allow beta testers to instant delete

--- src/core/function_admin.tf
+++ src/core/function_admin.tf
@@ -158,7 +158,7 @@ locals {
       PROFILE_EMAILS_TABLE_NAME                = "profileEmails"
 
       # Instant delete
-      INSTANT_DELETE_ENABLED_USERS = data.azurerm_key_vault_secret.fn_admin_INSTANT_DELETE_ENABLED_USERS.value
+      INSTANT_DELETE_ENABLED_USERS = join(",", [data.azurerm_key_vault_secret.fn_admin_INSTANT_DELETE_ENABLED_USERS.value, local.test_users])
     }
   }
 }
