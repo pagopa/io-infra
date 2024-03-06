a9f11a8ec384fd05746ecf13869599ebf95d58be
Andrea Grillo <andrea.grillo@pagopa.it>
2024-03-06 15:20:23 +0100

    remove italy north

--- src/domains/cgn/prod/italy-north/storage_accounts.tf
+++
@@ -1,9 +0,0 @@
-module "storage_accounts" {
-  source = "../../_modules/storage_accounts"
-
-  env_short           = local.env_short
-  location            = local.location
-  resource_group_name = module.resource_groups.resource_group_cgn.name
-
-  tags = local.tags
-}
