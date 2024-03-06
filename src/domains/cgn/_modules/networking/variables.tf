82c512c2d3886f713ef4506eee67daf9edcaac45
Andrea Grillo <andrea.grillo@pagopa.it>
2024-03-06 14:48:02 +0100

    remove pendpoint subnet

--- src/domains/cgn/_modules/networking/variables.tf
+++ src/domains/cgn/_modules/networking/variables.tf
@@ -6,11 +6,6 @@ variable "tags" {
   type = map(any)
 }
 
-variable "cidr_subnet_pendpoints" {
-  type        = list(string)
-  description = "Private Endpoints address space."
-}
-
 variable "cidr_subnet_redis" {
   type = list(string)
 }
--- src/domains/cgn/prod/west-europe/networking.tf
+++ src/domains/cgn/prod/west-europe/networking.tf
@@ -2,7 +2,6 @@ module "networking" {
   source = "../../_modules/networking"
 
   project                = local.project
-  cidr_subnet_pendpoints = ["10.0.240.0/23"]
   cidr_subnet_redis      = ["10.0.14.0/25"]
   cidr_subnet_cgn        = ["10.0.8.0/26"]
 
