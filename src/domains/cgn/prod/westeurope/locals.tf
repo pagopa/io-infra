9cf4a1acda4c4f8e41b2181dd9d2e866201399df
Andrea Grillo <andrea.grillo@pagopa.it>
2024-03-15 15:53:55 +0100

    add tag

--- src/domains/cgn/prod/westeurope/locals.tf
+++ src/domains/cgn/prod/westeurope/locals.tf
@@ -7,10 +7,11 @@ locals {
   secondary_location = "northeurope"
 
   tags = {
-    CostCenter  = "TS310 - PAGAMENTI & SERVIZI"
-    CreatedBy   = "Terraform"
-    Environment = "Prod"
-    Owner       = "IO"
-    Source      = "https://github.com/pagopa/io-infra/blob/main/src/domains/cgn/prod/westeurope"
+    CostCenter     = "TS310 - PAGAMENTI & SERVIZI"
+    CreatedBy      = "Terraform"
+    Environment    = "Prod"
+    Owner          = "IO"
+    ManagementTeam = "IO Comunicazione"
+    Source         = "https://github.com/pagopa/io-infra/blob/main/src/domains/cgn/prod/westeurope"
   }
 }
