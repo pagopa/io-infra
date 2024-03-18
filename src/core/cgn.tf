bf3f4c36d283ecf5f0dad641b02603bef5ed3ba0
Andrea Grillo <andrea.grillo@pagopa.it>
2024-03-18 10:24:53 +0100

    remove redis from core

--- src/core/cgn.tf
+++ src/core/cgn.tf
@@ -7,57 +7,6 @@ data "azurerm_storage_account" "iopstcgn" {
   resource_group_name = data.azurerm_resource_group.rg_cgn.name
 }
 
-data "azurerm_subnet" "redis_cgn_snet" {
-  name                 = format("%s-redis-cgn-snet", local.project)
-  resource_group_name  = azurerm_resource_group.rg_common.name
-  virtual_network_name = module.vnet_common.name
-}
-
-module "redis_cgn" {
-  source                = "git::https://github.com/pagopa/terraform-azurerm-v3.git//redis_cache?ref=v7.61.0"
-  name                  = format("%s-redis-cgn-std", local.project)
-  resource_group_name   = data.azurerm_resource_group.rg_cgn.name
-  location              = data.azurerm_resource_group.rg_cgn.location
-  capacity              = 1
-  family                = "C"
-  sku_name              = "Standard"
-  enable_authentication = true
-  zones                 = null
-  redis_version         = "6"
-
-  // when azure can apply patch?
-  patch_schedules = [{
-    day_of_week    = "Sunday"
-    start_hour_utc = 23
-    },
-    {
-      day_of_week    = "Monday"
-      start_hour_utc = 23
-    },
-    {
-      day_of_week    = "Tuesday"
-      start_hour_utc = 23
-    },
-    {
-      day_of_week    = "Wednesday"
-      start_hour_utc = 23
-    },
-    {
-      day_of_week    = "Thursday"
-      start_hour_utc = 23
-    },
-  ]
-
-  private_endpoint = {
-    enabled              = true
-    virtual_network_id   = module.vnet_common.id
-    subnet_id            = data.azurerm_subnet.redis_cgn_snet.id
-    private_dns_zone_ids = [azurerm_private_dns_zone.privatelink_redis_cache.id]
-  }
-
-  tags = var.tags
-}
-
 ##################
 ## CosmosDB cgn ##
 ##################
