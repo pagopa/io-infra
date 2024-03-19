2fc75ca4d982c29ab660e67c0eee98bc54cf46fc
Andrea Grillo <andrea.grillo@pagopa.it>
2024-03-19 09:59:13 +0100

    update tags

--- src/domains/selfcare/prod/westeurope/locals.tf
+++ src/domains/selfcare/prod/westeurope/locals.tf
@@ -15,10 +15,11 @@ locals {
   selfcare_external_hostname     = "selfcare.pagopa.it"
 
   tags = {
-    CostCenter  = "TS310 - PAGAMENTI & SERVIZI"
-    CreatedBy   = "Terraform"
-    Environment = "Prod"
-    Owner       = "IO"
-    Source      = "https://github.com/pagopa/io-infra/blob/main/src/domains/selfcare/prod/west-europe"
+    CostCenter     = "TS310 - PAGAMENTI & SERVIZI"
+    CreatedBy      = "Terraform"
+    Environment    = "Prod"
+    Owner          = "IO"
+    ManagementTeam = "IO Enti & Servizi"
+    Source         = "https://github.com/pagopa/io-infra/blob/main/src/domains/selfcare/prod/westeurope"
   }
 }
