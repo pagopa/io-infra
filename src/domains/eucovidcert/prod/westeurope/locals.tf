29ea07e4f89af5fb6b2bd7d8c3b26d9606c3c12d
Andrea Grillo <andrea.grillo@pagopa.it>
2024-03-20 22:36:09 +0100

    fmt

--- src/domains/eucovidcert/prod/westeurope/locals.tf
+++ src/domains/eucovidcert/prod/westeurope/locals.tf
@@ -6,10 +6,10 @@ locals {
   location = "westeurope"
 
   tags = {
-    CostCenter     = "TS310 - PAGAMENTI & SERVIZI"
-    CreatedBy      = "Terraform"
-    Environment    = "Prod"
-    Owner          = "IO"
-    Source         = "https://github.com/pagopa/io-infra/blob/main/src/domains/eucovidcert/prod/westeurope"
+    CostCenter  = "TS310 - PAGAMENTI & SERVIZI"
+    CreatedBy   = "Terraform"
+    Environment = "Prod"
+    Owner       = "IO"
+    Source      = "https://github.com/pagopa/io-infra/blob/main/src/domains/eucovidcert/prod/westeurope"
   }
 }
