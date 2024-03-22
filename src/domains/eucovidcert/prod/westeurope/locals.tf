0e346bd82cac402b077d8df1f40a19adbb63c77f
Andrea Grillo <andrea.grillo@pagopa.it>
2024-03-22 15:51:03 +0100

    [EC-217] Update tags and use a single file (#922)

--- src/domains/eucovidcert/prod/westeurope/locals.tf
+++ src/domains/eucovidcert/prod/westeurope/locals.tf
@@ -6,10 +6,11 @@ locals {
   location = "westeurope"
 
   tags = {
-    CostCenter  = "TS310 - PAGAMENTI & SERVIZI"
-    CreatedBy   = "Terraform"
-    Environment = "Prod"
-    Owner       = "IO"
-    Source      = "https://github.com/pagopa/io-infra/blob/main/src/domains/eucovidcert/prod/westeurope"
+    CostCenter     = "TS310 - PAGAMENTI & SERVIZI"
+    CreatedBy      = "Terraform"
+    Environment    = "Prod"
+    Owner          = "IO"
+    Source         = "https://github.com/pagopa/io-infra/blob/main/src/domains/eucovidcert/prod/westeurope"
+    ManagementTeam = "IO Wallet"
   }
 }
