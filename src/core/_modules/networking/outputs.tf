4d829bca0b31156d09b74bfe5667a433fcdea1f1
christian-calabrese <christian.calabrese@pagopa.it>
2024-04-23 10:53:28 +0200

    feat: created common snet for private endpoints

--- src/core/_modules/networking/outputs.tf
+++ src/core/_modules/networking/outputs.tf
@@ -5,3 +5,11 @@ output "vnet_itn_common" {
     address_space = module.vnet_itn_common.address_space
   }
 }
+
+output "pep_snet" {
+  value = {
+    id               = module.pep_snet.id
+    name             = module.pep_snet.name
+    address_prefixes = module.pep_snet.address_prefixes
+  }
+}
