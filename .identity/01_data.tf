94dc39592efbe246485a0076bcedcbbfe5dd025e
Andrea Grillo <andrea.grillo@pagopa.it>
2024-05-30 17:26:07 +0200

    delete github data in identity config

--- .identity/01_data.tf
+++
@@ -1,8 +0,0 @@
-resource "azuread_directory_role" "directory_readers" {
-  display_name = "Directory Readers"
-}
-
-data "github_organization_teams" "all" {
-  root_teams_only = true
-  summary_only    = true
-}
