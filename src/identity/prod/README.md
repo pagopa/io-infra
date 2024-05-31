abd8c1b7b95e4e3ecb7b9024a2f9aaece190328f
Andrea Grillo <andrea.grillo@pagopa.it>
2024-05-31 12:14:11 +0200

    [EC-316] Apply standards to manage federated managed identities (#1004)

---
+++ src/identity/prod/README.md
@@ -0,0 +1,28 @@
+# IO Infra - Identity
+
+<!-- markdownlint-disable -->
+<!-- BEGINNING OF PRE-COMMIT-TERRAFORM DOCS HOOK -->
+## Requirements
+
+| Name | Version |
+|------|---------|
+| <a name="requirement_azurerm"></a> [azurerm](#requirement\_azurerm) | <= 3.105.0 |
+
+## Modules
+
+| Name | Source | Version |
+|------|--------|---------|
+| <a name="module_federated_identities"></a> [federated\_identities](#module\_federated\_identities) | github.com/pagopa/dx//infra/modules/azure_federated_identity_with_github | main |
+
+## Resources
+
+No resources.
+
+## Inputs
+
+No inputs.
+
+## Outputs
+
+No outputs.
+<!-- END OF PRE-COMMIT-TERRAFORM DOCS HOOK -->
---
+++ src/identity/prod/main.tf
@@ -0,0 +1,72 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "<= 3.105.0"
+    }
+  }
+
+  backend "azurerm" {
+    resource_group_name  = "terraform-state-rg"
+    storage_account_name = "tfappprodio"
+    container_name       = "terraform-state"
+    key                  = "io-infra.identity.tfstate"
+  }
+}
+
+provider "azurerm" {
+  features {
+  }
+}
+
+module "federated_identities" {
+  source = "github.com/pagopa/dx//infra/modules/azure_federated_identity_with_github?ref=main"
+
+  prefix    = local.prefix
+  env_short = local.env_short
+  env       = local.env
+  domain    = local.domain
+
+  repositories = [local.repo_name]
+
+  continuos_integration = {
+    enable = true
+    roles = {
+      subscription = [
+        "Reader",
+        "PagoPA IaC Reader",
+        "Reader and Data Access",
+        "Storage Blob Data Reader",
+        "Storage File Data SMB Share Reader",
+        "Storage Queue Data Reader",
+        "Storage Table Data Reader",
+        "Key Vault Reader",
+        "DocumentDB Account Contributor",
+        "API Management Service Contributor",
+      ]
+      resource_groups = {
+        terraform-state-rg = [
+          "Storage Blob Data Contributor"
+        ]
+      }
+    }
+  }
+
+  continuos_delivery = {
+    enable = true
+    roles = {
+      subscription = [
+        "Contributor",
+        "Storage Account Contributor",
+        "Storage Blob Data Contributor",
+        "Storage File Data SMB Share Contributor",
+        "Storage Queue Data Contributor",
+        "Storage Table Data Contributor",
+        "Key Vault Contributor",
+      ]
+      resource_groups = {}
+    }
+  }
+
+  tags = local.tags
+}
