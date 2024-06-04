be28c93460fbb91f9ff4ef51a1cc6b50df8d7c70
Andrea Grillo <andrea.grillo@pagopa.it>
2024-06-04 09:38:33 +0200

    remove duplicated pool

--- src/core/appgateway.tf
+++ src/core/appgateway.tf
@@ -106,20 +106,6 @@ module "app_gw" {
       pick_host_name_from_backend = true
     }
 
-    session-manager-app = {
-      protocol     = "Https"
-      host         = null
-      port         = 443
-      ip_addresses = null # with null value use fqdns
-      fqdns = [
-        data.azurerm_linux_web_app.session_manager.default_hostname
-      ]
-      probe                       = "/healthcheck"
-      probe_name                  = "probe-session-manager-app"
-      request_timeout             = 10
-      pick_host_name_from_backend = true
-    }
-
     developerportal-backend = {
       protocol     = "Https"
       host         = null
