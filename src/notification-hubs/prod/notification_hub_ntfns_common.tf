f8b96c4eb918db6df4fdc8022b6a110f98a996c4
Andrea Grillo <andrea.grillo@pagopa.it>
2024-06-07 11:56:32 +0200

    add auth rules

--- src/notification-hubs/prod/notification_hub_ntfns_common.tf
+++ src/notification-hubs/prod/notification_hub_ntfns_common.tf
@@ -11,8 +11,8 @@ resource "azurerm_notification_hub_namespace" "common" {
 resource "azurerm_notification_hub" "common" {
   name                = "${local.legacy_project}-ntf-common"
   namespace_name      = azurerm_notification_hub_namespace.common.name
-  resource_group_name = data.azurerm_resource_group.weu_common.name
-  location            = data.azurerm_resource_group.weu_common.location
+  resource_group_name = azurerm_notification_hub_namespace.common.resource_group_name
+  location            = azurerm_notification_hub_namespace.common.location
 
   apns_credential {
     application_mode = "Production"
@@ -32,8 +32,28 @@ resource "azurerm_notification_hub" "common" {
 resource "azurerm_notification_hub" "common01" {
   name                = "${local.legacy_project}-ntf-common01"
   namespace_name      = azurerm_notification_hub_namespace.common.name
-  resource_group_name = data.azurerm_resource_group.weu_common.name
-  location            = data.azurerm_resource_group.weu_common.location
+  resource_group_name = azurerm_notification_hub_namespace.common.resource_group_name
+  location            = azurerm_notification_hub_namespace.common.location
 
   tags = local.tags
+}
+
+resource "azurerm_notification_hub_authorization_rule" "common_default_listen" {
+  name                  = "DefaultListenSharedAccessSignature"
+  notification_hub_name = azurerm_notification_hub.common.name
+  namespace_name      = azurerm_notification_hub_namespace.common.name
+  resource_group_name = azurerm_notification_hub_namespace.common.resource_group_name
+  manage                = false
+  send                  = false
+  listen                = true
+}
+
+resource "azurerm_notification_hub_authorization_rule" "common_default_full" {
+  name                  = "DefaultFullSharedAccessSignature"
+  notification_hub_name = azurerm_notification_hub.common.name
+  namespace_name      = azurerm_notification_hub_namespace.common.name
+  resource_group_name = azurerm_notification_hub_namespace.common.resource_group_name
+  manage                = true
+  send                  = true
+  listen                = true
 }
