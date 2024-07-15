64e64e3054c5929929ecca56dc3ef2e44f4bac67
christian-calabrese <christian.calabrese@pagopa.it>
2024-07-15 12:32:29 +0200

    fix: peering now uses resources to avoid duplicates

--- src/core/_modules/vnet_peering/variables.tf
+++ src/core/_modules/vnet_peering/variables.tf
@@ -1,11 +1,11 @@
 variable "source_vnet" {
   type = object({
-    name                  = string
-    id                    = string
-    resource_group_name   = string
-    allow_gateway_transit = optional(bool, false)
+    name                         = string
+    id                           = string
+    resource_group_name          = string
+    allow_gateway_transit        = optional(bool, false)
     allow_virtual_network_access = optional(bool, true)
-    allow_forwarded_traffic = optional(bool, false)
+    allow_forwarded_traffic      = optional(bool, false)
   })
 
   description = "The source vnet information"
