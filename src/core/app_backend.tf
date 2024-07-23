44d7f3194f0d39701d68f3e6045eeb35f05d6806
christian-calabrese <christian.calabrese@pagopa.it>
2024-07-23 09:40:00 +0200

    fix: updated version of module

--- src/core/app_backend.tf
+++ src/core/app_backend.tf
@@ -1378,7 +1378,7 @@ resource "azurerm_monitor_autoscale_setting" "appservice_app_backendli" {
 ## web availabolity test
 module "app_backend_web_test_api" {
   for_each = { for v in local.app_backend_test_urls : v.id => v if v != null }
-  source   = "github.com/pagopa/terraform-azurerm-v3//application_insights_web_test_preview?ref=v8.27.0"
+  source   = "github.com/pagopa/terraform-azurerm-v3//application_insights_web_test_preview?ref=v8.29.1"
 
   subscription_id                   = data.azurerm_subscription.current.subscription_id
   name                              = format("%s-test", each.value.name)
