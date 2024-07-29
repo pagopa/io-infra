c3442744b6b89226879839c71ba46f36307f41b4
Greta Quadrati <75862507+gquadrati@users.noreply.github.com>
2024-07-29 12:33:44 +0200

    [#IOPID-1875] Route all backend traffic to `app-fn-2` (#1082)

--- src/core/app_backend.tf
+++ src/core/app_backend.tf
@@ -350,7 +350,7 @@ locals {
     app_settings_l1 = {
       IS_APPBACKENDLI = "false"
       // FUNCTIONS
-      API_URL              = "https://${data.azurerm_linux_function_app.function_app[0].default_hostname}/api/v1"
+      API_URL              = "https://${data.azurerm_linux_function_app.function_app[1].default_hostname}/api/v1"
       APP_MESSAGES_API_URL = "https://${data.azurerm_linux_function_app.app_messages_1.default_hostname}/api/v1"
     }
     app_settings_l2 = {
@@ -368,7 +368,7 @@ locals {
     app_settings_li = {
       IS_APPBACKENDLI = "true"
       // FUNCTIONS
-      API_URL              = "https://${data.azurerm_linux_function_app.function_app[0].default_hostname}/api/v1" # not used
+      API_URL              = "https://${data.azurerm_linux_function_app.function_app[1].default_hostname}/api/v1" # not used
       APP_MESSAGES_API_URL = "https://${data.azurerm_linux_function_app.app_messages_1.default_hostname}/api/v1"  # not used
     }
   }
