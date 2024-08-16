359df845c651167908863ff10f4c7005f85840b7
Daniele Manni <dan.manni@gmail.com>
2024-08-16 16:59:14 +0200

    [IOPID-1927] Fix static code

--- src/domains/elt/_modules/function_apps/monitor.tf
+++ src/domains/elt/_modules/function_apps/monitor.tf
@@ -38,7 +38,7 @@ resource "azurerm_monitor_scheduled_query_rules_alert_v2" "service_preferences_f
   evaluation_frequency = "PT15M" # Select how often the alert rule is to be run. Select a frequency that's smaller than the aggregation granularity to generate a sliding window for the evaluation.
 
   criteria {
-    query = <<-QUERY
+    query                   = <<-QUERY
       StorageQueueLogs
         | where OperationName contains "PutMessage"
         | where Uri contains "${local.service_preferences_failure_queue_name}"
