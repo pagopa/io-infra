7d731300aa82619732a22dfaee93c6a1138721a4
Michael Disaro <15191694+michaeldisaro@users.noreply.github.com>
2024-08-28 16:50:15 +0200

    [IOCOM-1636] Added apis to point citizen exposed messages functions (#1136)

--- src/domains/messages-common/05_apim_v2.tf
+++ src/domains/messages-common/05_apim_v2.tf
@@ -190,6 +190,7 @@ data "azurerm_api_management_product" "apim_v2_product_services" {
 
 # APIM APIs
 
+# MESSAGES SENDING FUNC EXTERNAL
 data "http" "messages_sending_external_openapi" {
   url = "https://raw.githubusercontent.com/pagopa/io-functions-services-messages/master/openapi/index_external.yaml"
 }
@@ -215,6 +216,7 @@ module "apim_v2_messages_sending_external_api_v1" {
   xml_content = file("./api/messages-sending/v1/_base_policy_external.xml")
 }
 
+# MESSAGES SENDING FUNC INTERNAL
 data "http" "messages_sending_internal_openapi" {
   url = "https://raw.githubusercontent.com/pagopa/io-functions-services-messages/master/openapi/index.yaml"
 }
@@ -240,6 +242,7 @@ module "apim_v2_messages_sending_internal_api_v1" {
   xml_content = file("./api/messages-sending/v1/_base_policy_internal.xml")
 }
 
+# SERVICE MESSAGE MANAGE (TO REMOVE)
 data "http" "service_messages_manage_openapi" {
   url = "https://raw.githubusercontent.com/pagopa/io-functions-services-messages/833616dceab72bd65c4d3875c64eb75787b19258/openapi/index_external.yaml"
 }
@@ -265,6 +268,7 @@ module "apim_v2_service_messages_manage_api_v1" {
   xml_content = file("./api/service-messages/v1/_base_policy.xml")
 }
 
+# SERVICE MESSAGE INTERNAL (TO REMOVE)
 data "http" "service_messages_internal_openapi" {
   url = "https://raw.githubusercontent.com/pagopa/io-functions-services-messages/833616dceab72bd65c4d3875c64eb75787b19258/openapi/index.yaml"
 }
@@ -288,4 +292,78 @@ module "apim_v2_service_messages_internal_api_v1" {
   content_value  = data.http.service_messages_internal_openapi.body
 
   xml_content = file("./api/service-messages/v1/_base_policy.xml")
+}
+
+# MESSAGES CITIZEN FUNC
+module "apim_v2_product_messages_backend" {
+  source = "github.com/pagopa/terraform-azurerm-v3//api_management_product?ref=v8.27.0"
+
+  product_id   = "io-messages-backend-api"
+  display_name = "IO MESSAGES BACKEND API"
+  description  = "Product for IO MESSAGES BACKEND API"
+
+  api_management_name = data.azurerm_api_management.apim_v2_api.name
+  resource_group_name = data.azurerm_api_management.apim_v2_api.resource_group_name
+
+  published             = true
+  subscription_required = true
+  approval_required     = false
+
+  policy_xml = file("./api_product/backend/_base_policy.xml")
+}
+
+
+resource "azurerm_api_management_subscription" "messages_backend_v2" {
+  api_management_name = data.azurerm_api_management.apim_v2_api.name
+  resource_group_name = data.azurerm_api_management.apim_v2_api.resource_group_name
+  product_id          = module.apim_v2_product_messages_backend.id
+  display_name        = "Messages Backend API"
+  state               = "active"
+  allow_tracing       = false
+}
+
+data "http" "messages_citizen_openapi" {
+  url = "https://raw.githubusercontent.com/pagopa/io-messages/main/apps/citizen-func/openapi/index.yaml"
+}
+
+module "apim_v2_messages_citizen_l1_api_v1" {
+  source = "github.com/pagopa/terraform-azurerm-v3//api_management_api?ref=v8.17.0"
+
+  name                  = format("%s-%s-messages-citizen-api-01", local.product, var.location_short)
+  api_management_name   = data.azurerm_api_management.apim_v2_api.name
+  resource_group_name   = data.azurerm_api_management.apim_v2_api.resource_group_name
+  product_ids           = [module.apim_v2_product_messages_backend.product_id]
+  subscription_required = true
+  service_url           = null
+
+  description  = "IO Messages Citizen - L1 - API"
+  display_name = "IO Messages Citizen - L1 - API"
+  path         = "l1/api/v1"
+  protocols    = ["https"]
+
+  content_format = "openapi"
+  content_value  = data.http.messages_citizen_openapi.body
+
+  xml_content = file("./api/messages-citizen/v1/_base_policy_l1.xml")
+}
+
+module "apim_v2_messages_citizen_l2_api_v1" {
+  source = "github.com/pagopa/terraform-azurerm-v3//api_management_api?ref=v8.17.0"
+
+  name                  = format("%s-%s-messages-citizen-api-02", local.product, var.location_short)
+  api_management_name   = data.azurerm_api_management.apim_v2_api.name
+  resource_group_name   = data.azurerm_api_management.apim_v2_api.resource_group_name
+  product_ids           = [module.apim_v2_product_messages_backend.product_id]
+  subscription_required = true
+  service_url           = null
+
+  description  = "IO Messages Citizen - L2 - API"
+  display_name = "IO Messages Citizen - L2 - API"
+  path         = "l2/api/v1"
+  protocols    = ["https"]
+
+  content_format = "openapi"
+  content_value  = data.http.messages_citizen_openapi.body
+
+  xml_content = file("./api/messages-citizen/v1/_base_policy_l2.xml")
 }
