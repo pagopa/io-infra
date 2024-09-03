61f38e19aaf3bc3a78844eee66c6928abde1562a
Andrea Grillo <andrea.grillo@pagopa.it>
2024-09-03 14:53:59 +0200

    import monitoring in common

--- src/common/prod/westeurope.tf
+++ src/common/prod/westeurope.tf
@@ -10,7 +10,7 @@ module "event_hubs_weu" {
   project        = local.project_weu_legacy
 
   resource_group_common = data.azurerm_resource_group.common_weu.name
-  servicebus_dns_zone   = local.core.global.dns.private_dns_zones.servicebus
+  servicebus_dns_zone   = module.global.dns.private_dns_zones.servicebus
   vnet_common           = local.core.networking.weu.vnet_common
   key_vault             = local.core.key_vault.weu.kv
   error_action_group_id = data.azurerm_monitor_action_group.error_action_group.id
