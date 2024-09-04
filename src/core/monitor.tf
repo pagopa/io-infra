d7a43cca8a33de3890129a1b3df49c9618f2eb43
Andrea Grillo <andrea.grillo@pagopa.it>
2024-09-04 12:37:58 +0200

    remove monitoring from old core

--- src/core/monitor.tf
+++ src/core/monitor.tf
@@ -4,6 +4,6 @@ data "azurerm_application_insights" "application_insights" {
 }
 
 data "azurerm_monitor_action_group" "error_action_group" {
-  name                = format("%s-ai-common", local.project)
+  name                = "${var.prefix}${var.env_short}error"
   resource_group_name = azurerm_resource_group.rg_common.name
 }
