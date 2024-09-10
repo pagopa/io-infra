360978ed5f30b7104aac2721f53ef9d9f60ff842
christian-calabrese <christian.calabrese@pagopa.it>
2024-09-10 14:21:57 +0200

    chore: application gateway changed resource group output name
    Co-authored-by: Andrea Grillo <andrea.grillo@pagopa.it>

--- src/common/_modules/application_gateway/outputs.tf
+++ src/common/_modules/application_gateway/outputs.tf
@@ -3,6 +3,6 @@ output "public_ip" {
     address        = azurerm_public_ip.appgateway_public_ip.ip_address
     id             = azurerm_public_ip.appgateway_public_ip.id
     name           = azurerm_public_ip.appgateway_public_ip.name
-    resource_group = azurerm_public_ip.appgateway_public_ip.resource_group_name
+    resource_group_name = azurerm_public_ip.appgateway_public_ip.resource_group_name
   }
 }
