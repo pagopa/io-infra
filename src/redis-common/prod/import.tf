fa631dad9d5f2be646636c115876785288ddb227
Andrea Grillo <andrea.grillo@pagopa.it>
2024-09-20 16:37:52 +0200

    delete redis common

--- src/redis-common/prod/import.tf
+++
@@ -1,4 +0,0 @@
-import {
-  to = azurerm_redis_cache.common
-  id = "/subscriptions/ec285037-c673-4f58-b594-d7c480da4e8b/resourceGroups/io-p-rg-common/providers/Microsoft.Cache/redis/io-p-redis-common"
-}
