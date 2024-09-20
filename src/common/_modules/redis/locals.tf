908568a8886532e9bcded79dec740a5fabae524e
Andrea Grillo <andrea.grillo@pagopa.it>
2024-09-20 16:39:41 +0200

    fix

--- src/common/_modules/redis/locals.tf
+++ src/common/_modules/redis/locals.tf
@@ -2,7 +2,7 @@ locals {
   nonstandard = {
     weu = {
       subnet          = "rediscommon"
-      redis_common    = "io-p-redis-common"
+      redis_common    = "${var.project}-redis-common"
       storage_account = replace(format("%s-stredisbackup", var.project), "-", "")
     }
   }
