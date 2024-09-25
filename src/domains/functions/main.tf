74ce809ca25241b7a4e533a5a33170abb3092342
Garma <giovanniarmaleo00@gmail.com>
2024-09-25 15:55:51 +0200

    update module in functions

--- src/domains/functions/main.tf
+++ src/domains/functions/main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "<= 3.100.0"
+      version = "<= 3.116"
     }
     azuread = {
       source  = "hashicorp/azuread"
@@ -16,3 +16,4 @@ terraform {
 provider "azurerm" {
   features {}
 }
+
