a6b36ce1e2a5b00a8c0c18222649bf263b036da7
christian-calabrese <christian.calabrese@pagopa.it>
2024-09-26 15:12:38 +0200

    Make AppBackend point to trial system apim instead of appgw (#1191)

--- src/common/_modules/app_backend/app_settings.tf
+++ src/common/_modules/app_backend/app_settings.tf
@@ -41,8 +41,8 @@ locals {
     APP_MESSAGES_API_KEY        = data.azurerm_key_vault_secret.app_backend_APP_MESSAGES_API_KEY[(var.index - 1) % 2].value
     LOLLIPOP_API_URL            = "https://${var.backend_hostnames.lollipop}"
     LOLLIPOP_API_KEY            = data.azurerm_key_vault_secret.app_backend_LOLLIPOP_ITN_API_KEY.value
-    TRIAL_SYSTEM_API_URL        = "https://${var.backend_hostnames.trial_system_api}"  # PROD-TRIAL subscription
-    TRIAL_SYSTEM_APIM_URL       = "https://${var.backend_hostnames.trial_system_apim}" # Add this variable to avoid downtime
+    TRIAL_SYSTEM_API_URL        = "https://${var.backend_hostnames.trial_system_api}" # PROD-TRIAL subscription
+    TRIAL_SYSTEM_APIM_URL       = var.backend_hostnames.trial_system_apim             # Add this variable to avoid downtime
     TRIAL_SYSTEM_API_KEY        = data.azurerm_key_vault_secret.app_backend_TRIAL_SYSTEM_API_KEY.value
     TRIAL_SYSTEM_APIM_KEY       = data.azurerm_key_vault_secret.app_backend_TRIAL_SYSTEM_APIM_KEY.value
     IO_WALLET_API_URL           = "https://${var.backend_hostnames.iowallet}"
@@ -57,7 +57,6 @@ locals {
     IO_SIGN_API_BASE_PATH             = "/api/v1/sign"
     LOLLIPOP_API_BASE_PATH            = "/api/v1"
     TRIAL_SYSTEM_API_BASE_PATH        = "/api/v1"
-    TRIAL_SYSTEM_APIM_BASE_PATH       = "/manage/api/v1"
     IO_WALLET_API_BASE_PATH           = "/api/v1/wallet"
 
     // REDIS
