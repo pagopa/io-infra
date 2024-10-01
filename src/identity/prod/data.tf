bc21691cac0ff58066f71f180c2b64ba70b0ac7e
Mario Mupo <43968294+mamu0@users.noreply.github.com>
2024-10-01 17:48:00 +0200

    [CES-142] Fix identity for CD (#1220)

--- src/identity/prod/data.tf
+++ src/identity/prod/data.tf
@@ -5,3 +5,9 @@ data "azurerm_subscription" "trial_system" {
 data "azurerm_subscription" "cgn" {
   provider = azurerm.prod-cgn
 }
+
+data "azurerm_postgresql_server" "cgn_psql" {
+  provider            = azurerm.prod-cgn
+  name                = "cgnonboardingportal-p-db-postgresql"
+  resource_group_name = "cgnonboardingportal-p-db-rg"
+}
