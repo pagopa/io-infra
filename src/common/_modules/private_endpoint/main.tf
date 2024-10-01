1a8508e341370bc9982712129dcdfc493e081f94
Mario Mupo <43968294+mamu0@users.noreply.github.com>
2024-10-01 12:50:09 +0200

    [PE-703] Private Endpoint Common module and CGN configuration (#1211)

---
+++ src/common/_modules/private_endpoint/main.tf
@@ -0,0 +1,27 @@
+resource "azurerm_private_endpoint" "this" {
+  for_each = merge([
+    for pep, instances in local.private_endpoints : {
+      for i, values in instances :
+      "${pep}-pep-${i}" => values
+    }
+  ]...)
+
+  name                = "${var.project}-${each.key}"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  subnet_id           = var.pep_snet_id
+
+  private_service_connection {
+    name                           = "${var.project}-${each.key}-pep-01"
+    private_connection_resource_id = each.value.resource_id
+    is_manual_connection           = false
+    subresource_names              = each.value.subresource_names
+  }
+
+  private_dns_zone_group {
+    name                 = "private-dns-zone-group"
+    private_dns_zone_ids = [each.value.private_dns_zone_id]
+  }
+
+  tags = var.tags
+}
