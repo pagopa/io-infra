d6fb959c42969ff9e9c84efed5e036927e0a77a1
Gianmaria Scorza <gianmaria@scorza.dev>
2024-10-10 12:34:35 +0200

    [IOPAE-1447] ADD Captcha on SignIn,SignUp,Password reset for devportal (#1237)
    Co-authored-by: gunzip <gunzip@users.noreply.github.com>

--- src/domains/selfcare/_modules/app_services/locals.tf
+++ src/domains/selfcare/_modules/app_services/locals.tf
@@ -214,8 +214,8 @@ locals {
 
       # devportal configs
       CLIENT_NAME                = "io-p-developer-portal-app"
-      POLICY_NAME                = "B2C_1_SignUpIn"
-      RESET_PASSWORD_POLICY_NAME = "B2C_1_PasswordReset"
+      POLICY_NAME                = "B2C_1_SignUpInSecure"
+      RESET_PASSWORD_POLICY_NAME = "B2C_1_SignUpInSecure" # Then new userflow handles sign up/sign in and password reset, no need for a dedicate password reset policy anymore
       POST_LOGIN_URL             = "https://${var.devportal_frontend_hostname}"
       POST_LOGOUT_URL            = "https://${var.devportal_frontend_hostname}"
       REPLY_URL                  = "https://${var.devportal_frontend_hostname}"
