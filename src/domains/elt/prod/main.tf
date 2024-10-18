41e5ad4f9ee88f44e2a6be927796db02e6e8154e
christian-calabrese <christian.calabrese@pagopa.it>
2024-10-18 09:12:31 +0200

    feat: unify terraform state files location

--- src/domains/elt/prod/main.tf
+++ src/domains/elt/prod/main.tf
@@ -2,7 +2,7 @@ terraform {
 
   backend "azurerm" {
     resource_group_name  = "terraform-state-rg"
-    storage_account_name = "tfinfprodio"
+    storage_account_name = "iopitntfst001"
     container_name       = "terraform-state"
     key                  = "io-infra.elt.tfstate"
   }
