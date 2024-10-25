3404dd4a7070f5330578461f6ac5c5794e7f78f0
christian-calabrese <christian.calabrese@pagopa.it>
2024-10-25 10:02:17 +0200

    chore: ran pre-commit

--- src/identity/prod/main.tf
+++ src/identity/prod/main.tf
@@ -37,11 +37,11 @@ provider "azurerm" {
 module "federated_identities" {
   source = "github.com/pagopa/dx//infra/modules/azure_federated_identity_with_github?ref=main"
 
-  prefix    = local.prefix
-  env_short = local.env_short
-  env       = local.env
-  domain    = local.domain
-  location  = local.location
+  prefix       = local.prefix
+  env_short    = local.env_short
+  env          = local.env
+  domain       = local.domain
+  location     = local.location
   repositories = [local.repo_name]
 
   continuos_integration = {
