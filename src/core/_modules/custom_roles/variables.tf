84aeca79e88b8873f79446661a60fc51283c2c52
Andrea Grillo <andrea.grillo@pagopa.it>
2024-10-25 18:40:12 +0200

    Update src/core/_modules/custom_roles/variables.tf
    Co-authored-by: Mario Mupo <43968294+mamu0@users.noreply.github.com>

--- src/core/_modules/custom_roles/variables.tf
+++ src/core/_modules/custom_roles/variables.tf
@@ -1,4 +1,4 @@
 variable "subscription_id" {
   type        = string
-  description = "Id of the subscription to use as roles' scope"
+  description = "The subscription ID used as the scope for role definitions."
 }
