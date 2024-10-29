febbb7775aa60e79bc5fa34532dbf444e964f3f8
ccuffari <75679158+ccuffari@users.noreply.github.com>
2024-10-29 16:34:36 +0100

    CES-218-migrate-ioplollipopassertionsst new module code

--- src/domains/citizen-auth-common/99_locals.tf
+++ src/domains/citizen-auth-common/99_locals.tf
@@ -33,4 +33,16 @@ locals {
 
   vnet_common_name_itn                = "${local.common_project_itn}-common-vnet-01"
   vnet_common_resource_group_name_itn = "${local.common_project_itn}-common-rg-01"
+
+  location           = "westeurope"
+  secondary_location = "italynorth"
+
+  itn_environment = {
+    prefix          = var.prefix
+    env_short       = var.env_short
+    location        = var.location_short
+    domain          = "citizen-auth-common"
+    instance_number = "01"
+  }
+  
 }
