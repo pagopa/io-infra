de3b847a6f2ce43b6b1830734d4b1957c7669759
ccuffari <75679158+ccuffari@users.noreply.github.com>
2024-10-29 15:01:58 +0100

    CES-214-migrate-iopweumessagesnotifst new module code fix common_values link

--- src/domains/messages-common/99_main.tf
+++ src/domains/messages-common/99_main.tf
@@ -30,5 +30,5 @@ data "azurerm_subscription" "current" {}
 data "azurerm_client_config" "current" {}
 
 module "common_values" {
-  source = "../../../_modules/common_values"
+  source = "../../_modules/common_values"
 }
