9f079024bf2d87e86e4896a8fffb475e9c8f8c9b
ccuffari <75679158+ccuffari@users.noreply.github.com>
2024-11-07 14:03:10 +0100

    CES-462-migrate-iopstredisbackup new code modified

--- src/common/_modules/redis/storage_account.tf
+++ src/common/_modules/redis/storage_account.tf
@@ -20,7 +20,7 @@ module "azure_storage_account" {
   source = "github.com/pagopa/dx//infra/modules/azure_storage_account?ref=main"
 
   environment                          = local.itn_environment
-  resource_group_name                  = var.resource_group_name
+  resource_group_name                  = var.resource_group_common
   tier                                 = "l"
   subnet_pep_id                        = data.azurerm_subnet.subnet_pep_itn.id
   private_dns_zone_resource_group_name = "${local.prefix}-${local.env_short}-rg-common"
