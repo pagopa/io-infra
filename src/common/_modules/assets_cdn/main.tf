b8e0c11489591e9e1f54d84755368dce475e23db
ccuffari <75679158+ccuffari@users.noreply.github.com>
2024-11-07 13:01:50 +0100

    CES-458-migate-iopstcdnassets new code modified

--- src/common/_modules/assets_cdn/main.tf
+++ src/common/_modules/assets_cdn/main.tf
@@ -32,17 +32,17 @@ resource "azurerm_cdn_profile" "assets_cdn_profile" {
 module "azure_storage_account" {
   source = "github.com/pagopa/dx//infra/modules/azure_storage_account?ref=main"
 
-  environment         = local.itn_environment
-  resource_group_name = var.resource_group_common
-  tier                = "l"
+  environment                          = local.itn_environment
+  resource_group_name                  = var.resource_group_common
+  tier                                 = "l"
   subnet_pep_id                        = data.azurerm_subnet.subnet_pep_itn.id
   private_dns_zone_resource_group_name = "${local.prefix}-${local.env_short}-rg-common"
 
   subservices_enabled = {
     blob  = true
     file  = false
-    queue  = false
-    table  = false
+    queue = false
+    table = false
   }
 
   force_public_network_access_enabled = true
