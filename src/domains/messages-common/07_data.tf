5cee6e79df5f069b9cb2d7500dd88f1d3f074619
ccuffari <75679158+ccuffari@users.noreply.github.com>
2024-11-07 17:45:38 +0100

    CES-214-migrate-iopweumessagesnotifst new code modified

--- src/domains/messages-common/07_data.tf
+++ src/domains/messages-common/07_data.tf
@@ -1,5 +1,5 @@
 data "azurerm_resource_group" "notification_rg" {
-  name     = "${local.prefix}-${local.env_short}-${local.location}-messages-notifications-rg"
+  name = "${local.prefix}-${local.env_short}-${local.location}-messages-notifications-rg"
 }
 
 data "azurerm_virtual_network" "vnet_itn" {
