3adfce6af8f80934fd02667a38ecf86493013785
ccuffari <75679158+ccuffari@users.noreply.github.com>
2024-11-07 12:59:33 +0100

    CES-458-migate-iopstcdnassets new code modified

--- src/common/_modules/assets_cdn/data.tf
+++ src/common/_modules/assets_cdn/data.tf
@@ -4,11 +4,11 @@ data "azurerm_key_vault_secret" "assets_cdn_fn_key_cdn" {
 }
 
 data "azurerm_virtual_network" "vnet_itn" {
-  name                = "${var.project}-itn-common-vnet-01"
-  resource_group_name = "${var.project}-itn-common-rg-01"
+  name                = "${local.prefix}-${local.env_short}-itn-common-vnet-01"
+  resource_group_name = "${local.prefix}-${local.env_short}-itn-common-rg-01"
 }
 
-data "azurerm_subnet" "subnet_private_endpoints_itn" {
+data "azurerm_subnet" "subnet_pep_itn" {
   name                 = "io-p-itn-pep-snet-01 "
   resource_group_name  = data.azurerm_virtual_network.vnet_itn.resource_group_name
   virtual_network_name = data.azurerm_virtual_network.vnet_itn.name
