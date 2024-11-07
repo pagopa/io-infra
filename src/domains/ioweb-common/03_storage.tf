825a04e77a62d97e80b2d631478d6fcf16a788dd
ccuffari <75679158+ccuffari@users.noreply.github.com>
2024-11-07 09:43:09 +0100

    CES-457-migrate-ioplvlogsimmst private endpoint added

--- src/domains/ioweb-common/03_storage.tf
+++ src/domains/ioweb-common/03_storage.tf
@@ -144,8 +144,11 @@ module "azure_storage_account" {
   source = "github.com/pagopa/dx//infra/modules/azure_storage_account?ref=main"
 
   environment         = local.itn_environment
-  resource_group_name = var.resource_group_name ###TO CHECK
+  resource_group_name = data.azurerm_resource_group.name
   access_tier        = "Hot"
+
+  subnet_pep_id                        = data.azurerm_subnet.subnet_pep_itn.id
+  private_dns_zone_resource_group_name = "${local.prefix}-${local.env_short}-itn-common-rg-01"
 
   subservices_enabled = {
     blob  = false
--- src/domains/ioweb-common/07_data.tf
+++ src/domains/ioweb-common/07_data.tf
@@ -11,3 +11,19 @@ data "azurerm_user_assigned_identity" "managed_identity_io_infra_cd" {
   name                = "${local.product}-infra-github-cd-identity"
   resource_group_name = "${local.product}-identity-rg"
 }
+
+# azure storage account existing resource group
+data "azurerm_resource_group" "core_ext" {
+  name = format("%s-citizen-auth-data-rg", local.product)
+}
+
+data "azurerm_virtual_network" "vnet_itn" {
+  name                = "${local.project}-itn-common-vnet-01"
+  resource_group_name = "${local.project}-itn-common-rg-01"
+}
+
+data "azurerm_subnet" "subnet_private_endpoints_itn" {
+  name                 = "io-p-itn-pep-snet-01 "
+  resource_group_name  = data.azurerm_virtual_network.vnet_itn.resource_group_name
+  virtual_network_name = data.azurerm_virtual_network.vnet_itn.name
+}
