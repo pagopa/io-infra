b1327bc8dd42389284ae91bb6546841fb8d47913
ccuffari <75679158+ccuffari@users.noreply.github.com>
2024-11-08 15:03:19 +0100

    CES-460-migrate-iopsteucovidcert blob feature added and domain/appname checked

--- src/domains/eucovidcert/_modules/storage_accounts/locals.tf
+++ src/domains/eucovidcert/_modules/storage_accounts/locals.tf
@@ -1,14 +1,16 @@
 ###Italy North
 locals {
-  prefix          = "io"
-  env_short       = "p"
-  domain          = "eucovidcert"
+  prefix    = "io"
+  env_short = "p"
+  # domain          = "eucovidcert"
+  app_name        = "eucovidcert"
   instance_number = "01"
   itn_environment = {
-    prefix          = local.prefix
-    env_short       = local.env_short
-    location        = var.location
-    domain          = local.domain
+    prefix    = local.prefix
+    env_short = local.env_short
+    location  = var.location
+    # domain          = local.domain
+    app_name        = local.app_name
     instance_number = local.instance_number
   }
 }
