ce6cd457a3ec4262b77588e440d9621819236173
ccuffari <75679158+ccuffari@users.noreply.github.com>
2024-11-08 14:40:35 +0100

    CES-465-migrate-iopweucitizenauthst blob feature added and domain/appname checked

--- src/domains/citizen-auth-common/06_data.tf
+++ src/domains/citizen-auth-common/06_data.tf
@@ -39,11 +39,11 @@ data "azurerm_resource_group" "data_rg_itn" {
 }
 
 data "azurerm_virtual_network" "vnet_itn" {
-  name                = "${local.project}-itn-common-vnet-01"
-  resource_group_name = "${local.project}-itn-common-rg-01"
+  name                = "${local.prefix}-${local.env_short}-itn-common-vnet-01"
+  resource_group_name = "${local.prefix}-${local.env_short}-itn-common-rg-01"
 }
 
-data "azurerm_subnet" "subnet_private_endpoints_itn" {
+data "azurerm_subnet" "subnet_pep_itn" {
   name                 = "io-p-itn-pep-snet-01 "
   resource_group_name  = data.azurerm_virtual_network.vnet_itn.resource_group_name
   virtual_network_name = data.azurerm_virtual_network.vnet_itn.name
