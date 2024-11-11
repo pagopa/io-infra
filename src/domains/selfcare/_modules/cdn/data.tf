c1300a7876a63ae4a0eeab5c4e4b085869d8b1c2
ccuffari <75679158+ccuffari@users.noreply.github.com>
2024-11-11 15:19:27 +0100

    [CES-473] Added new storage account iopselfcaresa for ITN migration refactoring with latests suggestions

--- src/domains/selfcare/_modules/cdn/data.tf
+++ src/domains/selfcare/_modules/cdn/data.tf
@@ -12,14 +12,3 @@ data "azurerm_log_analytics_workspace" "law_common" {
   name                = "${var.project}-law-common"
   resource_group_name = "${var.project}-rg-common"
 }
-
-data "azurerm_virtual_network" "vnet_itn" {
-  name                = "${local.prefix}-${local.env_short}-itn-common-vnet-01"
-  resource_group_name = "${local.prefix}-${local.env_short}-itn-common-rg-01"
-}
-
-data "azurerm_subnet" "subnet_pep_itn" {
-  name                 = "io-p-itn-pep-snet-01 "
-  resource_group_name  = data.azurerm_virtual_network.vnet_itn.resource_group_name
-  virtual_network_name = data.azurerm_virtual_network.vnet_itn.name
-}
