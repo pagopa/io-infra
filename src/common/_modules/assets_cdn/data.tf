45f7e68c6919e8dfe51ff62d6f0c41fc89515014
ccuffari <75679158+ccuffari@users.noreply.github.com>
2024-11-11 15:29:14 +0100

    [CES-458] Added new storage account iopstcdnassets for ITN migration with latests suggestions

--- src/common/_modules/assets_cdn/data.tf
+++ src/common/_modules/assets_cdn/data.tf
@@ -2,14 +2,3 @@ data "azurerm_key_vault_secret" "assets_cdn_fn_key_cdn" {
   name         = "${var.assets_cdn_fn.name}-KEY-CDN"
   key_vault_id = var.key_vault_common.id
 }
-
-data "azurerm_virtual_network" "vnet_itn" {
-  name                = "${local.prefix}-${local.env_short}-itn-common-vnet-01"
-  resource_group_name = "${local.prefix}-${local.env_short}-itn-common-rg-01"
-}
-
-data "azurerm_subnet" "subnet_pep_itn" {
-  name                 = "io-p-itn-pep-snet-01 "
-  resource_group_name  = data.azurerm_virtual_network.vnet_itn.resource_group_name
-  virtual_network_name = data.azurerm_virtual_network.vnet_itn.name
-}
