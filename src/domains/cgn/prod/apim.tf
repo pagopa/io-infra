79e27035a037e0809b21d4d070d78f24c03724ec
Michael Disaro <15191694+michaeldisaro@users.noreply.github.com>
2024-11-19 12:34:37 +0100

    Update apim.tf

--- src/domains/cgn/prod/apim.tf
+++ src/domains/cgn/prod/apim.tf
@@ -3,7 +3,7 @@ module "apim" {
 
   project                        = local.project
   env_short                      = local.env_short
-  function_cgn_merchant_hostname = module.functions.function_app_cgn_merchant.hostname
+
   apim = {
     name                = local.apim_v2_name
     resource_group_name = local.apim_resource_group_name
@@ -17,7 +17,7 @@ module "apim_itn" {
 
   project                        = local.project
   env_short                      = local.env_short
-  function_cgn_merchant_hostname = module.functions.function_app_cgn_merchant.hostname
+
   apim = {
     name                = local.apim_itn_name
     resource_group_name = local.apim_itn_resource_group_name
