5e29f0273ab081eb6ae7f8dfd88d2ddc1601cb48
Mario Mupo <mario.mupo@pagopa.it>
2024-11-20 15:54:30 +0100

    fix: change address prefix

--- src/common/prod/westeurope.tf
+++ src/common/prod/westeurope.tf
@@ -448,7 +448,7 @@ module "app_backend_weu" {
   slot_allowed_subnets       = concat([local.azdoa_snet_id["weu"]], data.azurerm_subnet.services_snet.*.id, [module.application_gateway_weu.snet.id, module.apim_weu.snet.id, module.apim_itn.snet.id])
   allowed_ips                = module.monitoring_weu.appi.reserved_ips
   slot_allowed_ips           = module.monitoring_weu.appi.reserved_ips
-  apim_snet_address_prefixes = module.apim_itn.snet.address_prefixes
+  apim_snet_address_prefixes = module.apim_weu.snet.address_prefixes # module.apim_itn.snet.address_prefixes Change this for APIM ITN switch
 
   backend_hostnames = local.backend_hostnames
 
@@ -502,7 +502,7 @@ module "app_backend_li_weu" {
       "51.105.109.140/32"
   ])
   slot_allowed_ips           = []
-  apim_snet_address_prefixes = module.apim_itn.snet.address_prefixes
+  apim_snet_address_prefixes = module.apim_weu.snet.address_prefixes # module.apim_itn.snet.address_prefixes Change this for APIM ITN switch
 
   backend_hostnames = local.backend_hostnames
 
