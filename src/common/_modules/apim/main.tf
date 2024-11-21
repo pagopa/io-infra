39c3fa24ec728fb9c7e3c7ffac626107e63e7c56
Mario Mupo <43968294+mamu0@users.noreply.github.com>
2024-11-21 17:56:38 +0100

    [CES-68] Reduced instances for APIM ITN (#1333)

--- src/common/_modules/apim/main.tf
+++ src/common/_modules/apim/main.tf
@@ -10,10 +10,10 @@ module "apim_v2" {
   notification_sender_email = data.azurerm_key_vault_secret.apim_publisher_email.value
   sku_name                  = var.migration ? "Premium_1" : "Premium_2"
   virtual_network_type      = "Internal"
-  zones                     = ["1", "2"]
+  zones                     = var.migration ? ["1"] : ["1", "2"]
 
   redis_cache_id       = null
-  public_ip_address_id = azurerm_public_ip.apim.id
+  public_ip_address_id = var.migration ? azurerm_public_ip.apim_tmp[0].id : azurerm_public_ip.apim.id
 
   hostname_configuration = {
     proxy = [
