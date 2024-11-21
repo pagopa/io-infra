42f7834ea878781624c2dc79bcc9d8f9b312dcca
Mario Mupo <mario.mupo@pagopa.it>
2024-11-21 09:19:53 +0100

    feat: changed ref for itn apim

--- src/domains/citizen-auth-app/04_function_lollipop.tf
+++ src/domains/citizen-auth-app/04_function_lollipop.tf
@@ -1,5 +1,5 @@
 data "azurerm_key_vault_secret" "first_lollipop_consumer_subscription_key" {
-  name         = "first-lollipop-consumer-pagopa-subscription-key-v2" # itn" Change it for itn switch
+  name         = "first-lollipop-consumer-pagopa-subscription-key-itn"
   key_vault_id = data.azurerm_key_vault.kv.id
 }
 
