43e58a4ac8dba6d341c234e4dcb202fcf1bb4f16
Mario Mupo <mario.mupo@pagopa.it>
2024-11-21 16:21:43 +0100

    fix: minor

--- src/domains/eucovidcert/_modules/function_apps/locals.tf
+++ src/domains/eucovidcert/_modules/function_apps/locals.tf
@@ -55,9 +55,8 @@ locals {
 
 # Region ITN
 locals {
-  itn_location       = "italynorth"
   itn_location_short = "itn"
 
-  vnet_common_name_itn                = "io-p-itn-common-vnet-01"
-  vnet_common_resource_group_name_itn = "io-p-itn-common-rg-01"
+  vnet_common_name_itn                = "${var.project}-${local.itn_location_short}-common-vnet-01"
+  vnet_common_resource_group_name_itn = "${var.project}-${local.itn_location_short}-common-rg-01"
 }
