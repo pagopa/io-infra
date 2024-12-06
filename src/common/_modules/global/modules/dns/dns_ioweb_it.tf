57a8ff1f7ad13b8b53ef2069249ac69fcc93c5f0
arcogabbo <arcogabbo@gmail.com>
2024-12-06 12:23:48 +0100

    introduced new zone

--- src/common/_modules/global/modules/dns/dns_ioweb_it.tf
+++ src/common/_modules/global/modules/dns/dns_ioweb_it.tf
@@ -58,18 +58,3 @@ resource "azurerm_dns_cname_record" "zendesk" {
   ttl                 = var.dns_default_ttl_sec
   record              = "hc-io.zendesk.com"
 }
-
-# A record for account.ioapp.it
-data "azurerm_cdn_frontdoor_profile" "io_web_profile_itn_cdn_profile" {
-  name                = format("%s-itn-ioweb-profile-portal-afd-01", var.project)
-  resource_group_name = format("%s-itn-ioweb-fe-rg-01", var.project)
-}
-
-resource "azurerm_dns_a_record" "account" {
-  name                = "account"
-  zone_name           = azurerm_dns_zone.ioweb_it.name
-  resource_group_name = var.resource_groups.external
-  ttl                 = var.dns_default_ttl_sec
-  target_resource_id  = data.azurerm_cdn_frontdoor_profile.io_web_profile_itn_cdn_profile.id
-}
-##############################
