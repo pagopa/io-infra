21ef4a88c416f22a66af1b2284b5ba39b35a4037
christian-calabrese <christian.calabrese@pagopa.it>
2025-01-14 15:07:33 +0100

    fix: test without pre-commit config

--- src/core/prod/main.tf
+++ src/core/prod/main.tf
@@ -19,3 +19,17 @@ terraform {
 provider "azurerm" {
   features {}
 }
+
+module "naming_convention" {
+  source  = "pagopa/dx-azure-naming-convention/azurerm"
+  version = "~> 0"
+
+  environment = {
+    prefix          = var.environment.prefix
+    env_short       = var.environment.env_short
+    location        = var.environment.location
+    domain          = var.environment.domain
+    app_name        = var.environment.app_name
+    instance_number = var.environment.instance_number
+  }
+}
