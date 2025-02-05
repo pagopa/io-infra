ba840c7f23572f6de9839081ae79a8e0b9ffb1a5
Mario Mupo <43968294+mamu0@users.noreply.github.com>
2025-02-05 15:58:19 +0100

    [CES-722] Added roles for admin/developers in selfcare (#1405)

--- src/domains/selfcare/prod/westeurope/data.tf
+++ src/domains/selfcare/prod/westeurope/data.tf
@@ -2,3 +2,11 @@ data "azurerm_application_insights" "application_insights" {
   name                = "${local.project}-ai-common"
   resource_group_name = "${local.project}-rg-common"
 }
+
+data "azuread_group" "svc_admins" {
+  display_name = "${local.project}-adgroup-svc-admins"
+}
+
+data "azuread_group" "svc_devs" {
+  display_name = "${local.project}-adgroup-svc-developers"
+}
