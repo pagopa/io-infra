1215f9e5ffd558a1a9752cadcdf64b931d791cbc
Mario Mupo <mario.mupo@pagopa.it>
2025-02-06 16:58:59 +0100

    feat: Added APIM roles for app_services selfcare

--- src/domains/selfcare/prod/westeurope/data.tf
+++ src/domains/selfcare/prod/westeurope/data.tf
@@ -10,3 +10,15 @@ data "azuread_group" "svc_admins" {
 data "azuread_group" "svc_devs" {
   display_name = "${local.project}-adgroup-svc-developers"
 }
+
+# APIM in WEU
+data "azurerm_api_management" "apim_v2_api" {
+  name                = local.apim_v2_name
+  resource_group_name = local.apim_resource_group_name
+}
+
+# APIM in ITN
+data "azurerm_api_management" "apim_itn_api" {
+  name                = local.apim_itn_name
+  resource_group_name = local.apim_itn_resource_group_name
+}
