99f558e124004f962bfe950ddb1ab5c9c1890aa9
Michael Disarò <michael.disaro@pagopa.it>
2025-02-07 11:27:19 +0100

    changed references to new cgn-xxx-02 functions under shared service plan

--- src/domains/cgn/_modules/apim/named_values_cgn_os.tf
+++ src/domains/cgn/_modules/apim/named_values_cgn_os.tf
@@ -3,7 +3,7 @@ resource "azurerm_api_management_named_value" "cgnonboardingportal_os_url_value_
   api_management_name = data.azurerm_api_management.apim.name
   resource_group_name = data.azurerm_api_management.apim.resource_group_name
   display_name        = "cgnonboardingportal-os-url"
-  value               = "https://io-p-itn-cgn-search-func-01.azurewebsites.net"
+  value               = "https://io-p-itn-cgn-search-func-02.azurewebsites.net"
 }
 
 resource "azurerm_api_management_named_value" "cgnonboardingportal_os_key_v2" {
