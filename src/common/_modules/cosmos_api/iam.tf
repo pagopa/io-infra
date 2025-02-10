bc00a90c2b61d1035b3a50928f9d7b15f55ae0eb
christian-calabrese <christian.calabrese@pagopa.it>
2025-02-10 14:25:49 +0100

    feat: added data plane role assignments for cosmos

--- src/common/_modules/cosmos_api/iam.tf
+++ src/common/_modules/cosmos_api/iam.tf
@@ -49,3 +49,79 @@ module "cosno_api_com_admins" {
     }
   ]
 }
+
+module "cosno_api_com_devs" {
+  source  = "pagopa/dx-azure-role-assignments/azurerm"
+  version = "~>0"
+
+  principal_id          = var.azure_adgroup_com_devs_object_id
+
+  cosmos = [
+    {
+      account_name        = azurerm_cosmosdb_account.this.name
+      resource_group_name = azurerm_cosmosdb_account.this.resource_group_name
+      database            = azurerm_cosmosdb_sql_database.db.id
+      role                = "reader"
+    }
+  ]
+}
+module "cosno_api_svc_admins" {
+  source  = "pagopa/dx-azure-role-assignments/azurerm"
+  version = "~>0"
+
+  principal_id          = var.azure_adgroup_svc_admins_object_id
+
+  cosmos = [
+    {
+      account_name        = azurerm_cosmosdb_account.this.name
+      resource_group_name = azurerm_cosmosdb_account.this.resource_group_name
+      database            = azurerm_cosmosdb_sql_database.db.id
+      role                = "reader"
+    }
+  ]
+}
+module "cosno_api_svc_devs" {
+  source  = "pagopa/dx-azure-role-assignments/azurerm"
+  version = "~>0"
+
+  principal_id          = var.azure_adgroup_svc_devs_object_id
+
+  cosmos = [
+    {
+      account_name        = azurerm_cosmosdb_account.this.name
+      resource_group_name = azurerm_cosmosdb_account.this.resource_group_name
+      database            = azurerm_cosmosdb_sql_database.db.id
+      role                = "reader"
+    }
+  ]
+}
+module "cosno_api_auth_admins" {
+  source  = "pagopa/dx-azure-role-assignments/azurerm"
+  version = "~>0"
+
+  principal_id          = var.azure_adgroup_auth_admins_object_id
+
+  cosmos = [
+    {
+      account_name        = azurerm_cosmosdb_account.this.name
+      resource_group_name = azurerm_cosmosdb_account.this.resource_group_name
+      database            = azurerm_cosmosdb_sql_database.db.id
+      role                = "reader"
+    }
+  ]
+}
+module "cosno_api_auth_devs" {
+  source  = "pagopa/dx-azure-role-assignments/azurerm"
+  version = "~>0"
+
+  principal_id          = var.azure_adgroup_auth_devs_object_id
+
+  cosmos = [
+    {
+      account_name        = azurerm_cosmosdb_account.this.name
+      resource_group_name = azurerm_cosmosdb_account.this.resource_group_name
+      database            = azurerm_cosmosdb_sql_database.db.id
+      role                = "reader"
+    }
+  ]
+}
