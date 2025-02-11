0de94b61ed47aa4e205506bf17c41dc3ad70ac01
christian-calabrese <christian.calabrese@pagopa.it>
2025-02-11 15:43:47 +0100

    feat: add txt record for maven verification

--- src/common/_modules/global/modules/dns/dns_wallet_io_pagopa_it.tf
+++ src/common/_modules/global/modules/dns/dns_wallet_io_pagopa_it.tf
@@ -4,3 +4,15 @@ resource "azurerm_dns_zone" "wallet_io_pagopa_it" {
 
   tags = var.tags
 }
+
+# TXT for Maven verification
+resource "azurerm_dns_txt_record" "wallet_io_pagopa_it" {
+  name                = "@"
+  zone_name           = azurerm_dns_zone.wallet_io_pagopa_it.name
+  resource_group_name = var.resource_groups.external
+  ttl                 = var.dns_default_ttl_sec
+  record {
+    value = "qidvirtenu"
+  }
+  tags = var.tags
+}
