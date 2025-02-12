1b08761886105731fb6d91eb315e614fafac7156
Christian Calabrese <christian.calabrese@pagopa.it>
2025-02-12 11:21:59 +0100

    [CES-742] Added delegation of zone wallet.io.pagopa.it (#1421)

--- src/common/_modules/global/modules/dns/dns_io_pagopa_it.tf
+++ src/common/_modules/global/modules/dns/dns_io_pagopa_it.tf
@@ -145,3 +145,13 @@ resource "azurerm_dns_ns_record" "ipatente_io_pagopa_it_ns" {
   ttl                 = var.dns_default_ttl_sec
   tags                = var.tags
 }
+
+# wallet.io.pagopa.it
+resource "azurerm_dns_ns_record" "wallet_io_pagopa_it_ns" {
+  name                = "wallet"
+  zone_name           = azurerm_dns_zone.io_pagopa_it.name
+  resource_group_name = var.resource_groups.external
+  records             = azurerm_dns_zone.wallet_io_pagopa_it.name_servers
+  ttl                 = 30
+  tags                = var.tags
+}
