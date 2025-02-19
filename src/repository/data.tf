a98ae0da74634acfa15d7fddcc2f5bf540097846
Krusty93 <andrea.grillo@pagopa.it>
2025-02-19 12:42:43 +0100

    add dev env

--- src/repository/data.tf
+++ src/repository/data.tf
@@ -3,9 +3,21 @@ data "azurerm_user_assigned_identity" "identity_prod_ci" {
   resource_group_name = local.identity_resource_group_name
 }
 
+data "azurerm_user_assigned_identity" "identity_dev_ci" {
+  provider            = azurerm.dev-io
+  name                = "${local.project_dev}-infra-github-ci-identity"
+  resource_group_name = local.identity_resource_group_name_dev
+}
+
 data "azurerm_user_assigned_identity" "identity_prod_cd" {
   name                = "${local.project}-infra-github-cd-identity"
   resource_group_name = local.identity_resource_group_name
+}
+
+data "azurerm_user_assigned_identity" "identity_dev_cd" {
+  provider            = azurerm.dev-io
+  name                = "${local.project_dev}-infra-github-cd-identity"
+  resource_group_name = local.identity_resource_group_name_dev
 }
 
 data "github_organization_teams" "all" {
