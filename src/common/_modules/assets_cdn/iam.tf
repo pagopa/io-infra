19e01991d73d87e800b5d396cc4e4f1f075dd2e1
Stefano.D'aversa <stefano.daversa@pagopa.it>
2025-02-24 17:43:19 +0100

    add permission on storage blob for es team

---
+++ src/common/_modules/assets_cdn/iam.tf
@@ -0,0 +1,19 @@
+module "roles_svc_devs" {
+  source  = "pagopa/dx-azure-role-assignments/azurerm"
+  version = "~>0"
+
+  principal_id = var.azure_adgroup_svc_devs_object_id
+
+  storage_blob = [
+    {
+      storage_account_name = "${var.project}-stcdnassets"
+      resource_group_name  = var.resource_group_assets_cdn
+      role                 = "reader"
+    },
+    {
+      storage_account_name = "${var.project}-stcdnassets"
+      resource_group_name  = var.resource_group_assets_cdn
+      role                 = "writer"
+    }
+  ]
+}
