9f46f6aad65bd2604321981e0869d453c3cd3f46
Andrea Grillo <andrea.grillo@pagopa.it>
2025-03-03 18:07:01 +0100

    Add scheduler to APIM Migration workflow to test the complete setup tonight (#1440)
    Replace production resources with demos to simulate the APIM migration
    
    Closes #CES-790
    
    ---------
    
    Co-authored-by: Christian Calabrese <christian.calabrese@pagopa.it>

--- src/repository/github_repository.tf
+++ src/repository/github_repository.tf
@@ -11,7 +11,7 @@ resource "github_repository" "this" {
   allow_merge_commit          = false
   allow_squash_merge          = true
   squash_merge_commit_title   = "PR_TITLE"
-  squash_merge_commit_message = "BLANK"
+  squash_merge_commit_message = "PR_BODY"
 
   delete_branch_on_merge = true
 
