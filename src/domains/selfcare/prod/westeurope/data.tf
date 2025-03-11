100ae991190c91c73352456467d81feacae755a9
Rodolfo Viti <62432865+rodoviti@users.noreply.github.com>
2025-03-11 12:33:13 +0100

    [IOPAE] remove apim weu references (#1463)
    ### Motivation and Context
    
    <!--- Why is this change required? What problem does it solve? -->
    
    ### Major Changes
    
    <!--- Describe the major changes introduced by this PR -->
    
    ### Dependencies
    
    <!--- If this PR depends on or is related to other PRs, 
    list them here using the GitHub syntax: `depends on #123` -->
    
    ### Testing
    
    <!--- Describe the testing steps you have performed -->
    <!--- and/or if this PR is already (partially) applied and why -->
    
    ### Documentation
    
    <!--- Are there any updates to the documentation? -->
    
    ### Other Considerations
    
    <!--- Any additional context, such as breaking changes, security
    concerns, etc. -->

--- src/domains/selfcare/prod/westeurope/data.tf
+++ src/domains/selfcare/prod/westeurope/data.tf
@@ -11,12 +11,6 @@ data "azuread_group" "svc_devs" {
   display_name = "${local.project}-adgroup-svc-developers"
 }
 
-# APIM in WEU
-data "azurerm_api_management" "apim_v2_api" {
-  name                = local.apim_v2_name
-  resource_group_name = local.apim_resource_group_name
-}
-
 # APIM in ITN
 data "azurerm_api_management" "apim_itn_api" {
   name                = local.apim_itn_name
