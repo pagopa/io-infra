ac087d0f056ef3d991edb357ef358bd898aba0d9
Krusty93 <andrea.grillo@pagopa.it>
2025-03-11 15:12:35 +0100

    update tags

--- src/domains/ioweb-common/07_data.tf
+++ src/domains/ioweb-common/07_data.tf
@@ -26,12 +26,6 @@ data "azurerm_user_assigned_identity" "managed_identity_auth_n_identity_infra_cd
 # APIM #
 ########
 
-# APIM in WEU
-data "azurerm_api_management" "apim_v2_api" {
-  name                = local.apim_v2_name
-  resource_group_name = local.apim_resource_group_name
-}
-
 # APIM in ITN
 data "azurerm_api_management" "apim_itn_api" {
   name                = local.apim_itn_name
