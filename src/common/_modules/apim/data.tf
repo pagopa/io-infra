30c50f64339322276ad60c71acac33a375258bcf
Mario Mupo <43968294+mamu0@users.noreply.github.com>
2025-03-11 10:06:49 +0100

    Decommission APIM WEU (#1455)
    ### Motivation and Context
    
    <!--- Why is this change required? What problem does it solve? -->
    This pull request removes the APIM (Azure API Management) instance in
    the WEU region as the migration to the ITN region has been successfully
    completed. The migration process has been verified, and all related
    services have been transitioned to ITN, making the WEU instance
    redundant.
    
    ### Major Changes
    
    <!--- Describe the major changes introduced by this PR -->
    
    ### Dependencies
    
    <!--- If this PR depends on or is related to other PRs, 
    list them here using the GitHub syntax: `depends on #123` -->
    
    ### Testing
    
    <!--- Describe the testing steps you have performed -->
    <!--- and/or if this PR is already (partially) applied and why -->
    
    ### Documentation
    
    <!--- Are there any updates to the documentation? -->
    
    ### Other Considerations
    
    <!--- Any additional context, such as breaking changes, security
    concerns, etc. -->
    Resolves: #CES-818

--- src/common/_modules/apim/data.tf
+++ src/common/_modules/apim/data.tf
@@ -13,27 +13,17 @@ data "azurerm_key_vault_certificate" "api_app_internal_io_pagopa_it" {
   key_vault_id = var.key_vault.id
 }
 
-###############
-# FOR TESTING #
-###############
-
 data "azurerm_private_dns_zone" "azure_api_net" {
-  count = var.migration ? 1 : 0
-
   name                = "azure-api.net"
   resource_group_name = "io-p-rg-common"
 }
 
 data "azurerm_private_dns_zone" "management_azure_api_net" {
-  count = var.migration ? 1 : 0
-
   name                = "management.azure-api.net"
   resource_group_name = "io-p-rg-common"
 }
 
 data "azurerm_private_dns_zone" "scm_azure_api_net" {
-  count = var.migration ? 1 : 0
-
   name                = "scm.azure-api.net"
   resource_group_name = "io-p-rg-common"
 }
