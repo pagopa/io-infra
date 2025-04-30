c3251a89d2a7729fae2f52ebf6003e7bdd16f1da
Daniele Manni <dan.manni@gmail.com>
2025-04-30 10:56:27 +0200

    [#IOPLT-1052] depends on io-proxy for rbac

--- src/common/_modules/io_proxy/rbac.tf
+++ src/common/_modules/io_proxy/rbac.tf
@@ -2,6 +2,10 @@ module "iam_adgroup_product_admins" {
   source  = "pagopa-dx/azure-role-assignments/azurerm"
   version = "~> 0.0"
 
+  depends_on = [
+    module.io_proxy
+  ]
+
   principal_id = var.azure_adgroup_platform_admins_object_id
 
   apim = [
