d9c7f81cbd8a68f5128b2cd0f237ba1934609b86
Daniele Manni <dan.manni@gmail.com>
2025-04-30 13:35:21 +0200

    Fix required params for azure-role-assignments update

--- src/common/_modules/io_proxy/data.tf
+++ src/common/_modules/io_proxy/data.tf
@@ -2,3 +2,5 @@ data "azurerm_key_vault_secret" "apim_publisher_email" {
   name         = "apim-publisher-email"
   key_vault_id = var.key_vault.id
 }
+
+data "azurerm_client_config" "current" {}
--- src/common/_modules/io_proxy/rbac.tf
+++ src/common/_modules/io_proxy/rbac.tf
@@ -6,12 +6,14 @@ module "iam_adgroup_product_admins" {
     module.io_proxy
   ]
 
-  principal_id = var.azure_adgroup_platform_admins_object_id
+  principal_id    = var.azure_adgroup_platform_admins_object_id
+  subscription_id = data.azurerm_client_config.current.subscription_id
 
   apim = [
     {
       name                = module.io_proxy.name
       resource_group_name = module.io_proxy.resource_group_name
+      description         = "Platform team admin group"
       role                = "owner"
     }
   ]
