df60ed356bc5b55d0028bb52cb728aa04a9bd06e
Daniele Manni <dan.manni@gmail.com>
2025-05-08 17:27:02 +0200

    Refactory using azurerm provider instead of old pagopa modules

--- src/common/_modules/platform_api_gateway/api_platform.tf
+++ src/common/_modules/platform_api_gateway/api_platform.tf
@@ -1,6 +1,4 @@
-module "api_gateway_io_platform_product" {
-  source = "github.com/pagopa/terraform-azurerm-v3//api_management_product?ref=v8.92.2"
-
+resource "azurerm_api_management_product" "platform_apim_product" {
   product_id   = "io-platform"
   display_name = "IO PLATFORM"
   description  = "Product for IO Plaftorm APIs"
@@ -12,39 +10,97 @@ module "api_gateway_io_platform_product" {
   subscription_required = false
   approval_required     = false
 
-  policy_xml = file("./apis/base_policy.xml")
+  tags = var.tags
 }
 
-data "http" "platform_io_backend_openapi" {
+resource "azurerm_api_management_product_policy" "platform_apim_product_policy" {
+  product_id          = azurerm_api_management_product.platform_apim_product.product_id
+  api_management_name = module.platform_api_gateway.name
+  resource_group_name = module.platform_api_gateway.resource_group_name
+
+  xml_content = file("./apis/base_policy.xml")
+
+  tags = var.tags
+}
+
+resource "azurerm_api_management_group" "platform_apim_group" {
+  name                = "platform"
+  api_management_name = module.platform_api_gateway.name
+  resource_group_name = module.platform_api_gateway.resource_group_name
+  display_name        = "Platform APIM Product Owners"
+  description         = "Owners of io-platform product with management rights"
+  type                = "external"
+  external_id         = "aad://${var.azure_adgroup_platform_admins_object_id}"
+
+  tags = var.tags
+}
+
+resource "azurerm_api_management_product_group" "platform_apim_product_group" {
+  product_id          = azurerm_api_management_product.platform_apim_product.product_id
+  group_name          = azurerm_api_management_group.platform_apim_group.name
+  api_management_name = module.platform_api_gateway.name
+  resource_group_name = module.platform_api_gateway.resource_group_name
+
+  tags = var.tags
+}
+
+data "http" "platform_app_backend_openapi" {
   # TODO: update this URL with master branch when the new openapi spec will be released
   url = "https://raw.githubusercontent.com/pagopa/io-backend/refs/heads/refactor-openapi-specs/openapi/generated/api_platform.yaml"
 }
 
-module "api_gateway_io_platform_api" {
-  source = "github.com/pagopa/terraform-azurerm-v3//api_management_api?ref=v8.92.2"
-
+resource "azurerm_api_management_api" "platform_app_backend_api" {
   name                  = format("%s-p-app-backend-platform-api", var.prefix)
-  api_management_name = module.platform_api_gateway.name
-  resource_group_name = module.platform_api_gateway.resource_group_name
-  product_ids           = [module.api_gateway_io_platform_product.product_id]
+  api_management_name   = module.platform_api_gateway.name
+  resource_group_name   = module.platform_api_gateway.resource_group_name
   subscription_required = false
-  api_version           = "v1"
-  service_url           = null
+
+  revision = 1
+  version  = "v1"
 
   description  = "IO Platform app-backend API"
-  display_name = "app-backend"
+  display_name = "Platform app-backend"
   path         = "/"
   protocols    = ["https"]
 
-  content_format = "openapi"
-  content_value  = data.http.platform_io_backend_openapi.body
+  import {
+    content_format = "openapi"
+    content_value  = data.http.platform_app_backend_openapi.body
+  }
+
+  tags = var.tags
+}
+
+resource "azurerm_api_management_product_api" "platform_api_product_link" {
+  api_name            = azurerm_api_management_api.platform_app_backend_api.name
+  product_id          = azurerm_api_management_product.platform_apim_product.product_id
+  api_management_name = module.platform_api_gateway.name
+  resource_group_name = module.platform_api_gateway.resource_group_name
+
+  tags = var.tags
+}
+
+resource "azurerm_api_management_api_policy" "platform_app_backend_api_policy" {
+  api_name            = azurerm_api_management_api.platform_app_backend_api.name
+  api_management_name = module.platform_api_gateway.name
+  resource_group_name = module.platform_api_gateway.resource_group_name
 
   xml_content = file("./apis/platform/_base_policy.xml")
 
-  api_operation_policies = [
-    {
-      operation_id = "getServicesStatus"
-      xml_content  = file("./apis/platform/v1/get_services_status_policy.xml")
-    },
-  ]
+  tags = var.tags
 }
+
+resource "azurerm_api_management_api_operation_policy" "get_services_status_operation_policy" {
+  depends_on = [
+    azurerm_api_management_api.platform_app_backend_api
+  ]
+
+  api_name            = azurerm_api_management_api.platform_app_backend_api.name
+  api_management_name = module.platform_api_gateway.name
+  resource_group_name = module.platform_api_gateway.resource_group_name
+  operation_id        = "getServicesStatus"
+  xml_content         = file("./apis/platform/v1/get_services_status_policy.xml")
+
+  tags = var.tags
+}
+
