11cf35628b1a773f97b755dcf3e16bb2d0db4aa8
Daniele Manni <dan.manni@gmail.com>
2025-05-09 15:05:21 +0200

    Remove depends_on on rbac

--- src/common/_modules/platform_api_gateway/rbac.tf
+++ src/common/_modules/platform_api_gateway/rbac.tf
@@ -2,10 +2,6 @@ module "iam_adgroup_product_admins" {
   source  = "pagopa-dx/azure-role-assignments/azurerm"
   version = "~> 1.0"
 
-  depends_on = [
-    module.platform_api_gateway
-  ]
-
   principal_id    = var.azure_adgroup_platform_admins_object_id
   subscription_id = data.azurerm_client_config.current.subscription_id
 
