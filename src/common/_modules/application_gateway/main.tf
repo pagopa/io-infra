95d0b6f954504e6fa3391ef1017eaa50bbefdb31
Daniele Manni <dan.manni@gmail.com>
2025-05-20 12:19:43 +0200

    Fix missing required property in rewrite rule

--- src/common/_modules/application_gateway/main.tf
+++ src/common/_modules/application_gateway/main.tf
@@ -817,7 +817,10 @@ module "app_gw" {
           # URL rewriting preserving the specific endpoint
           url = {
             path = {
-              value = "/api/platform-legacy/v1/{var_uri_path_1}"
+              value        = "/api/platform-legacy/v1/{var_uri_path_1}"
+              query_string = null
+              reroute      = true
+              components   = "path_only"
             }
           }
           request_header_configurations  = []
