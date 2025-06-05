88f0a136766eef606e96a29da07908ac0fcbd19e
Daniele Manni <dan.manni@gmail.com>
2025-06-05 12:57:00 +0200

    [#IOPLT-1142] Add private dns zone domain binding to proxy

--- src/common/_modules/platform_api_gateway/data.tf
+++ src/common/_modules/platform_api_gateway/data.tf
@@ -3,4 +3,9 @@ data "azurerm_key_vault_secret" "apim_publisher_email" {
   key_vault_id = var.key_vault.id
 }
 
+data "azurerm_key_vault_certificate" "proxy_internal_io_pagopa_it" {
+  name         = replace(local.proxy_hostname_internal, ".", "-")
+  key_vault_id = var.key_vault.id
+}
+
 data "azurerm_client_config" "current" {}
