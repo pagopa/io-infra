455153783767dbea7ad5b10dedb44e9c1c864bba
Stefano.D'aversa <stefano.daversa@pagopa.it>
2025-06-19 16:15:02 +0200

    revert remove var declaration

--- src/domains/selfcare/_modules/networking/subnet_selfcare_be_common.tf
+++ src/domains/selfcare/_modules/networking/subnet_selfcare_be_common.tf
@@ -5,6 +5,8 @@ module "snet_selfcare_be_common" {
   resource_group_name  = data.azurerm_virtual_network.vnet_common.resource_group_name
   virtual_network_name = data.azurerm_virtual_network.vnet_common.name
 
+  address_prefixes = [var.cidr_subnet_selfcare_be]
+
   private_endpoint_network_policies_enabled = false
   service_endpoints = [
     "Microsoft.Web",
