11dc3fbd8f5ddc64079523ee515a49a519a5dacd
Krusty93 <andrea.grillo@pagopa.it>
2025-07-11 16:22:34 +0200

    remove unused data

--- src/domains/citizen-auth-app/02_security.tf
+++ src/domains/citizen-auth-app/02_security.tf
@@ -1,8 +1,3 @@
-data "azurerm_key_vault" "kv" {
-  name                = "${local.product}-${var.domain}-kv"
-  resource_group_name = "${local.product}-${var.domain}-sec-rg"
-}
-
 data "azurerm_key_vault" "kv_common" {
   name                = "${local.product}-kv-common"
   resource_group_name = "${local.product}-rg-common"
@@ -11,14 +6,4 @@ data "azurerm_key_vault" "kv_common" {
 data "azurerm_key_vault" "auth" {
   name                = "${local.short_project_itn}-kv-01"
   resource_group_name = "${local.short_project_itn}-main-rg-01"
-}
-
-data "azurerm_key_vault_certificate_data" "lollipop_certificate_v1" {
-  name         = "lollipop-certificate-v1"
-  key_vault_id = data.azurerm_key_vault.kv.id
-}
-
-data "azurerm_key_vault" "ioweb_kv" {
-  name                = format("%s-ioweb-kv", local.product)
-  resource_group_name = format("%s-ioweb-sec-rg", local.product)
 }
--- src/domains/citizen-auth-app/06_storage.tf
+++ src/domains/citizen-auth-app/06_storage.tf
@@ -1,8 +1,3 @@
-data "azurerm_storage_account" "citizen_auth_common" {
-  name                = replace(format("%s-weu-%s-st", local.product, var.domain), "-", "")
-  resource_group_name = format("%s-%s-data-rg", local.product, var.domain)
-}
-
 data "azurerm_storage_account" "lollipop_assertion_storage" {
   name                = replace(format("%s-lollipop-assertions-st", local.product), "-", "")
   resource_group_name = format("%s-%s-data-rg", local.product, var.domain)
@@ -17,30 +12,3 @@ data "azurerm_storage_account" "push_notifications_storage" {
   name                = replace(format("%s-com-st-01", local.common_project_itn), "-", "")
   resource_group_name = format("%s-com-rg-01", local.common_project_itn)
 }
-
-# Storages used by fn-profile
-data "azurerm_storage_account" "storage_api" {
-  name                = replace(format("%s-st-api", local.product), "-", "")
-  resource_group_name = data.azurerm_resource_group.rg_internal.name
-}
-
-data "azurerm_storage_account" "assets_cdn" {
-  name                = replace(format("%s-st-cdn-assets", local.product), "-", "")
-  resource_group_name = data.azurerm_resource_group.rg_common.name
-}
-
-data "azurerm_storage_account" "notifications" {
-  name                = replace(format("%s-st-notifications", local.product), "-", "")
-  resource_group_name = data.azurerm_resource_group.rg_internal.name
-}
-
-data "azurerm_storage_account" "iopstapp" {
-  name                = replace(format("%s-st-app", local.product), "-", "")
-  resource_group_name = data.azurerm_resource_group.rg_internal.name
-}
-
-data "azurerm_storage_account" "storage_apievents" {
-  name                = replace(format("%s-st-api-events", local.product), "-", "")
-  resource_group_name = data.azurerm_resource_group.rg_internal.name
-}
-#################################
