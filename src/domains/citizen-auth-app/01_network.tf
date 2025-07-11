dfd9b6a2ffe5f378c894e4210c91738b4d871a2d
Andrea Grillo <andrea.grillo@pagopa.it>
2025-07-11 16:41:28 +0200

    Remove unused data sources from citizen-auth-app (#1584)

--- src/domains/citizen-auth-app/01_network.tf
+++ src/domains/citizen-auth-app/01_network.tf
@@ -3,28 +3,8 @@ data "azurerm_virtual_network" "vnet_common" {
   resource_group_name = local.vnet_common_resource_group_name
 }
 
-data "azurerm_private_dns_zone" "internal" {
-  name                = local.internal_dns_zone_name
-  resource_group_name = local.internal_dns_zone_resource_group_name
-}
-
 data "azurerm_private_dns_zone" "privatelink_azurewebsites_net" {
   name                = "privatelink.azurewebsites.net"
-  resource_group_name = format("%s-rg-common", local.product)
-}
-
-data "azurerm_private_dns_zone" "privatelink_blob_core_windows_net" {
-  name                = "privatelink.blob.core.windows.net"
-  resource_group_name = format("%s-rg-common", local.product)
-}
-
-data "azurerm_private_dns_zone" "privatelink_queue_core_windows_net" {
-  name                = "privatelink.queue.core.windows.net"
-  resource_group_name = format("%s-rg-common", local.product)
-}
-
-data "azurerm_private_dns_zone" "privatelink_file_core_windows_net" {
-  name                = "privatelink.file.core.windows.net"
   resource_group_name = format("%s-rg-common", local.product)
 }
 
@@ -33,34 +13,11 @@ data "azurerm_private_dns_zone" "privatelink_table_core_windows_net" {
   resource_group_name = format("%s-rg-common", local.product)
 }
 
-data "azurerm_private_dns_zone" "privatelink_documents_azure_com" {
-  name                = "privatelink.documents.azure.com"
-  resource_group_name = format("%s-rg-common", local.product)
-}
-
-data "azurerm_private_dns_zone" "privatelink_mongo_cosmos_azure_com" {
-  name                = "privatelink.mongo.cosmos.azure.com"
-  resource_group_name = format("%s-rg-common", local.product)
-}
-
 data "azurerm_subnet" "private_endpoints_subnet" {
   name                 = "pendpoints"
   virtual_network_name = local.vnet_common_name
   resource_group_name  = local.vnet_common_resource_group_name
 }
-
-data "azurerm_subnet" "app_backend_l1_snet" {
-  name                 = "appbackendl1"
-  virtual_network_name = local.vnet_common_name
-  resource_group_name  = local.vnet_common_resource_group_name
-}
-
-data "azurerm_subnet" "app_backend_l2_snet" {
-  name                 = "appbackendl2"
-  virtual_network_name = local.vnet_common_name
-  resource_group_name  = local.vnet_common_resource_group_name
-}
-
 
 data "azurerm_subnet" "apim_itn_snet" {
   name                 = "io-p-itn-apim-snet-01"
