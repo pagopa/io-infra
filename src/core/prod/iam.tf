3573c326ead0d14f7fd5119213865936441e0dc0
Krusty93 <andrea.grillo@pagopa.it>
2025-07-28 13:19:03 +0200

    add policies

--- src/core/prod/iam.tf
+++ src/core/prod/iam.tf
@@ -67,3 +67,19 @@ resource "azurerm_role_assignment" "dashboards_wallet_devs" {
   role_definition_name = "PagoPA Opex Dashboards Contributor"
   description          = "Allow IO Wallet devs to manage dashboards"
 }
+
+resource "azurerm_key_vault_access_policy" "kv_common_infra_ci" {
+  key_vault_id = module.key_vault_weu.kv_common.id
+  object_id    = data.azurerm_user_assigned_identity.managed_identity_io_infra_ci.principal_id
+  tenant_id    = data.azurerm_client_config.current.tenant_id
+
+  secret_permissions = ["Get", "List"]
+}
+
+resource "azurerm_key_vault_access_policy" "kv_common_infra_cd" {
+  key_vault_id = module.key_vault_weu.kv_common.id
+  object_id    = data.azurerm_user_assigned_identity.managed_identity_io_infra_cd.principal_id
+  tenant_id    = data.azurerm_client_config.current.tenant_id
+
+  secret_permissions = ["Get", "List", "Set", "Delete"]
+}
