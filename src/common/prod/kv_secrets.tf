19baa47cc8b846d9a632e90c1fd2f78fa4803e98
Andrea Grillo <andrea.grillo@pagopa.it>
2025-07-28 11:36:13 +0200

    export cosmos api secrets to common kv (#1603)

---
+++ src/common/prod/kv_secrets.tf
@@ -0,0 +1,15 @@
+resource "azurerm_key_vault_secret" "cosmos_api_connection_string" {
+  name         = "cosmos-api-connection-string"
+  key_vault_id = local.core.key_vault.weu.kv_common.id
+  value        = module.cosmos_api_weu.connection_string
+
+  tags = local.tags
+}
+
+resource "azurerm_key_vault_secret" "cosmos_api_primary_key" {
+  name         = "cosmos-api-primary-key"
+  key_vault_id = local.core.key_vault.weu.kv_common.id
+  value        = module.cosmos_api_weu.primary_key
+
+  tags = local.tags
+}
