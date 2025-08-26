d4e7e26a53f135ddf215e27d49ad24d7a39934ff
Stefano D'Aversa <stefano.daversa@pagopa.it>
2025-08-26 13:13:43 +0200

    [#IOPLT-1229] migrate function-services to itn (#1616)
    create/import resources in itn for functions-services migration
    
    ---------
    
    Co-authored-by: Daniele Manni <BurnedMarshal@users.noreply.github.com>

--- src/common/prod/italynorth.tf
+++ src/common/prod/italynorth.tf
@@ -203,3 +203,32 @@ module "application_gateway_itn" {
 
   tags = local.tags
 }
+
+module "function_app_services" {
+  source                              = "../_modules/function_services/function-app"
+  prefix                              = local.prefix
+  env_short                           = local.env_short
+  function_services_kind              = local.function_services.function_services_kind
+  function_services_sku_tier          = local.function_services.function_services_sku_tier
+  function_services_sku_size          = local.function_services.function_services_sku_size
+  function_services_autoscale_minimum = local.function_services.function_services_autoscale_minimum
+  function_services_autoscale_maximum = local.function_services.function_services_autoscale_maximum
+  function_services_autoscale_default = local.function_services.function_services_autoscale_default
+  vnet_common_name_itn                = local.function_services.vnet_common_name_itn
+  common_resource_group_name_itn      = local.function_services.common_resource_group_name_itn
+  project_itn                         = local.project_itn
+  services_snet_cidr                  = local.function_services.cidr_subnet_services
+  tags                                = local.tags
+}
+
+module "containers_services" {
+  source              = "../_modules/function_services/containers"
+  cosmos_db_name      = module.function_app_services.db_name
+  resource_group_name = local.resource_groups.weu.internal
+  legacy_project      = local.project_weu_legacy
+}
+
+import {
+  to = module.containers_services.module.db_subscription_cidrs_container.azurerm_cosmosdb_sql_container.this
+  id = "/subscriptions/ec285037-c673-4f58-b594-d7c480da4e8b/resourceGroups/io-p-rg-internal/providers/Microsoft.DocumentDB/databaseAccounts/io-p-cosmos-api/sqlDatabases/db/containers/subscription-cidrs"
+}
