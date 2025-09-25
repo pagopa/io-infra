0dffe61be43dae937c3d743da0ad0e64ef1af8f7
Matteo Anselmi <matteo.anselmi@bitrock.it>
2025-09-25 13:34:42 +0200

    align permissions order

--- src/core/prod/iam.tf
+++ src/core/prod/iam.tf
@@ -83,7 +83,7 @@ resource "azurerm_key_vault_access_policy" "kv_common_infra_cd" {
   object_id    = data.azurerm_user_assigned_identity.managed_identity_io_infra_cd.principal_id
   tenant_id    = data.azurerm_client_config.current.tenant_id
 
-  secret_permissions      = ["Get", "List", "Set", "Delete"]
+  secret_permissions      = ["Get", "List", "Delete", "Set"]
   certificate_permissions = ["Get", "List"]
   key_permissions         = ["Get", "List"]
 }
