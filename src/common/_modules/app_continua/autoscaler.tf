e1a08c0a838cc0c3028d0348ac52aab79b0daf3a
Stefano D'Aversa <stefano.daversa@pagopa.it>
2025-10-02 15:51:37 +0200

    fix name

--- src/common/_modules/app_continua/autoscaler.tf
+++ src/common/_modules/app_continua/autoscaler.tf
@@ -1,5 +1,5 @@
 resource "azurerm_monitor_autoscale_setting" "appservice_continua" {
-  name                = format("%s-autoscale", module.appservice_continua_itn.app_service.name)
+  name                = format("%s-autoscale", module.appservice_continua_itn.app_service.app_service.name)
   resource_group_name = azurerm_resource_group.continua_itn_rg.name
   location            = var.location_itn
   target_resource_id  = module.appservice_continua_itn.app_service.plan.id
