f7fc032576c7fbaa9a284343a3edaea55741f5fd
Matteo Anselmi <matteo.anselmi@bitrock.it>
2025-10-08 14:53:26 +0200

    create new tfappprodio and tfappprodio storage accounts

--- src/core/_modules/storage_accounts/terraform_states.tf
+++ src/core/_modules/storage_accounts/terraform_states.tf
@@ -13,3 +13,35 @@ resource "azurerm_storage_account" "terraform" {
 
   tags = var.tags
 }
+
+resource "azurerm_storage_account" "tfinfprodio" {
+  name                     = replace("${var.project}tfinfprodio", "-", "")
+  resource_group_name      = var.resource_group_name
+  location                 = var.location
+
+  account_tier             = "Standard"
+  access_tier              = "Hot"
+  account_kind             = "StorageV2"
+  account_replication_type = "ZRS" # GZRS not available at the moment in ITN
+
+  allow_nested_items_to_be_public  = false
+  cross_tenant_replication_enabled = false
+
+  tags = var.tags
+}
+
+resource "azurerm_storage_account" "tfappprodio" {
+  name                     = replace("${var.project}tfappprodio", "-", "")
+  resource_group_name      = var.resource_group_name
+  location                 = var.location
+
+  account_tier             = "Standard"
+  access_tier              = "Hot"
+  account_kind             = "StorageV2"
+  account_replication_type = "ZRS" # GZRS not available at the moment in ITN
+
+  allow_nested_items_to_be_public  = false
+  cross_tenant_replication_enabled = false
+
+  tags = var.tags
+}
