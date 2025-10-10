212acc1b2a7644e2908621de1740eef8245e1a01
Matteo Anselmi <matteo.anselmi@bitrock.it>
2025-10-10 10:05:48 +0200

    first commit

---
+++ src/common/_modules/storage_accounts/italynorth.tf
@@ -0,0 +1,31 @@
+resource "azurerm_storage_account" "iopitnstexportdata" {
+
+  name                     = "${var.project}-stexportdata"
+  resource_group_name      = var.resource_group_operations
+  location                 = var.location
+  account_tier             = "Standard"
+  account_replication_type = "ZRS" # GZRS not available at the moment in ITN
+
+  public_network_access_enabled    = true
+  shared_access_key_enabled        = true
+  allow_nested_items_to_be_public  = false
+  cross_tenant_replication_enabled = false
+
+  tags = var.tags
+}
+
+resource "azurerm_storage_account" "iopitnstlogs" {
+
+  name                     = "${var.project}-stlogs"
+  resource_group_name      = var.resource_group_operations
+  location                 = var.location
+  account_tier             = "Standard"
+  account_replication_type = "ZRS" # GZRS not available at the moment in ITN
+
+  blob_properties {
+    versioning_enabled  = true
+    change_feed_enabled = true
+  }
+
+  tags = var.tags
+}
