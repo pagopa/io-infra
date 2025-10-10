f1d6e3710082cc52f3e23ae28703399887aa84e0
Matteo Anselmi <matteo.anselmi@bitrock.it>
2025-10-10 11:17:11 +0200

    fix storage account naming, add cdn assets

--- src/common/_modules/storage_accounts/italynorth.tf
+++ src/common/_modules/storage_accounts/italynorth.tf
@@ -1,6 +1,6 @@
 resource "azurerm_storage_account" "iopitnstexportdata" {
 
-  name                     = "${var.project}-stexportdata"
+  name                     = replace("${var.project}stexportdata", "-", "")
   resource_group_name      = var.resource_group_operations
   location                 = var.location
   account_tier             = "Standard"
@@ -9,18 +9,22 @@ resource "azurerm_storage_account" "iopitnstexportdata" {
   public_network_access_enabled    = true
   shared_access_key_enabled        = true
   allow_nested_items_to_be_public  = false
-  cross_tenant_replication_enabled = false
+  cross_tenant_replication_enabled = true
 
   tags = var.tags
 }
 
 resource "azurerm_storage_account" "iopitnstlogs" {
 
-  name                     = "${var.project}-stlogs"
+  name                     = replace("${var.project}stlogs", "-", "")
   resource_group_name      = var.resource_group_operations
   location                 = var.location
   account_tier             = "Standard"
   account_replication_type = "ZRS" # GZRS not available at the moment in ITN
+
+  public_network_access_enabled   = true
+  allow_nested_items_to_be_public = true
+  large_file_share_enabled        = true
 
   blob_properties {
     versioning_enabled  = true
