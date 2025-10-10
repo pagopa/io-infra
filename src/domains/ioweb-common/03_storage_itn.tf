8e75c1f46fe5315ae9e9a44f859b61714d100277
Matteo Anselmi <matteo.anselmi@bitrock.it>
2025-10-10 11:41:08 +0200

    add iowebportalsa

---
+++ src/domains/ioweb-common/03_storage_itn.tf
@@ -0,0 +1,20 @@
+resource "azurerm_storage_account" "iopitniowebportalsa" {
+
+  name                     = replace("${local.project_itn}iowebportalsa", "-", "")
+  resource_group_name      = data.azurerm_resource_group.storage_rg.name
+  location                 = locals.itn_location
+  account_tier             = "Standard"
+  account_replication_type = "ZRS" # GZRS not available at the moment in ITN
+
+  public_network_access_enabled    = true
+  shared_access_key_enabled        = true
+  allow_nested_items_to_be_public  = true
+  cross_tenant_replication_enabled = false
+  large_file_share_enabled = true
+
+  blob_properties {
+    versioning_enabled = true
+  }
+
+  tags = var.tags
+}
