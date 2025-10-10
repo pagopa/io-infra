5adb6c5b528dabf61900a3737e708a23cbe1081c
Matteo Anselmi <matteo.anselmi@bitrock.it>
2025-10-10 12:34:43 +0200

    update pre commit

--- src/common/_modules/storage_accounts/italynorth.tf
+++ src/common/_modules/storage_accounts/italynorth.tf
@@ -33,3 +33,24 @@ resource "azurerm_storage_account" "iopitnstlogs" {
 
   tags = var.tags
 }
+
+resource "azurerm_storage_account" "iopitnstcdnassets" {
+
+  name                     = replace("${var.project}stcdnassets", "-", "")
+  resource_group_name      = var.resource_group_operations
+  location                 = var.location
+  account_tier             = "Standard"
+  account_replication_type = "ZRS" # GZRS not available at the moment in ITN
+
+  public_network_access_enabled    = true
+  allow_nested_items_to_be_public  = true
+  large_file_share_enabled         = true
+  cross_tenant_replication_enabled = true
+
+  blob_properties {
+    versioning_enabled  = true
+    change_feed_enabled = false
+  }
+
+  tags = var.tags
+}
