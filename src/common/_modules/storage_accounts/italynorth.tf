14ac25d34d23a128a0564dde1700ef1466e1ff80
Matteo Anselmi <matteo.anselmi@bitrock.it>
2025-10-13 11:17:12 +0200

    update cross tenant replication

--- src/common/_modules/storage_accounts/italynorth.tf
+++ src/common/_modules/storage_accounts/italynorth.tf
@@ -26,9 +26,10 @@ resource "azurerm_storage_account" "iopitnstlogs" {
   account_tier             = "Standard"
   account_replication_type = "ZRS" # GZRS not available at the moment in ITN
 
-  public_network_access_enabled   = true
-  allow_nested_items_to_be_public = true
-  large_file_share_enabled        = true
+  public_network_access_enabled    = true
+  allow_nested_items_to_be_public  = true
+  large_file_share_enabled         = true
+  cross_tenant_replication_enabled = true
 
   blob_properties {
     versioning_enabled  = true
