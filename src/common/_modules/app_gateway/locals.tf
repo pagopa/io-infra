1455b849148132ac2aeb8c4548684f9e340cc107
Luca Mancosu <luca.mancosu@it.ey.com>
2025-10-15 12:24:38 +0200

    Add io-com path rule

--- src/common/_modules/app_gateway/locals.tf
+++ src/common/_modules/app_gateway/locals.tf
@@ -523,6 +523,11 @@ locals {
           backend               = "platform-api-gateway",
           rewrite_rule_set_name = "rewrite-rule-set-api-app"
         },
+        api-gateway-com = {
+          paths                 = ["/api/com/*"]
+          backend               = "platform-api-gateway",
+          rewrite_rule_set_name = "rewrite-rule-set-api-app"
+        },
         session-manager = {
           paths                 = ["/api/auth/v1/*", "/api/sso/bpd/v1/user", "/api/sso/pagopa/v1/user", "/api/sso/zendesk/v1/jwt"]
           backend               = "platform-api-gateway"
