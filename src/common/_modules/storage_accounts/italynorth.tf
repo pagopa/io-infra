0e5abdedf8b67695c235dee8a46fbd2a6272c909
Matteo Anselmi <29119211+ansmatbitrock@users.noreply.github.com>
2025-10-23 11:27:13 +0200

    Italy North Migration - Create new storage accounts (#1673)
    Create new storage accounts in ITN for the following resources:
    
    - iopstlogs
    - iopstexportdata
    - iopstcdnassets
    
    ---------
    
    Co-authored-by: Daniele Manni <BurnedMarshal@users.noreply.github.com>

---
+++ src/common/_modules/storage_accounts/italynorth.tf
@@ -0,0 +1,100 @@
+resource "azurerm_storage_account" "iopitndataexportst01" {
+
+  count = var.location == "italynorth" ? 1 : 0
+
+  name                     = replace("${var.project}dataexportst01", "-", "")
+  resource_group_name      = var.resource_group_operations
+  location                 = var.location
+  account_tier             = "Standard"
+  account_replication_type = "ZRS" # GZRS not available at the moment in ITN
+
+  public_network_access_enabled    = false
+  shared_access_key_enabled        = true
+  allow_nested_items_to_be_public  = false
+  large_file_share_enabled         = false
+  cross_tenant_replication_enabled = true
+
+  tags = var.tags
+}
+
+resource "azurerm_storage_account" "iopitnlogst01" {
+
+  count = var.location == "italynorth" ? 1 : 0
+
+  name                     = replace("${var.project}logst01", "-", "")
+  resource_group_name      = var.resource_group_operations
+  location                 = var.location
+  account_tier             = "Standard"
+  account_replication_type = "ZRS" # GZRS not available at the moment in ITN
+
+  public_network_access_enabled     = false
+  allow_nested_items_to_be_public   = true
+  large_file_share_enabled          = false
+  cross_tenant_replication_enabled  = true
+  infrastructure_encryption_enabled = true
+
+  blob_properties {
+    versioning_enabled  = true
+    change_feed_enabled = true
+  }
+
+  tags = var.tags
+}
+
+resource "azurerm_storage_management_policy" "iopitnlogst01" {
+
+  count = var.location == "italynorth" ? 1 : 0
+
+  depends_on         = [azurerm_storage_account.iopitnlogst01]
+  storage_account_id = azurerm_storage_account.iopitnlogst01[0].id
+  rule {
+    name    = "insightlogs1year"
+    enabled = true
+    filters {
+      prefix_match = ["insights-activity-logs",
+        "insights-logs-applicationgatewayaccesslog",
+        "insights-logs-auditlogs",
+        "insights-logs-noninteractiveusersigninlogs",
+      "insights-logs-serviceprincipalsigninlogs"]
+      blob_types = ["appendBlob"]
+    }
+    actions {
+      base_blob {
+        delete_after_days_since_modification_greater_than = 365
+      }
+    }
+  }
+}
+
+resource "azurerm_storage_encryption_scope" "iopitnlogst01" {
+
+  count = var.location == "italynorth" ? 1 : 0
+
+  depends_on         = [azurerm_storage_account.iopitnlogst01]
+  name               = "logsencryption"
+  storage_account_id = azurerm_storage_account.iopitnlogst01[0].id
+  source             = "Microsoft.Storage"
+}
+
+resource "azurerm_storage_account" "iopitncdnassetsst01" {
+
+  count = var.location == "italynorth" ? 1 : 0
+
+  name                     = replace("${var.project}cdnassetsst01", "-", "")
+  resource_group_name      = var.resource_group_operations
+  location                 = var.location
+  account_tier             = "Standard"
+  account_replication_type = "ZRS" # GZRS not available at the moment in ITN
+
+  public_network_access_enabled    = false
+  allow_nested_items_to_be_public  = true
+  large_file_share_enabled         = false
+  cross_tenant_replication_enabled = true
+
+  blob_properties {
+    versioning_enabled  = true
+    change_feed_enabled = false
+  }
+
+  tags = var.tags
+}
