79489e36cacf3df672f1a1b89950727eed5a3b4e
salvatorelaiso <salvatorelaiso@gmail.com>
2025-10-24 17:12:37 +0200

    fix: reactivate UserDataDeleteOrchestratorV2

--- src/domains/functions/function_admin.tf
+++ src/domains/functions/function_admin.tf
@@ -225,8 +225,7 @@ module "function_admin" {
   app_settings = merge(
     local.function_admin.app_settings_common, {
       "AzureWebJobs.CheckXmlCryptoCVESamlResponse.Disabled"      = "1",
-      "AzureWebJobs.CheckIoWebXmlCryptoCVESamlResponse.Disabled" = "1",
-      "AzureWebJobs.UserDataDeleteOrchestratorV2.Disabled"       = "1" // TODO: remove after `profileEmails` ITN migration
+      "AzureWebJobs.CheckIoWebXmlCryptoCVESamlResponse.Disabled" = "1"
     }
   )
 
@@ -262,8 +261,7 @@ module "function_admin" {
     "AzureWebJobs.UserDataProcessingTrigger.Disabled",
     "AzureWebJobs.SanitizeProfileEmail.Disabled",
     "AzureWebJobs.CheckXmlCryptoCVESamlResponse.Disabled",
-    "AzureWebJobs.CheckIoWebXmlCryptoCVESamlResponse.Disabled",
-    "AzureWebJobs.UserDataDeleteOrchestratorV2.Disabled" // TODO: remove after `profileEmails` ITN migration
+    "AzureWebJobs.CheckIoWebXmlCryptoCVESamlResponse.Disabled"
   ]
 
   tags = var.tags
