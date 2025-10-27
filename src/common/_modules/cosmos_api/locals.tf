1648d5fa31f854c0a345160dfa7a33e63e829c2c
salvatorelaiso <salvatorelaiso@gmail.com>
2025-10-27 12:35:16 +0100

    fix: add partition key version and default TTL

--- src/common/_modules/cosmos_api/locals.tf
+++ src/common/_modules/cosmos_api/locals.tf
@@ -130,8 +130,10 @@ locals {
       }
     },
     {
-      name               = "profile-emails-uniqueness-leases-itn-002",
-      partition_key_path = "/id",
+      name                  = "profile-emails-uniqueness-leases-itn-002",
+      partition_key_path    = "/id",
+      partition_key_version = null
+      default_ttl           = -1
       autoscale_settings = {
         max_throughput = 2000
       }
