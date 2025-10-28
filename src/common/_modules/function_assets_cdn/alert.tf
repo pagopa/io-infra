46492b8da8b3690fcf96209967d3b15ae49fa5d7
Stefano D'Aversa <stefano.daversa@pagopa.it>
2025-10-28 10:57:00 +0100

    change alert scopes and revert metrci_namespace

--- src/common/_modules/function_assets_cdn/alert.tf
+++ src/common/_modules/function_assets_cdn/alert.tf
@@ -1,14 +1,14 @@
 resource "azurerm_monitor_metric_alert" "function_assets_health_check" {
   name                = "${module.function_assets_cdn_itn.function_app.function_app.name}-health-check-failed"
   resource_group_name = azurerm_resource_group.function_assets_cdn_itn_rg.name
-  scopes              = [module.function_assets_cdn_itn.function_app.plan.id]
+  scopes              = [module.function_assets_cdn_itn.id]
   description         = "${module.function_assets_cdn_itn.function_app.function_app.name} health check failed"
   severity            = 1
   frequency           = "PT5M"
   auto_mitigate       = false
 
   criteria {
-    metric_namespace = "Microsoft.Web/serverFarms"
+    metric_namespace = "Microsoft.Web/sites"
     metric_name      = "HealthCheckStatus"
     aggregation      = "Average"
     operator         = "LessThan"
@@ -23,14 +23,14 @@ resource "azurerm_monitor_metric_alert" "function_assets_health_check" {
 resource "azurerm_monitor_metric_alert" "function_assets_http_server_errors" {
   name                = "${module.function_assets_cdn_itn.function_app.function_app.name}-http-server-errors"
   resource_group_name = azurerm_resource_group.function_assets_cdn_itn_rg.name
-  scopes              = [module.function_assets_cdn_itn.function_app.plan.id]
+  scopes              = [module.function_assets_cdn_itn.id]
   description         = "${module.function_assets_cdn_itn.function_app.function_app.name} http server errors"
   severity            = 1
   frequency           = "PT5M"
   auto_mitigate       = false
 
   criteria {
-    metric_namespace = "Microsoft.Web/serverFarms"
+    metric_namespace = "Microsoft.Web/sites"
     metric_name      = "Http5xx"
     aggregation      = "Total"
     operator         = "GreaterThan"
@@ -45,14 +45,14 @@ resource "azurerm_monitor_metric_alert" "function_assets_http_server_errors" {
 resource "azurerm_monitor_metric_alert" "function_assets_response_time" {
   name                = "${module.function_assets_cdn_itn.function_app.function_app.name}-response-time"
   resource_group_name = azurerm_resource_group.function_assets_cdn_itn_rg.name
-  scopes              = [module.function_assets_cdn_itn.function_app.plan.id]
+  scopes              = [module.function_assets_cdn_itn.id]
   description         = "${module.function_assets_cdn_itn.function_app.function_app.name} response time is greater than 0.5s"
   severity            = 1
   frequency           = "PT5M"
   auto_mitigate       = false
 
   criteria {
-    metric_namespace = "Microsoft.Web/serverFarms"
+    metric_namespace = "Microsoft.Web/sites"
     metric_name      = "HttpResponseTime"
     aggregation      = "Average"
     operator         = "GreaterThan"
