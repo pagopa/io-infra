33639050148b16d7dd333940da3e74d622925e6f
Stefano D'Aversa <stefano.daversa@pagopa.it>
2025-10-28 18:09:05 +0100

    move import to root

--- src/common/_modules/function_elt/storage_account_elt.tf
+++ src/common/_modules/function_elt/storage_account_elt.tf
@@ -37,10 +37,7 @@ resource "azurerm_storage_container" "messages_step_final_itn" {
   container_access_type = "private"
 }
 
-import {
-  to = azurerm_storage_container.messages_step_final_itn
-  id = "/subscriptions/ec285037-c673-4f58-b594-d7c480da4e8b/resourceGroups/io-p-itn-elt-rg-01/providers/Microsoft.Storage/storageAccounts/iopitneltst02/blobServices/default/containers/messages-report-step-final"
-}
+
 
 resource "azurerm_storage_container" "messages_report_step1_itn" {
   name                  = "messages-report-step1"
@@ -48,67 +45,45 @@ resource "azurerm_storage_container" "messages_report_step1_itn" {
   container_access_type = "private"
 }
 
-import {
-  to = azurerm_storage_container.messages_report_step1_itn
-  id = "/subscriptions/ec285037-c673-4f58-b594-d7c480da4e8b/resourceGroups/io-p-itn-elt-rg-01/providers/Microsoft.Storage/storageAccounts/iopitneltst02/blobServices/default/containers/messages-report-step1"
-}
+
 
 resource "azurerm_storage_table" "fnelterrors_itn" {
   name                 = "fnelterrors"
   storage_account_name = module.storage_account_itn_elt.name
 }
 
-import {
-  to = azurerm_storage_table.fnelterrors_itn
-  id = "https://iopitneltst02.table.core.windows.net/Tables('fnelterrors')"
-}
+
 
 resource "azurerm_storage_table" "fnelterrors_messages_itn" {
   name                 = "fnelterrorsMessages"
   storage_account_name = module.storage_account_itn_elt.name
 }
 
-import {
-  to = azurerm_storage_table.fnelterrors_messages_itn
-  id = "https://iopitneltst02.table.core.windows.net/Tables('fnelterrorsMessages')"
-}
+
 
 resource "azurerm_storage_table" "fnelterrors_message_status_itn" {
   name                 = "fnelterrorsMessageStatus"
   storage_account_name = module.storage_account_itn_elt.name
 }
 
-import {
-  to = azurerm_storage_table.fnelterrors_message_status_itn
-  id = "https://iopitneltst02.table.core.windows.net/Tables('fnelterrorsMessageStatus')"
-}
+
 
 resource "azurerm_storage_table" "fnelterrors_notification_status_itn" {
   name                 = "fnelterrorsNotificationStatus"
   storage_account_name = module.storage_account_itn_elt.name
 }
 
-import {
-  to = azurerm_storage_table.fnelterrors_notification_status_itn
-  id = "https://iopitneltst02.table.core.windows.net/Tables('fnelterrorsNotificationStatus')"
-}
+
 
 resource "azurerm_storage_table" "fneltcommands_itn" {
   name                 = "fneltcommands"
   storage_account_name = module.storage_account_itn_elt.name
 }
 
-import {
-  to = azurerm_storage_table.fneltcommands_itn
-  id = "https://iopitneltst02.table.core.windows.net/Tables('fneltcommands')"
-}
+
 
 resource "azurerm_storage_table" "fneltexports_itn" {
   name                 = "fneltexports"
   storage_account_name = module.storage_account_itn_elt.name
 }
 
-import {
-  to = azurerm_storage_table.fneltexports_itn
-  id = "https://iopitneltst02.table.core.windows.net/Tables('fneltexports')"
-}
