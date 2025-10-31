7b0f322284fca8106730573e27fca8d3068405e7
Daniele Manni <dan.manni@gmail.com>
2025-10-31 09:34:37 +0100

    [#IOPLT-1393] Scale up fn-services with new instance

--- src/common/_modules/function_services/function-app/autoscaler.tf
+++ src/common/_modules/function_services/function-app/autoscaler.tf
@@ -102,3 +102,75 @@ resource "azurerm_monitor_autoscale_setting" "function_services_autoscale" {
     }
   }
 }
+
+
+module "function_services_autoscale" {
+  source  = "pagopa-dx/azure-app-service-plan-autoscaler/azurerm"
+  version = "~> 2.0"
+
+  resource_group_name = module.function_services.function_app.resource_group_name
+  location            = var.location
+  app_service_plan_id = module.function_services.function_app.plan.id
+  target_service = {
+    function_apps = [
+      {
+        name = module.function_services.function_app.function_app.name
+      }
+    ]
+  }
+
+  scheduler = {
+    normal_load = {
+      minimum = 4
+      default = 10
+    },
+    maximum = 30
+  }
+
+  scale_metrics = {
+    requests = {
+      #################
+      # scale out
+      #################
+      statistic_increase   = "Max"
+      time_window_increase = 1
+      time_aggregation     = "Maximum"
+      upper_threshold      = 2500
+      increase_by          = 2
+      cooldown_increase    = 3
+      #################
+      # scale in
+      #################
+      statistic_decrease        = "Average"
+      time_window_decrease      = 5
+      time_aggregation_decrease = "Average"
+      lower_threshold           = 500
+      decrease_by               = 1
+      cooldown_decrease         = 3
+    }
+    cpu = {
+      #################
+      # scale out
+      #################
+      statistic_increase        = "Max"
+      time_window_increase      = 1
+      time_aggregation_increase = "Maximum"
+      upper_threshold           = 40
+      increase_by               = 2
+      cooldown_increase         = 3
+      #################
+      # scale in
+      #################
+      statistic_decrease        = "Average"
+      time_window_decrease      = 5
+      time_aggregation_decrease = "Average"
+      lower_threshold           = 15
+      decrease_by               = 1
+      cooldown_decrease         = 3
+    }
+    memory = null
+  }
+
+  tags = var.tags
+}
+
--- src/common/_modules/function_services/function-app/main.tf
+++ src/common/_modules/function_services/function-app/main.tf
@@ -63,3 +63,71 @@ module "function_services_dx" {
 
   tags = var.tags
 }
+
+module "function_services" {
+  source  = "pagopa-dx/azure-function-app/azurerm"
+  version = "~> 4.0"
+
+  environment = {
+    prefix          = var.prefix
+    env_short       = var.env_short
+    location        = var.location_itn
+    app_name        = "services"
+    instance_number = "01"
+  }
+
+  size = "P2mv3"
+
+  resource_group_name = "${var.project_itn}-services-rg-01"
+
+  virtual_network = {
+    name                = var.vnet_common_name_itn
+    resource_group_name = var.common_resource_group_name_itn
+  }
+
+  subnet_cidr                          = var.services_snet_cidr
+  health_check_path                    = "/api/info"
+  subnet_pep_id                        = data.azurerm_subnet.private_endpoints_subnet_itn.id
+  private_dns_zone_resource_group_name = data.azurerm_resource_group.weu-common.name
+
+  app_settings = merge(
+    local.function_services.app_settings_common,
+    {
+      # Disabled functions on slot - trigger, queue and timer
+      # mark this configurations as slot settings
+      "AzureWebJobs.CreateNotification.Disabled"     = "0"
+      "AzureWebJobs.EmailNotification.Disabled"      = "0"
+      "AzureWebJobs.OnFailedProcessMessage.Disabled" = "0"
+      "AzureWebJobs.ProcessMessage.Disabled"         = "0"
+      "AzureWebJobs.WebhookNotification.Disabled"    = "0"
+      "APPINSIGHTS_CLOUD_ROLE_NAME"                  = "io-p-itn-services-func-01",
+    }
+  )
+
+  sticky_app_setting_names = [
+    "APPINSIGHTS_CLOUD_ROLE_NAME",
+    "AzureWebJobs.CreateNotification.Disabled",
+    "AzureWebJobs.EmailNotification.Disabled",
+    "AzureWebJobs.OnFailedProcessMessage.Disabled",
+    "AzureWebJobs.ProcessMessage.Disabled",
+    "AzureWebJobs.WebhookNotification.Disabled",
+  ]
+
+  slot_app_settings = merge(
+    local.function_services.app_settings_common,
+    {
+      # Disabled functions on slot - trigger, queue and timer
+      # mark this configurations as slot settings
+      "AzureWebJobs.CreateNotification.Disabled"     = "1"
+      "AzureWebJobs.EmailNotification.Disabled"      = "1"
+      "AzureWebJobs.OnFailedProcessMessage.Disabled" = "1"
+      "AzureWebJobs.ProcessMessage.Disabled"         = "1"
+      "AzureWebJobs.WebhookNotification.Disabled"    = "1"
+      "APPINSIGHTS_CLOUD_ROLE_NAME"                  = "io-p-itn-services-func-01-staging",
+    }
+  )
+
+  action_group_ids = [data.azurerm_monitor_action_group.error_action_group.id]
+
+  tags = var.tags
+}
