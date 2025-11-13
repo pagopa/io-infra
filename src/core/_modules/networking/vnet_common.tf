dd786b36f6d623db1affae5e8fb2942f5e09814e
Daniele Manni <dan.manni@gmail.com>
2025-11-13 09:48:47 +0100

    Configurable ddos protection in network module

--- src/core/_modules/networking/vnet_common.tf
+++ src/core/_modules/networking/vnet_common.tf
@@ -4,10 +4,13 @@ resource "azurerm_virtual_network" "common" {
   location            = var.location
   address_space       = [var.vnet_cidr_block]
 
-  # ddos_protection_plan {
-  #   id     = local.ddos_protection_plan.id
-  #   enable = local.ddos_protection_plan.enable
-  # }
+  dynamic "ddos_protection_plan" {
+    for_each = var.ddos_protection_plan_enabled ? [1] : []
+    content {
+      id     = local.ddos_protection_plan.id
+      enable = var.ddos_protection_plan_enabled
+    }
+  }
 
   tags = var.tags
 }
