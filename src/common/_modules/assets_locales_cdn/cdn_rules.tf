503df7c982278624145cd89307022ff3dcd174b6
Matteo Anselmi <matteo.anselmi@bitrock.it>
2025-11-24 09:47:33 +0100

    convert function proxies to cnd routes, add locales route

--- src/common/_modules/assets_locales_cdn/cdn_rules.tf
+++ src/common/_modules/assets_locales_cdn/cdn_rules.tf
@@ -3,6 +3,46 @@ resource "azurerm_cdn_frontdoor_rule_set" "primary_ruleset" {
   cdn_frontdoor_profile_id = azurerm_cdn_frontdoor_profile.cdn_profile.id
 }
 
+resource "azurerm_cdn_frontdoor_rule" "assistancetoolszendesk" {
+  name                      = "assistancetoolszendesk"
+  cdn_frontdoor_rule_set_id = azurerm_cdn_frontdoor_rule_set.primary_ruleset.id
+  order                     = 5
+
+  conditions {
+    request_uri_condition {
+      operator     = "BeginsWith"
+      match_values = ["/assistanceTools/zendesk.json"]
+    }
+  }
+
+  actions {
+    route_configuration_override_action {
+      cache_behavior = "OverrideAlways"
+      cache_duration = "00:05:00"
+    }
+  }
+}
+
+resource "azurerm_cdn_frontdoor_rule" "bonus" {
+  name                      = "bonus"
+  cdn_frontdoor_rule_set_id = azurerm_cdn_frontdoor_rule_set.primary_ruleset.id
+  order                     = 3
+
+  conditions {
+    request_uri_condition {
+      operator     = "BeginsWith"
+      match_values = ["/bonus"]
+    }
+  }
+
+  actions {
+    route_configuration_override_action {
+      cache_behavior = "OverrideAlways"
+      cache_duration = "00:15:00"
+    }
+  }
+}
+
 resource "azurerm_cdn_frontdoor_rule" "global_cache" {
   name                      = "globalcache"
   cdn_frontdoor_rule_set_id = azurerm_cdn_frontdoor_rule_set.primary_ruleset.id
@@ -44,22 +84,24 @@ resource "azurerm_cdn_frontdoor_rule" "servicesdata" {
   }
 }
 
-resource "azurerm_cdn_frontdoor_rule" "bonus" {
-  name                      = "bonus"
+resource "azurerm_cdn_frontdoor_rule" "sign" {
+  name                      = "sign"
   cdn_frontdoor_rule_set_id = azurerm_cdn_frontdoor_rule_set.primary_ruleset.id
-  order                     = 3
+  order                     = 6
 
   conditions {
     request_uri_condition {
       operator     = "BeginsWith"
-      match_values = ["/bonus"]
+      match_values = ["/sign"]
+      transforms   = ["Lowercase"]
     }
   }
 
   actions {
-    route_configuration_override_action {
-      cache_behavior = "OverrideAlways"
-      cache_duration = "00:15:00"
+    response_header_action {
+      header_action = "Append"
+      header_name   = "Access-Control-Allow-Origin"
+      value         = "*"
     }
   }
 }
@@ -80,48 +122,6 @@ resource "azurerm_cdn_frontdoor_rule" "status" {
     route_configuration_override_action {
       cache_behavior = "OverrideAlways"
       cache_duration = "00:05:00"
-    }
-  }
-}
-
-resource "azurerm_cdn_frontdoor_rule" "assistancetoolszendesk" {
-  name                      = "assistancetoolszendesk"
-  cdn_frontdoor_rule_set_id = azurerm_cdn_frontdoor_rule_set.primary_ruleset.id
-  order                     = 5
-
-  conditions {
-    request_uri_condition {
-      operator     = "BeginsWith"
-      match_values = ["/assistanceTools/zendesk.json"]
-    }
-  }
-
-  actions {
-    route_configuration_override_action {
-      cache_behavior = "OverrideAlways"
-      cache_duration = "00:05:00"
-    }
-  }
-}
-
-resource "azurerm_cdn_frontdoor_rule" "sign" {
-  name                      = "sign"
-  cdn_frontdoor_rule_set_id = azurerm_cdn_frontdoor_rule_set.primary_ruleset.id
-  order                     = 6
-
-  conditions {
-    request_uri_condition {
-      operator     = "BeginsWith"
-      match_values = ["/sign"]
-      transforms   = ["Lowercase"]
-    }
-  }
-
-  actions {
-    response_header_action {
-      header_action = "Append"
-      header_name   = "Access-Control-Allow-Origin"
-      value         = "*"
     }
   }
 }
