fb593c3af4caf2fb32db9901f7eaf0efa0c5a1f8
Matteo Anselmi <matteo.anselmi@bitrock.it>
2025-11-25 10:17:46 +0100

    migrate custom domains to frontdoor

--- src/common/_modules/assets_locales_cdn/custom_domains.tf
+++ src/common/_modules/assets_locales_cdn/custom_domains.tf
@@ -1,29 +1,27 @@
-resource "azurerm_cdn_endpoint_custom_domain" "assets_cdn" {
+resource "azurerm_cdn_frontdoor_custom_domain" "assets_cdn" {
   depends_on = [
     azurerm_dns_cname_record.assets_cdn_io_pagopa_it,
   ]
 
-  name            = replace("${azurerm_dns_cname_record.assets_cdn_io_pagopa_it.name}.${var.public_dns_zones.io.name}", ".", "-")
-  cdn_endpoint_id = azurerm_cdn_endpoint.assets_cdn_endpoint.id
-  host_name       = "${azurerm_dns_cname_record.assets_cdn_io_pagopa_it.name}.${var.public_dns_zones.io.name}"
-  cdn_managed_https {
-    certificate_type = "Dedicated"
-    protocol_type    = "ServerNameIndication"
-    tls_version      = "TLS12"
+  name                     = replace("${azurerm_dns_cname_record.assets_cdn_io_pagopa_it.name}.${var.public_dns_zones.io.name}", ".", "-")
+  cdn_frontdoor_profile_id = azurerm_cdn_frontdoor_endpoint.assets_cdn_endpoint.id
+  host_name                = "${azurerm_dns_cname_record.assets_cdn_io_pagopa_it.name}.${var.public_dns_zones.io.name}"
+  tls {
+    certificate_type    = "Dedicated"
+    minimum_tls_version = "TLS12"
   }
 }
 
-resource "azurerm_cdn_endpoint_custom_domain" "assets_cdn_io_italia_it" {
+resource "azurerm_cdn_frontdoor_custom_domain" "assets_cdn_io_italia_it" {
   depends_on = [
     azurerm_dns_cname_record.assets_cdn_io_italia_it,
   ]
 
-  name            = replace("${azurerm_dns_cname_record.assets_cdn_io_pagopa_it.name}.${var.public_dns_zones.io_italia_it.name}", ".", "-")
-  cdn_endpoint_id = azurerm_cdn_endpoint.assets_cdn_endpoint.id
-  host_name       = "${azurerm_dns_cname_record.assets_cdn_io_pagopa_it.name}.${var.public_dns_zones.io_italia_it.name}"
-  cdn_managed_https {
-    certificate_type = "Dedicated"
-    protocol_type    = "ServerNameIndication"
-    tls_version      = "TLS12"
+  name                     = replace("${azurerm_dns_cname_record.assets_cdn_io_pagopa_it.name}.${var.public_dns_zones.io_italia_it.name}", ".", "-")
+  cdn_frontdoor_profile_id = azurerm_cdn_frontdoor_endpoint.assets_cdn_endpoint.id
+  host_name                = "${azurerm_dns_cname_record.assets_cdn_io_pagopa_it.name}.${var.public_dns_zones.io_italia_it.name}"
+  tls {
+    certificate_type    = "Dedicated"
+    minimum_tls_version = "TLS12"
   }
 }
