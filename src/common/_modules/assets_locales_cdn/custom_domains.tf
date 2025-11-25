9d7b4caa81b2b36b9cee8f4bbcba7621601b45f5
Matteo Anselmi <matteo.anselmi@bitrock.it>
2025-11-25 10:24:33 +0100

    fix certificate type

--- src/common/_modules/assets_locales_cdn/custom_domains.tf
+++ src/common/_modules/assets_locales_cdn/custom_domains.tf
@@ -7,7 +7,7 @@ resource "azurerm_cdn_frontdoor_custom_domain" "assets_cdn" {
   cdn_frontdoor_profile_id = azurerm_cdn_frontdoor_endpoint.cdn_endpoint.id
   host_name                = "${azurerm_dns_cname_record.assets_cdn_io_pagopa_it.name}.${var.public_dns_zones.io.name}"
   tls {
-    certificate_type    = "Dedicated"
+    certificate_type    = "ManagedCertificate"
     minimum_tls_version = "TLS12"
   }
 }
@@ -21,7 +21,7 @@ resource "azurerm_cdn_frontdoor_custom_domain" "assets_cdn_io_italia_it" {
   cdn_frontdoor_profile_id = azurerm_cdn_frontdoor_endpoint.cdn_endpoint.id
   host_name                = "${azurerm_dns_cname_record.assets_cdn_io_pagopa_it.name}.${var.public_dns_zones.io_italia_it.name}"
   tls {
-    certificate_type    = "Dedicated"
+    certificate_type    = "ManagedCertificate"
     minimum_tls_version = "TLS12"
   }
 }
