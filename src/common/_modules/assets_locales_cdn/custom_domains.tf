f0ed1efc9a1d71bdbdb8d690afa8b95e58c6a02b
Matteo Anselmi <matteo.anselmi@bitrock.it>
2025-11-25 10:20:57 +0100

    fix resource reference

--- src/common/_modules/assets_locales_cdn/custom_domains.tf
+++ src/common/_modules/assets_locales_cdn/custom_domains.tf
@@ -4,7 +4,7 @@ resource "azurerm_cdn_frontdoor_custom_domain" "assets_cdn" {
   ]
 
   name                     = replace("${azurerm_dns_cname_record.assets_cdn_io_pagopa_it.name}.${var.public_dns_zones.io.name}", ".", "-")
-  cdn_frontdoor_profile_id = azurerm_cdn_frontdoor_endpoint.assets_cdn_endpoint.id
+  cdn_frontdoor_profile_id = azurerm_cdn_frontdoor_endpoint.cdn_endpoint.id
   host_name                = "${azurerm_dns_cname_record.assets_cdn_io_pagopa_it.name}.${var.public_dns_zones.io.name}"
   tls {
     certificate_type    = "Dedicated"
@@ -18,7 +18,7 @@ resource "azurerm_cdn_frontdoor_custom_domain" "assets_cdn_io_italia_it" {
   ]
 
   name                     = replace("${azurerm_dns_cname_record.assets_cdn_io_pagopa_it.name}.${var.public_dns_zones.io_italia_it.name}", ".", "-")
-  cdn_frontdoor_profile_id = azurerm_cdn_frontdoor_endpoint.assets_cdn_endpoint.id
+  cdn_frontdoor_profile_id = azurerm_cdn_frontdoor_endpoint.cdn_endpoint.id
   host_name                = "${azurerm_dns_cname_record.assets_cdn_io_pagopa_it.name}.${var.public_dns_zones.io_italia_it.name}"
   tls {
     certificate_type    = "Dedicated"
