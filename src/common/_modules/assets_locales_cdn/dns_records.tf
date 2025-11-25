af7b023e122ed9050781187c0fdff853533174ba
Matteo Anselmi <matteo.anselmi@bitrock.it>
2025-11-25 10:10:03 +0100

    fix attribute for endpoint on dns records

--- src/common/_modules/assets_locales_cdn/dns_records.tf
+++ src/common/_modules/assets_locales_cdn/dns_records.tf
@@ -3,7 +3,7 @@ resource "azurerm_dns_cname_record" "assets_cdn_io_pagopa_it" {
   zone_name           = var.public_dns_zones.io.name
   resource_group_name = var.resource_group_external
   ttl                 = var.dns_default_ttl_sec
-  record              = azurerm_cdn_frontdoor_endpoint.cdn_endpoint.fqdn
+  record              = azurerm_cdn_frontdoor_endpoint.cdn_endpoint.host_name
 
   tags = var.tags
 }
@@ -13,7 +13,7 @@ resource "azurerm_dns_cname_record" "assets_cdn_io_italia_it" {
   zone_name           = var.public_dns_zones.io_italia_it.name
   resource_group_name = var.resource_group_external
   ttl                 = var.dns_default_ttl_sec
-  record              = azurerm_cdn_frontdoor_endpoint.cdn_endpoint.fqdn
+  record              = azurerm_cdn_frontdoor_endpoint.cdn_endpoint.host_name
 
   tags = var.tags
 }
