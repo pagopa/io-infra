757bd8d03aa567587e6289d0ed85abdee1695f56
salvatorelaiso <salvatorelaiso@gmail.com>
2026-01-12 14:35:11 +0100

    fix: update uniqueness email table connection

--- src/common/_modules/function_admin/local.tf
+++ src/common/_modules/function_admin/local.tf
@@ -77,8 +77,8 @@ locals {
       LOCKED_PROFILES_STORAGE_CONNECTION_STRING = data.azurerm_storage_account.locked_profiles_storage.primary_connection_string
       LOCKED_PROFILES_TABLE_NAME                = var.function_admin_locked_profiles_table_name
 
-      PROFILE_EMAILS_STORAGE_CONNECTION_STRING = data.azurerm_storage_account.citizen_auth_common.primary_connection_string
-      PROFILE_EMAILS_TABLE_NAME                = "profileEmails"
+      PROFILE_EMAILS_STORAGE_CONNECTION_STRING = data.azurerm_key_vault_secret.common_SESSION_ST_CONNECTION_STRING.value
+      PROFILE_EMAILS_TABLE_NAME                = "profileemails01"
 
       # Instant delete
       INSTANT_DELETE_ENABLED_USERS = join(",", [data.azurerm_key_vault_secret.fn_admin_INSTANT_DELETE_ENABLED_USERS.value, module.tests.users.light])
