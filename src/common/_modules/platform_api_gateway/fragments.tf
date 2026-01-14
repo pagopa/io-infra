8923bc4eac990fe1adf70f0831abab83d03962fa
Daniele Manni <BurnedMarshal@users.noreply.github.com>
2026-01-14 15:23:03 +0100

    Fix Fragment session ioapp (#1758)

--- src/common/_modules/platform_api_gateway/fragments.tf
+++ src/common/_modules/platform_api_gateway/fragments.tf
@@ -2,6 +2,6 @@ resource "azurerm_api_management_policy_fragment" "auth" {
   api_management_id = module.platform_api_gateway.id
   depends_on        = [azurerm_api_management_named_value.session_manager_introspection_url]
   name              = "ioapp-authenticated"
-  format            = "xml"
+  format            = "rawxml"
   value             = file("../_modules/platform_api_gateway/fragments/auth.xml")
 }
