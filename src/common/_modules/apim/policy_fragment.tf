c9d5752fb27d14b1d294fc40aeaef8ab90080750
Daniele Manni <dan.manni@gmail.com>
2026-01-14 09:42:51 +0100

    Refactory auth fragment with local send request

--- src/common/_modules/apim/policy_fragment.tf
+++
@@ -1,7 +0,0 @@
-resource "azurerm_api_management_policy_fragment" "auth" {
-  api_management_id = module.apim_v2.id
-  depends_on        = [azurerm_api_management_named_value.session_manager_baseurl, azurerm_api_management_named_value.session_manager_introspection_url]
-  name              = "ioapp-authenticated"
-  format            = "xml"
-  value             = file("../_modules/apim/api/fragment/auth.xml")
-}
---
+++ src/common/_modules/platform_api_gateway/named_values.tf
@@ -0,0 +1,15 @@
+resource "azurerm_api_management_named_value" "session_manager_introspection_url" {
+  name                = "session-manager-introspection-url"
+  resource_group_name = var.resource_group_internal
+  api_management_name = module.platform_api_gateway.name 
+  display_name        = "session-manager-introspection-url"
+  value               = "/api/auth/v1/user-identity"
+}
+
+resource "azurerm_api_management_named_value" "platform_api_gateway_hostname_internal" {
+  name                = "platform-api-gateway-hostname-internal"
+  resource_group_name = var.resource_group_internal
+  api_management_name = module.platform_api_gateway.name 
+  display_name        = "platform-api-gateway-hostname-internal"
+  value               = "proxy.internal.io.pagopa.it"
+}
