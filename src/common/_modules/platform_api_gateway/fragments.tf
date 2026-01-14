71b866b0c9cb51d32e8eb802f6be1219a2ea35c8
Daniele Manni <BurnedMarshal@users.noreply.github.com>
2026-01-14 15:19:47 +0100

    [#IOPLT-1012] Policy fragment for token instrospection io-proxy (#1475)
    ### Motivation and Context
    Add a new fragment for testing `io-proxy` integration in authenticated
    requests with token introspection for IO app API calls.
    <!--- Why is this change required? What problem does it solve? -->
    
    ### Major Changes
    Add a new APIM policy fragment
    <!--- Describe the major changes introduced by this PR -->
    
    ### Dependencies
    depends on https://github.com/pagopa/io-auth-n-identity-domain/pull/251
    <!--- If this PR depends on or is related to other PRs, 
    list them here using the GitHub syntax: `depends on #123` -->
    
    ### Testing
    not yet
    <!--- Describe the testing steps you have performed -->
    <!--- and/or if this PR is already (partially) applied and why -->
    
    ### Documentation
    
    <!--- Are there any updates to the documentation? -->
    
    ### Other Considerations
    
    <!--- Any additional context, such as breaking changes, security
    concerns, etc. -->
    
    ---------
    
    Co-authored-by: Greta Quadrati <75862507+gquadrati@users.noreply.github.com>

---
+++ src/common/_modules/platform_api_gateway/fragments.tf
@@ -0,0 +1,7 @@
+resource "azurerm_api_management_policy_fragment" "auth" {
+  api_management_id = module.platform_api_gateway.id
+  depends_on        = [azurerm_api_management_named_value.session_manager_introspection_url]
+  name              = "ioapp-authenticated"
+  format            = "xml"
+  value             = file("../_modules/platform_api_gateway/fragments/auth.xml")
+}
