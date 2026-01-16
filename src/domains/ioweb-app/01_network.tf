eb6f37b0771c45e43ec163e6a1a658e04d1b21a0
Matteo Anselmi <matteo.anselmi@bitrock.it>
2026-01-16 12:00:57 +0100

    decommission all remaining AKS references

--- src/domains/ioweb-app/01_network.tf
+++ src/domains/ioweb-app/01_network.tf
@@ -47,14 +47,6 @@ data "azurerm_private_dns_zone" "privatelink_documents_azure_com" {
   resource_group_name = format("%s-rg-common", local.product)
 }
 
-resource "azurerm_private_dns_a_record" "ingress" {
-  name                = local.ingress_hostname
-  zone_name           = data.azurerm_private_dns_zone.internal.name
-  resource_group_name = local.internal_dns_zone_resource_group_name
-  ttl                 = 3600
-  records             = [var.ingress_load_balancer_ip]
-}
-
 data "azurerm_subnet" "private_endpoints_subnet" {
   name                 = "pendpoints"
   virtual_network_name = local.vnet_common_name
