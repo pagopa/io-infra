65898843299d69cb364df0bb5d73d150a51d985a
Daniele Manni <dan.manni@gmail.com>
2026-01-16 16:19:47 +0100

    [#IOPLT-1515] Delete old fn-services p2mv3

--- src/common/prod/italynorth.tf
+++ src/common/prod/italynorth.tf
@@ -219,22 +219,6 @@ module "application_gateway_itn" {
   tags = local.tags
 }
 
-module "function_app_services" {
-  source                              = "../_modules/function_services/function-app"
-  prefix                              = local.prefix
-  env_short                           = local.env_short
-  function_services_autoscale_minimum = local.function_services.function_services_autoscale_minimum
-  function_services_autoscale_maximum = local.function_services.function_services_autoscale_maximum
-  function_services_autoscale_default = local.function_services.function_services_autoscale_default
-  sku_size                            = "P2mv3"
-  vnet_common_name_itn                = local.function_services.vnet_common_name_itn
-  common_resource_group_name_itn      = local.function_services.common_resource_group_name_itn
-  project_itn                         = local.project_itn
-  instance_number                     = "01"
-  services_snet_cidr                  = local.function_services.cidr_subnet_services
-  tags                                = local.tags
-}
-
 module "function_app_services_02" {
   source                              = "../_modules/function_services/function-app"
   prefix                              = local.prefix
