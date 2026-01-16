7e98cfab35bf329423ddc714512ce05cf288e289
Daniele Manni <BurnedMarshal@users.noreply.github.com>
2026-01-16 10:32:50 +0100

    [#IOPLT-1454] Scale-in fn-services post BE (#1738)
    Scale in `fn-services` post Bonus Elec. Future PR will create a new
    fn-services for scale down from `P2mv3` to `P1V3` sku instances
    (commented code).

--- src/common/prod/italynorth.tf
+++ src/common/prod/italynorth.tf
@@ -226,13 +226,30 @@ module "function_app_services" {
   function_services_autoscale_minimum = local.function_services.function_services_autoscale_minimum
   function_services_autoscale_maximum = local.function_services.function_services_autoscale_maximum
   function_services_autoscale_default = local.function_services.function_services_autoscale_default
+  sku_size                            = "P2mv3"
   vnet_common_name_itn                = local.function_services.vnet_common_name_itn
   common_resource_group_name_itn      = local.function_services.common_resource_group_name_itn
   project_itn                         = local.project_itn
-  services_snet_cidr_old              = local.function_services.cidr_subnet_services_old
+  instance_number                     = "01"
   services_snet_cidr                  = local.function_services.cidr_subnet_services
   tags                                = local.tags
 }
+
+# module "function_app_services_02" {
+#   source                              = "../_modules/function_services/function-app"
+#   prefix                              = local.prefix
+#   env_short                           = local.env_short
+#   function_services_autoscale_minimum = local.function_services.function_services_autoscale_minimum
+#   function_services_autoscale_maximum = local.function_services.function_services_autoscale_maximum
+#   function_services_autoscale_default = local.function_services.function_services_autoscale_default
+#   sku_size                            = "P1v3"
+#   vnet_common_name_itn                = local.function_services.vnet_common_name_itn
+#   instance_number                     = "02"
+#   common_resource_group_name_itn      = local.function_services.common_resource_group_name_itn
+#   project_itn                         = local.project_itn
+#   services_snet_cidr                  = local.function_services.cidr_subnet_services_02
+#   tags                                = local.tags
+# }
 
 module "containers_services" {
   source              = "../_modules/function_services/containers"
