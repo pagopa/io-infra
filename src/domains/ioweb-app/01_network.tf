b553b875a1778ae3d237332dde65a2993c193601
Matteo Anselmi <matteo.anselmi@bitrock.it>
2026-01-21 17:12:08 +0100

    remove unused data

--- src/domains/ioweb-app/01_network.tf
+++ src/domains/ioweb-app/01_network.tf
@@ -7,11 +7,6 @@ data "azurerm_application_gateway" "app_gateway" {
 }
 ##########################
 
-data "azurerm_virtual_network" "vnet" {
-  name                = local.vnet_name
-  resource_group_name = local.vnet_resource_group_name
-}
-
 data "azurerm_virtual_network" "vnet_common" {
   name                = local.vnet_common_name
   resource_group_name = local.vnet_common_resource_group_name
