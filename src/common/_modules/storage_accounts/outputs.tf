a4d172371530bae84f24ab0c948ed631f37fd5b4
Matteo Anselmi <matteo.anselmi@bitrock.it>
2026-01-22 11:08:56 +0100

    enable storage account output for itn

--- src/common/_modules/storage_accounts/outputs.tf
+++ src/common/_modules/storage_accounts/outputs.tf
@@ -21,3 +21,10 @@ output "logs_primary_connection_string" {
   value     = try(azurerm_storage_account.logs[0].primary_connection_string, null)
   sensitive = true
 }
+
+output "logs_itn" {
+  value = {
+    id   = try(azurerm_storage_account.logs[1].id, null)
+    name = try(azurerm_storage_account.logs[1].name, null)
+  }
+}
