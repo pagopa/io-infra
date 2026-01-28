f203357155e141a34253d0baa5fec463fc8e4642
Gianmaria Scorza <gianmaria@scorza.dev>
2026-01-28 17:30:34 +0100

    remove messages-report-step1 no longer needed storage container

--- src/common/_modules/function_elt/data.tf
+++ src/common/_modules/function_elt/data.tf
@@ -171,11 +171,6 @@ data "azurerm_storage_table" "fneltexports" {
 
 # Storage Containers
 
-data "azurerm_storage_container" "container_messages_report_step1" {
-  name                 = "messages-report-step1"
-  storage_account_name = module.storage_account_itn_elt.name
-}
-
 data "azurerm_storage_container" "container_messages_report_step_final" {
   name                 = "messages-report-step-final"
   storage_account_name = module.storage_account_itn_elt.name
