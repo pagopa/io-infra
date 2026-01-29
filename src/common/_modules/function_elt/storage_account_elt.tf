75036c0e7e4adab9aab262c4fc275320b0032fde
Gianmaria Scorza <gianmaria@scorza.dev>
2026-01-29 16:59:09 +0100

    Revert "removed pdnd-io-cosmosdb-messagestatus-failure no longer needed storage queue"
    This reverts commit 4ed398802eaad2eb38f073aaa153541ee387ce1a.

--- src/common/_modules/function_elt/storage_account_elt.tf
+++ src/common/_modules/function_elt/storage_account_elt.tf
@@ -51,6 +51,17 @@ resource "azurerm_storage_queue" "pdnd-io-cosmosdb-messages-failure-poison" {
   name                 = "pdnd-io-cosmosdb-messages-failure-poison"
   storage_account_name = module.storage_account_itn_elt.name
 }
+
+resource "azurerm_storage_queue" "pdnd-io-cosmosdb-messagestatus-failure" {
+  name                 = "pdnd-io-cosmosdb-messagestatus-failure"
+  storage_account_name = module.storage_account_itn_elt.name
+}
+
+resource "azurerm_storage_queue" "pdnd-io-cosmosdb-messagestatus-failure-poison" {
+  name                 = "pdnd-io-cosmosdb-messagestatus-failure-poison"
+  storage_account_name = module.storage_account_itn_elt.name
+}
+
 resource "azurerm_storage_queue" "pdnd-io-cosmosdb-services-failure" {
   name                 = "pdnd-io-cosmosdb-services-failure"
   storage_account_name = module.storage_account_itn_elt.name
