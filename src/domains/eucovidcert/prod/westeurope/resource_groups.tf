6bab58ed6b6cfa6ae3ab7a10261b74cb9e207bae
Daniele Manni <dan.manni@gmail.com>
2026-02-04 14:49:30 +0100

    [#1205] Remove eucovidcert domain

--- src/domains/eucovidcert/prod/westeurope/resource_groups.tf
+++
@@ -1,9 +0,0 @@
-// TODO: Before remove the RG delete all resources inside it not mapped in Terraform
-module "resource_groups" {
-  source = "../../_modules/resource_groups"
-
-  location = local.location
-  project  = local.project
-
-  tags = local.tags
-}
