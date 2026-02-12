93f9f924bd80ff8c044a6b92271c62f3db1134d0
Daniele Manni <BurnedMarshal@users.noreply.github.com>
2026-02-12 10:41:06 +0100

    [#IOPLT-1470] Add internal API endpoint to invalidate session token cache (#1800)

--- src/common/_modules/platform_api_gateway/named_values.tf
+++ src/common/_modules/platform_api_gateway/named_values.tf
@@ -11,5 +11,13 @@ resource "azurerm_api_management_named_value" "platform_api_gateway_hostname_int
   resource_group_name = var.resource_group_internal
   api_management_name = module.platform_api_gateway.name
   display_name        = "platform-api-gateway-hostname-internal"
-  value               = "proxy.internal.io.pagopa.it"
+  value               = local.proxy_hostname_internal
+}
+
+resource "azurerm_api_management_named_value" "session_token_cache_prefix" {
+  name                = "session-token-cache-prefix"
+  resource_group_name = var.resource_group_internal
+  api_management_name = module.platform_api_gateway.name
+  display_name        = "session-token-cache-prefix"
+  value               = "session-"
 }
