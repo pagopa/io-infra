e0208fb5589694091686ca163d0fc5e027b8e97c
Daniele Manni <dan.manni@gmail.com>
2026-02-13 08:47:08 +0100

    [#IOPLT-1029] Add empty token validation in cache policy

--- src/common/_modules/platform_api_gateway/fragments.tf
+++ src/common/_modules/platform_api_gateway/fragments.tf
@@ -10,7 +10,8 @@ resource "azurerm_api_management_policy_fragment" "auth_cache" {
   api_management_id = module.platform_api_gateway.id
   depends_on = [
     azurerm_api_management_named_value.session_manager_introspection_url,
-    azurerm_api_management_named_value.session_token_cache_prefix
+    azurerm_api_management_named_value.session_token_cache_prefix,
+    azurerm_api_management_named_value.platform_api_gateway_hostname_internal
   ]
   name   = "ioapp-authenticated-cache"
   format = "rawxml"
