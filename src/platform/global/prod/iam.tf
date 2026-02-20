bd6fbace35c98b3cf0086bcc3eb879ae1e058d04
Matteo Anselmi <matteo.anselmi@bitrock.it>
2026-02-20 09:23:51 +0100

    add comment to be changed

--- src/platform/global/prod/iam.tf
+++ src/platform/global/prod/iam.tf
@@ -23,7 +23,7 @@ resource "azurerm_role_assignment" "apim_client_role" {
   for_each             = toset(local.role_definition_names.apim_client)
   principal_id         = data.azuread_service_principal.apim_client_svc.object_id
   role_definition_name = each.value
-  #scope                = module.apim_itn.id
+  #scope                = module.apim_itn.id # To be changed
   scope = "/subscriptions/ec285037-c673-4f58-b594-d7c480da4e8b/resourceGroups/io-p-itn-common-rg-01/providers/Microsoft.ApiManagement/service/io-p-itn-apim-01"
 }
 
@@ -37,7 +37,7 @@ resource "azurerm_role_assignment" "dev_portal_role" {
   for_each             = toset(local.role_definition_names.dev_portal)
   principal_id         = data.azuread_service_principal.dev_portal_svc.object_id
   role_definition_name = each.value
-  #scope                = module.apim_itn.id
+  #scope                = module.apim_itn.id # To be changed
   scope = "/subscriptions/ec285037-c673-4f58-b594-d7c480da4e8b/resourceGroups/io-p-itn-common-rg-01/providers/Microsoft.ApiManagement/service/io-p-itn-apim-01"
 }
 
@@ -45,6 +45,6 @@ resource "azurerm_role_assignment" "dev_portal_role" {
 resource "azurerm_role_assignment" "svc_devs_itn" {
   principal_id         = data.azuread_group.svc_devs.object_id
   role_definition_name = "PagoPA API Management Operator App"
-  #scope                = module.apim_itn.id
+  #scope                = module.apim_itn.id # To be changed
   scope = "/subscriptions/ec285037-c673-4f58-b594-d7c480da4e8b/resourceGroups/io-p-itn-common-rg-01/providers/Microsoft.ApiManagement/service/io-p-itn-apim-01"
 }
