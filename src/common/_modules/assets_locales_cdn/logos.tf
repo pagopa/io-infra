8dc886e89460d30e85dccfe0009045d780678c2f
Matteo Anselmi <matteo.anselmi@bitrock.it>
2026-03-18 14:10:32 +0100

    fix txt record typo

--- src/common/_modules/assets_locales_cdn/logos.tf
+++ src/common/_modules/assets_locales_cdn/logos.tf
@@ -17,7 +17,7 @@ resource "azurerm_cdn_frontdoor_custom_domain" "logos_custom_domain" {
 }
 
 resource "azurerm_dns_txt_record" "logos_custom_domain_validation_txt_record" {
-  name                = join(".", ["_dnsauth", "logos"])
+  name                = join(".", ["_dnsauth", "logos.assets"])
   zone_name           = var.public_dns_zones.io.name
   resource_group_name = var.resource_group_external
   ttl                 = 3600
