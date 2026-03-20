fee24c2da1b4dc9d8cd13bf429c4d3225ed7fedd
Luca Cavallaro <luca.cavallaro@pagopa.it>
2026-03-20 09:21:15 +0000

    Update app-backend to use key vault references

--- src/common/_modules/app_backend/data_kv.tf
+++ src/common/_modules/app_backend/data_kv.tf
@@ -1,92 +1,15 @@
-## key vault
-
-data "azurerm_key_vault_secret" "app_backend_API_KEY" {
-  name         = "funcapp-KEY-APPBACKEND"
+data "azurerm_key_vault_secret" "app_backend_RECEIPT_SERVICE_TEST_API_KEY" {
+  name         = "appbackend-RECEIPT-SERVICE-TEST-API-KEY"
   key_vault_id = var.key_vault_common.id
 }
 
-data "azurerm_key_vault_secret" "app_backend_CGN_API_KEY" {
-  name         = "funccgn-KEY-APPBACKEND"
-  key_vault_id = var.key_vault_common.id
-}
-
-data "azurerm_key_vault_secret" "app_backend_CDC_SUPPORT_API_KEY" {
-  name         = "funccdcsupport-KEY-APPBACKEND"
+data "azurerm_key_vault_secret" "app_backend_RECEIPT_SERVICE_API_KEY" {
+  name         = "appbackend-RECEIPT-SERVICE-API-KEY"
   key_vault_id = var.key_vault_common.id
 }
 
 data "azurerm_key_vault_secret" "app_backend_IO_SIGN_API_KEY" {
   name         = "funciosign-KEY-APPBACKEND"
-  key_vault_id = var.key_vault_common.id
-}
-
-data "azurerm_key_vault_secret" "app_backend_IO_FIMS_API_KEY" {
-  name         = "funciofims-KEY-APPBACKEND"
-  key_vault_id = var.key_vault_common.id
-}
-
-data "azurerm_key_vault_secret" "app_backend_CGN_OPERATOR_SEARCH_API_KEY_PROD" {
-  name         = "funccgnoperatorsearch-KEY-PROD-APPBACKEND"
-  key_vault_id = var.key_vault_common.id
-}
-
-data "azurerm_key_vault_secret" "app_backend_CGN_OPERATOR_SEARCH_API_KEY_UAT" {
-  name         = "funccgnoperatorsearch-KEY-UAT-APPBACKEND"
-  key_vault_id = var.key_vault_common.id
-}
-
-data "azurerm_key_vault_secret" "app_backend_PAGOPA_ECOMMERCE_API_KEY" {
-  name         = "appbackend-PAGOPA-ECOMMERCE-API-KEY"
-  key_vault_id = var.key_vault_common.id
-}
-
-data "azurerm_key_vault_secret" "app_backend_PAGOPA_ECOMMERCE_UAT_API_KEY" {
-  name         = "appbackend-PAGOPA-ECOMMERCE-UAT-API-KEY"
-  key_vault_id = var.key_vault_common.id
-}
-
-data "azurerm_key_vault_secret" "app_backend_ALLOW_MYPORTAL_IP_SOURCE_RANGE" {
-  name         = "appbackend-ALLOW-MYPORTAL-IP-SOURCE-RANGE"
-  key_vault_id = var.key_vault_common.id
-}
-
-data "azurerm_key_vault_secret" "app_backend_JWT_SUPPORT_TOKEN_PRIVATE_RSA_KEY" {
-  name         = "appbackend-JWT-SUPPORT-TOKEN-PRIVATE-RSA-KEY"
-  key_vault_id = var.key_vault_common.id
-}
-
-data "azurerm_key_vault_secret" "app_backend_TEST_CGN_FISCAL_CODES" {
-  name         = "appbackend-TEST-CGN-FISCAL-CODES"
-  key_vault_id = var.key_vault_common.id
-}
-
-data "azurerm_key_vault_secret" "app_backend_PECSERVER_TOKEN_SECRET" {
-  name         = "appbackend-PECSERVER-TOKEN-SECRET"
-  key_vault_id = var.key_vault_common.id
-}
-
-data "azurerm_key_vault_secret" "app_backend_PECSERVER_ARUBA_TOKEN_SECRET" {
-  name         = "appbackend-PECSERVER-ARUBA-TOKEN-SECRET"
-  key_vault_id = var.key_vault_common.id
-}
-
-data "azurerm_key_vault_secret" "app_backend_COM_CITIZEN_FUNC_API_KEY" {
-  name         = "appbackend-COM-CITIZEN-FUNC-API-KEY"
-  key_vault_id = var.key_vault_common.id
-}
-
-data "azurerm_key_vault_secret" "app_backend_APP_CITIZEN_APIM_KEY" {
-  name         = "appbackend-APP-CITIZEN-APIM-KEY"
-  key_vault_id = var.key_vault_common.id
-}
-
-data "azurerm_key_vault_secret" "app_backend_APP_MESSAGES_BETA_FISCAL_CODES" {
-  name         = "appbackend-APP-MESSAGES-BETA-FISCAL-CODES"
-  key_vault_id = var.key_vault_common.id
-}
-
-data "azurerm_key_vault_secret" "app_backend_X_USER_BETA_FISCAL_CODES" {
-  name         = "appbackend-X-USER-BETA-FISCAL-CODES"
   key_vault_id = var.key_vault_common.id
 }
 
@@ -100,42 +23,7 @@ data "azurerm_key_vault_secret" "app_backend_PN_API_KEY_UAT_V2" {
   key_vault_id = var.key_vault_common.id
 }
 
-data "azurerm_key_vault_secret" "app_backend_PN_REAL_TEST_USERS" {
-  name         = "appbackend-PN-REAL-TEST-USERS"
-  key_vault_id = var.key_vault_common.id
-}
-
-data "azurerm_key_vault_secret" "app_backend_LOLLIPOP_ITN_API_KEY" {
-  name         = "appbackend-LOLLIPOP-ITN-API-KEY"
-  key_vault_id = var.key_vault_common.id
-}
-
 data "azurerm_key_vault_secret" "app_backend_UNIQUE_EMAIL_ENFORCEMENT_USER" {
   name         = "appbackend-UNIQUE-EMAIL-ENFORCEMENT-USER"
-  key_vault_id = var.key_vault_common.id
-}
-
-data "azurerm_key_vault_secret" "app_backend_RECEIPT_SERVICE_TEST_API_KEY" {
-  name         = "appbackend-RECEIPT-SERVICE-TEST-API-KEY"
-  key_vault_id = var.key_vault_common.id
-}
-
-data "azurerm_key_vault_secret" "app_backend_RECEIPT_SERVICE_API_KEY" {
-  name         = "appbackend-RECEIPT-SERVICE-API-KEY"
-  key_vault_id = var.key_vault_common.id
-}
-
-data "azurerm_key_vault_secret" "fn_eucovidcert_API_KEY_PUBLICIOEVENTDISPATCHER" {
-  name         = "funceucovidcert-KEY-PUBLICIOEVENTDISPATCHER"
-  key_vault_id = var.key_vault.id
-}
-
-data "azurerm_key_vault_secret" "app_backend_APP_BACKEND_PRIMARY_KEY" {
-  name         = "appbackend-APP-BACKEND-PRIMARY-KEY"
-  key_vault_id = var.key_vault_common.id
-}
-
-data "azurerm_key_vault_secret" "appbackend_SERVICES_APP_BACKEND_API_KEY" {
-  name         = "appbe-host-key-for-app-backend"
   key_vault_id = var.key_vault_common.id
 }
