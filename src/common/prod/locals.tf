99538382c8f130200070fe26ecd25692719d5426
Matteo Anselmi <29119211+ansmatbitrock@users.noreply.github.com>
2026-04-27 11:00:25 +0200

    IOPLT-1799 - Remove stale references to fn assets and assets rg from common (#1878)
    Remove remaining references / data for `function_assets_cdn` and
    `rg_assets_cdn_name` from `common` since the function and resource
    groups have been deleted.

--- src/common/prod/locals.tf
+++ src/common/prod/locals.tf
@@ -62,7 +62,6 @@ locals {
   backend_hostnames = {
     app                  = [data.azurerm_linux_function_app.function_profile.default_hostname]
     com_citizen_func     = data.azurerm_linux_function_app.com_citizen_func.default_hostname
-    assets_cdn           = data.azurerm_linux_function_app.function_assets_cdn.default_hostname
     services_app_backend = data.azurerm_linux_function_app.services_app_backend_function_app.default_hostname
     # services_app_backend = data.azurerm_container_app.services_app_backend_function_app.ingress[0].fqdn
     lollipop      = data.azurerm_linux_function_app.lollipop_function.default_hostname
@@ -197,9 +196,8 @@ locals {
   ]
 
   function_services = {
-    rg_common_name     = format("%s-rg-common", local.project_weu_legacy)
-    rg_internal_name   = format("%s-rg-internal", local.project_weu_legacy)
-    rg_assets_cdn_name = format("%s-assets-cdn-rg", local.project_weu_legacy)
+    rg_common_name   = format("%s-rg-common", local.project_weu_legacy)
+    rg_internal_name = format("%s-rg-internal", local.project_weu_legacy)
 
     tags = {
       BusinessUnit = "App IO"
