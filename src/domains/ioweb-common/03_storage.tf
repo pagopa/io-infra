59bfe15ecad914c25508970e0d7df91be62277be
Matteo Anselmi <matteo.anselmi@bitrock.it>
2026-04-30 15:16:05 +0200

    fix import and tfdocs

--- src/domains/ioweb-common/03_storage.tf
+++ src/domains/ioweb-common/03_storage.tf
@@ -176,7 +176,7 @@ resource "azurerm_storage_account" "ioweb_portal" {
 
 import {
   to = azurerm_monitor_metric_alert.ioweb_portal
-  id = "/subscriptions/ec285037-c673-4f58-b594-d7c480da4e8b/resourceGroups/io-p-weu-ioweb-fe-rg/providers/Microsoft.Insights/metricalerts/[iopweuiowebportalsa] Low Availability"
+  id = "/subscriptions/ec285037-c673-4f58-b594-d7c480da4e8b/resourceGroups/io-p-weu-ioweb-fe-rg/providers/Microsoft.Insights/metricAlerts/[iopweuiowebportalsa] Low Availability"
 }
 
 resource "azurerm_monitor_metric_alert" "ioweb_portal" {
