7a7b9b80852b2b0ff7f76d8531754b9a244f7fb7
Matteo Anselmi <matteo.anselmi@bitrock.it>
2026-05-05 12:44:55 +0200

    fix law id, add tfdocs

--- src/platform/cdn/prod/italynorth.tf
+++ src/platform/cdn/prod/italynorth.tf
@@ -14,6 +14,7 @@ module "ioapp_cdn" {
   storage_account_location       = local.location.westeurope
   resource_group_cdn             = local.core.resource_groups.italynorth.common
   storage_account_resource_group = local.resource_groups.weu.ioweb
-  log_analytics_workspace_id     = "a0afb87e-2fd9-4219-8b01-eb95a083285d" # TODO: io-p-itn-common-log-01 - convert to terraform remote state once the monitoring infrastructure has been migrated to the new platform domain
-  tags                           = local.tags
+  # TODO: io-p-itn-common-log-01 - convert to terraform remote state once the monitoring infrastructure has been migrated to the new platform domain
+  log_analytics_workspace_id = "/subscriptions/ec285037-c673-4f58-b594-d7c480da4e8b/resourcegroups/io-p-itn-common-rg-01/providers/microsoft.operationalinsights/workspaces/io-p-itn-common-log-01"
+  tags                       = local.tags
 }
