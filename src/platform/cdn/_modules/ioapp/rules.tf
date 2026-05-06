981733f01b84d405104512b063e961b243d8632e
Matteo Anselmi <matteo.anselmi@bitrock.it>
2026-05-06 15:03:47 +0200

    fix rule

--- src/platform/cdn/_modules/ioapp/rules.tf
+++ src/platform/cdn/_modules/ioapp/rules.tf
@@ -19,8 +19,8 @@ resource "azurerm_cdn_frontdoor_rule" "ioapp_global_cache" {
 
   actions {
     route_configuration_override_action {
-      query_string_caching_behavior = "OverrideAlways"
-      cache_duration                = "00:15:00"
+      cache_behavior = "Override"
+      cache_duration = "00:15:00"
     }
 
     response_header_action {
