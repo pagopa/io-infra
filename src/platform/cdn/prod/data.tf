6aabe778aaadca62c7b0be1f3b4253973358fc2e
Matteo Anselmi <matteo.anselmi@bitrock.it>
2026-05-07 12:54:57 +0200

    fix common state storage account

--- src/platform/cdn/prod/data.tf
+++ src/platform/cdn/prod/data.tf
@@ -19,7 +19,7 @@ data "terraform_remote_state" "common" {
 
   config = {
     resource_group_name  = "terraform-state-rg"
-    storage_account_name = "iopitntfst02"
+    storage_account_name = "iopitntfst001"
     container_name       = "terraform-state"
     key                  = "io-infra.common.prod.tfstate"
     use_azuread_auth     = true
