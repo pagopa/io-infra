b88ec3d166eb659292b1af164f82bc50bd3bb190
Matteo Anselmi <matteo.anselmi@bitrock.it>
2026-05-11 18:02:15 +0200

    change platform domain layout

--- src/platform/prod/cdn/westeurope.tf
+++ src/platform/prod/cdn/westeurope.tf
@@ -1,5 +1,5 @@
 module "common" {
-  source                  = "../_modules/common"
+  source                  = "./_modules/common"
   resource_group_cdn      = local.core.resource_groups.westeurope.common
   resource_group_external = local.core.resource_groups.westeurope.external
   public_dns_zones        = local.common.public_dns_zones
