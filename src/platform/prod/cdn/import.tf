6f6de935ff96f15d6851432c8fe1a739c986cce8
Matteo Anselmi <matteo.anselmi@bitrock.it>
2026-05-12 13:10:01 +0200

    fix import

--- src/platform/prod/cdn/import.tf
+++ src/platform/prod/cdn/import.tf
@@ -2,6 +2,6 @@
 # remember to cleanup the import code blocks with a separate PR once the import has been completed successfully.
 
 import {
-  to = module.ioapp.azurerm_dns_txt_record.developer_io_italia_it
+  to = module.common.azurerm_dns_txt_record.developer_io_italia_it
   id = "/subscriptions/ec285037-c673-4f58-b594-d7c480da4e8b/resourceGroups/io-p-rg-external/providers/Microsoft.Network/dnsZones/io.italia.it/TXT/_dnsauth.developer"
 }
