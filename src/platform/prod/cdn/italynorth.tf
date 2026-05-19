a202289ba597944053e50cbea76e3ec8d9f21198
Matteo Anselmi <matteo.anselmi@bitrock.it>
2026-05-19 11:28:08 +0200

    enable custom domain ioapp.it

--- src/platform/prod/cdn/italynorth.tf
+++ src/platform/prod/cdn/italynorth.tf
@@ -7,3 +7,15 @@ module "ioapp" {
   log_analytics_workspace_id     = local.common.monitoring.itn.law_id
   tags                           = local.tags
 }
+
+/*
+import {
+  to = module.ioapp.module.ioapp.azurerm_dns_a_record.this["ioapp.it"]
+  id = "/subscriptions/ec285037-c673-4f58-b594-d7c480da4e8b/resourceGroups/io-p-rg-external/providers/Microsoft.Network/dnsZones/ioapp.it/A/@"
+}
+
+import {
+  to = module.ioapp.module.ioapp.azurerm_dns_txt_record.validation["ioapp.it"]
+  id = "/subscriptions/ec285037-c673-4f58-b594-d7c480da4e8b/resourceGroups/io-p-rg-external/providers/Microsoft.Network/dnsZones/ioapp.it/TXT/_dnsauth"
+}
+*/
