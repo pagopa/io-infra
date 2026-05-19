0c14bdda31a852a5c6d1278b0784cebcb7dc8fd2
Matteo Anselmi <matteo.anselmi@bitrock.it>
2026-05-19 11:38:06 +0200

    switch www.ioapp.it dns records

--- src/platform/prod/cdn/_modules/ioapp/main.tf
+++ src/platform/prod/cdn/_modules/ioapp/main.tf
@@ -15,23 +15,17 @@ module "ioapp" {
   custom_domains = [
     {
       host_name = "ioapp.it"
-      # TODO: enable dns block / import txt validation records 
-      /*
       dns = {
         zone_name                = data.azurerm_dns_zone.ioapp_it.name
         zone_resource_group_name = data.azurerm_resource_group.core_ext.name
       }
-      */
     },
     {
       host_name = "www.ioapp.it"
-      # TODO: enable dns block / import txt validation records 
-      /*
       dns = {
         zone_name                = data.azurerm_dns_zone.ioapp_it.name
         zone_resource_group_name = data.azurerm_resource_group.core_ext.name
       }
-      */
     }
   ]
 
