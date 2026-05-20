049fc6788680293cca0c2f9475597520f011d1a1
Matteo Anselmi <matteo.anselmi@bitrock.it>
2026-05-20 16:02:03 +0200

    dummy edit for platform cdn

--- src/platform/prod/cdn/import.tf
+++ src/platform/prod/cdn/import.tf
@@ -1,3 +1,4 @@
 # Use this file to import the wanted resources inside the state file, 
 # remember to cleanup the import code blocks with a separate PR once the import has been completed successfully.
 # Here is the documentation which explains how to use the import code block: https://developer.hashicorp.com/terraform/language/block/import
+
