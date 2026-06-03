0e82d1d2de74db455686ff463a26621a2cbe1a09
Matteo Anselmi <29119211+ansmatbitrock@users.noreply.github.com>
2026-06-03 17:39:35 +0200

    IOPLT-1874 - Enable WAF for io-p-itn-ioapp-afd-01 (#1913)
    Enable Geo rate limit for ` io-p-itn-ioapp-afd-01`

---
+++ src/platform/prod/cdn/_modules/ioapp/waf.tf
@@ -0,0 +1,49 @@
+resource "azurerm_cdn_frontdoor_firewall_policy" "ioapp_firewall_policy" {
+  name                = "ioapp"
+  resource_group_name = var.resource_group_cdn
+  sku_name            = "Standard_AzureFrontDoor"
+
+  mode                              = "Detection"
+  enabled                           = true
+  custom_block_response_status_code = 403
+
+  custom_rule {
+    name     = "RateLimitOutsideEMEA"
+    priority = 100
+    enabled  = true
+    type     = "RateLimitRule"
+    action   = "Block"
+
+    rate_limit_duration_in_minutes = 30
+    rate_limit_threshold           = 1000
+
+    match_condition {
+      match_variable     = "RemoteAddr"
+      operator           = "GeoMatch"
+      negation_condition = true
+
+      match_values = [
+        "Italy" # TODO: To be changed to EMEA / list of countries
+      ]
+    }
+  }
+}
+
+resource "azurerm_cdn_frontdoor_security_policy" "ioapp_frontdoor_security_policy" {
+  name                     = "ioapp"
+  cdn_frontdoor_profile_id = module.ioapp.id
+
+  security_policies {
+    firewall {
+      cdn_frontdoor_firewall_policy_id = azurerm_cdn_frontdoor_firewall_policy.ioapp_firewall_policy.id
+
+      association {
+        domain {
+          cdn_frontdoor_domain_id = module.ioapp.endpoint_id
+        }
+
+        patterns_to_match = ["/*"]
+      }
+    }
+  }
+}
