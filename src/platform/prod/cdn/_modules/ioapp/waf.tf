49eaf648639584c93c6305a1eecadfbe541e525c
Matteo Anselmi <matteo.anselmi@bitrock.it>
2026-06-03 17:13:40 +0200

    add waf for ioapp cdn

---
+++ src/platform/prod/cdn/_modules/ioapp/waf.tf
@@ -0,0 +1,50 @@
+resource "azurerm_cdn_frontdoor_firewall_policy" "ioapp_firewall_policy" {
+  name                = "io-p-itn-ioapp-afd-01"
+  resource_group_name = var.resource_group_cdn
+  sku_name            = "Standard_AzureFrontDoor"
+
+  mode          = "Detection"
+  enabled       = true
+  custom_block_response_body  = ""
+  custom_block_response_status_code = 403
+
+  custom_rule {
+    name     = "RateLimitOutsideItaly"
+    priority = 100
+    enabled  = true
+    type     = "RateLimitRule"
+    action   = "Block"
+
+    rate_limit_duration_in_minutes = 5
+    rate_limit_threshold           = 1000
+
+    match_condition {
+      match_variable = "GeoMatch"
+      operator       = "Any"
+      negation_condition = true
+
+      match_values = [
+        "Italy"
+      ]
+    }
+  }
+}
+
+resource "azurerm_cdn_frontdoor_security_policy" "ioapp_frontdoor_security_policy" {
+  name                     = "io-p-itn-ioapp-afd-01"
+  cdn_frontdoor_profile_id = module.ioapp.id
+
+  security_policies {
+    firewall {
+      cdn_frontdoor_firewall_policy_id = azurerm_cdn_frontdoor_firewall_policy.ioapp_firewall_policy.id
+
+      association {
+        domain {
+          cdn_frontdoor_domain_id = module.ioapp.endpoint_id
+        }
+
+        patterns_to_match = ["/*"]
+      }
+    }
+  }
+}
