4dc624c85db7c48307c1989e8df785d8aff5d7bf
Matteo Anselmi <29119211+ansmatbitrock@users.noreply.github.com>
2026-06-03 15:28:04 +0200

    IOPLT-1838 - Switch ioapp.it apex record to io-p-itn-ioapp-afd-01 (#1900)
    Switch the @ DNS record ioapp.it on io-p-itn-ioapp-afd-01, import the
    already existing resources.

--- src/platform/prod/cdn/_modules/ioapp/main.tf
+++ src/platform/prod/cdn/_modules/ioapp/main.tf
@@ -15,13 +15,10 @@ module "ioapp" {
   custom_domains = [
     {
       host_name = "ioapp.it"
-      # TODO: enable dns block / import txt validation records 
-      /*
       dns = {
         zone_name                = var.public_dns_zones.ioweb_it.name
         zone_resource_group_name = var.resource_group_external
       }
-      */
       custom_certificate = {
         key_vault_certificate_versionless_id = data.azurerm_key_vault_certificate.ioapp_it_certificate.versionless_id
         key_vault_name                       = data.azurerm_key_vault.ioapp_it_kv.name
