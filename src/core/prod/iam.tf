4431dbf494cebeffb63228690256356d32ef96f7
Matteo Anselmi <matteo.anselmi@bitrock.it>
2026-06-16 09:35:51 +0200

    fix id for subscription

--- src/core/prod/iam.tf
+++ src/core/prod/iam.tf
@@ -89,7 +89,7 @@ resource "azurerm_key_vault_access_policy" "kv_common_infra_cd" {
 }
 
 resource "azurerm_role_assignment" "infra_cd_access_control_administrator" {
-  scope                = data.azurerm_subscription.current
+  scope                = data.azurerm_subscription.current.id
   principal_id         = data.azurerm_user_assigned_identity.managed_identity_io_infra_cd.principal_id
   role_definition_name = "User Access Administrator"
   description          = "Allow the io-p-infra-github-cd-identity to manage RBAC permissions"
