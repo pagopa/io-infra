c68e5b1d7ec281adddf3496ccc24c1ad86beefae
Matteo Anselmi <matteo.anselmi@bitrock.it>
2026-06-24 11:50:48 +0200

    fix references

--- src/platform/prod/cdn/italynorth.tf
+++ src/platform/prod/cdn/italynorth.tf
@@ -4,7 +4,7 @@ module "ioapp" {
   storage_account_location                 = local.location.westeurope
   resource_group_cdn                       = local.core.resource_groups.italynorth.common
   storage_account_resource_group           = local.resource_groups.weu.ioweb
-  log_analytics_workspace_id               = local.platform_observability.monitoring_italynorth.law_id
+  log_analytics_workspace_id               = local.platform_observability.monitoring_italynorth.log.id
   ioapp_apex_certificate_kv_name           = local.core.key_vault.weu.tlscert_itn_01.name
   ioapp_apex_certificate_kv_resource_group = local.core.key_vault.weu.tlscert_itn_01.resource_group_name
   ioapp_apex_certificate_name              = "ioapp-it"
