e99d30ba4a5765df690f6ef2212f1ddcffd66d70
Matteo Anselmi <29119211+ansmatbitrock@users.noreply.github.com>
2026-07-01 10:07:51 +0200

    IOPLT-1930 - Remove DEV environment from common (#1939)
    Decommission the DEV environment from common after successful import in
    platform.

--- src/platform/dev/core/import.tf
+++ src/platform/dev/core/import.tf
@@ -1,8 +1,3 @@
 # Use this file to import the wanted resources inside the state file, 
 # remember to cleanup the import code blocks with a separate PR once the import has been completed successfully.
 # Here is the documentation which explains how to use the import code block: https://developer.hashicorp.com/terraform/language/block/import
-
-import {
-  to = azurerm_resource_group.platform
-  id = "/subscriptions/a4e96bcd-59dc-4d66-b2f7-5547ad157c12/resourceGroups/io-d-itn-platform-rg-01"
-}
