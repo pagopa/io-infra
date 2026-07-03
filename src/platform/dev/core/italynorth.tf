2a0e57dd745877925f6cc332ce862e6fdaadf498
Matteo Anselmi <matteo.anselmi@bitrock.it>
2026-07-03 15:42:04 +0200

    add RG for new dev apim

--- src/platform/dev/core/italynorth.tf
+++ src/platform/dev/core/italynorth.tf
@@ -4,3 +4,10 @@ resource "azurerm_resource_group" "platform" {
 
   tags = local.tags
 }
+
+resource "azurerm_resource_group" "apim" {
+  name     = "${local.project}-apim-rg-01"
+  location = local.location
+
+  tags = local.tags
+}
