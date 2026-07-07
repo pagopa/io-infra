5b6fce98144565557acce579b1ba62d1e7abc4f8
Matteo Anselmi <matteo.anselmi@bitrock.it>
2026-07-07 12:09:06 +0200

    update import

--- src/platform/dev/app-routing/import.tf
+++ src/platform/dev/app-routing/import.tf
@@ -4,10 +4,10 @@
 
 import {
   to = module.iam_apim_itn_infra_ci.module.apim.azurerm_role_assignment.this["io-d-itn-apim-rg-01|io-d-itn-apim-01|owner"]
-  id = "/subscriptions/a4e96bcd-59dc-4d66-b2f7-5547ad157c12/resourceGroups/io-d-itn-apim-rg-01/providers/Microsoft.ApiManagement/service/io-d-itn-apim-01/providers/Microsoft.Authorization/roleAssignments/7c80149a-2c4b-401b-aa6e-b466076d591a"
+  id = "/subscriptions/a4e96bcd-59dc-4d66-b2f7-5547ad157c12/resourceGroups/io-d-itn-apim-rg-01/providers/Microsoft.ApiManagement/service/io-d-itn-apim-01/providers/Microsoft.Authorization/roleAssignments/cf826bba-4983-44f1-8017-0c6fd8dd7e1c"
 }
 
 import {
   to = module.iam_apim_itn_infra_cd.module.apim.azurerm_role_assignment.this["io-d-itn-apim-rg-01|io-d-itn-apim-01|owner"]
-  id = "/subscriptions/a4e96bcd-59dc-4d66-b2f7-5547ad157c12/resourceGroups/io-d-itn-apim-rg-01/providers/Microsoft.ApiManagement/service/io-d-itn-apim-01/providers/Microsoft.Authorization/roleAssignments/1eb00eec-2b76-436e-a7f9-6ca3cb149ccb"
+  id = "/subscriptions/a4e96bcd-59dc-4d66-b2f7-5547ad157c12/resourceGroups/io-d-itn-apim-rg-01/providers/Microsoft.ApiManagement/service/io-d-itn-apim-01/providers/Microsoft.Authorization/roleAssignments/1d4ba9ad-3bd6-4e18-972c-bf8224c3f5da"
 }
