93b701656da510169bdf7aa91b33321eac007653
Matteo Anselmi <29119211+ansmatbitrock@users.noreply.github.com>
2026-07-07 10:53:22 +0200

    IOPLT-1940 - fix APIM permissions for the Github managed identities (#1952)
    Allow the DEV CI / CD managed identities to read and update values on
    the new DEV APIM
    
    ---------
    
    Co-authored-by: Daniele Manni <dan.manni@gmail.com>

---
+++ src/platform/dev/app-routing/iam.tf
@@ -0,0 +1,37 @@
+module "iam_apim_itn_infra_ci" {
+
+  depends_on = [module.apim_itn]
+
+  source  = "pagopa-dx/azure-role-assignments/azurerm"
+  version = "~> 2.0"
+
+  subscription_id = data.azurerm_subscription.current.subscription_id
+  principal_id    = data.azurerm_user_assigned_identity.managed_identity_io_infra_ci.principal_id
+
+  apim = [{
+    name                = data.azurerm_key_vault.itn_common.name
+    resource_group_name = azurerm_resource_group.apim.name
+    has_rbac_support    = true
+    description         = "Allow the io-d-infra-github-ci-identity to read all values"
+    role                = "writer" # Write necessary to read all the APIM associated secret
+  }]
+}
+
+module "iam_apim_itn_infra_cd" {
+
+  depends_on = [module.apim_itn]
+
+  source  = "pagopa-dx/azure-role-assignments/azurerm"
+  version = "~> 2.0"
+
+  subscription_id = data.azurerm_subscription.current.subscription_id
+  principal_id    = data.azurerm_user_assigned_identity.managed_identity_io_infra_ci.principal_id
+
+  apim = [{
+    name                = data.azurerm_key_vault.itn_common.name
+    resource_group_name = azurerm_resource_group.apim.name
+    has_rbac_support    = true
+    description         = "Allow the io-d-infra-github-cd-identity to write all values"
+    role                = "writer"
+  }]
+}
