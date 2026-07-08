97cc6ba12456c6db43a927414e12c46205a20f5c
Matteo Anselmi <matteo.anselmi@bitrock.it>
2026-07-08 07:57:55 +0200

    import apim in platform app-routing

--- src/platform/prod/app-routing/italynorth.tf
+++ src/platform/prod/app-routing/italynorth.tf
@@ -3,6 +3,38 @@ resource "dx_available_subnet_cidr" "next_cidr_snet_agw" {
   prefix_length      = 24
 }
 
+module "apim_itn" {
+  source = "./_modules/apim"
+
+  location                = "italynorth"
+  location_short          = local.core.resource_groups.italynorth.location_short
+  project                 = local.project_itn
+  prefix                  = local.prefix
+  resource_group_common   = local.resource_groups.itn.common
+  resource_group_internal = local.resource_groups.itn.internal
+
+  vnet_common = local.core.networking.itn.vnet_common
+  cidr_subnet = "10.20.100.0/24"
+
+  datasources = {
+    azurerm_client_config = data.azurerm_client_config.current
+  }
+
+  key_vault        = local.core.key_vault.weu.kv
+  key_vault_common = local.core.key_vault.weu.kv_common
+
+  action_group_id      = local.platform_observability.monitoring_westeurope.action_groups.error
+  ai_connection_string = local.platform_observability.monitoring_westeurope.appi_connection_string
+
+  azure_adgroup_wallet_admins_object_id = data.azuread_group.wallet_admins.object_id
+  azure_adgroup_com_admins_object_id    = data.azuread_group.com_admins.object_id
+  azure_adgroup_svc_admins_object_id    = data.azuread_group.svc_admins.object_id
+  azure_adgroup_auth_admins_object_id   = data.azuread_group.auth_admins.object_id
+  azure_adgroup_bonus_admins_object_id  = data.azuread_group.bonus_admins.object_id
+
+  tags = local.tags
+}
+
 module "application_gateway_itn" {
   source = "./_modules/application_gateway"
 
