23f6f1582528814f257b492854af39a8a5f51f6a
Matteo Anselmi <matteo.anselmi@bitrock.it>
2026-07-08 12:08:26 +0200

    add uat.ioapp.it delegated zone

--- src/platform/prod/core/_modules/dns/zones/ioapp_it.tf
+++ src/platform/prod/core/_modules/dns/zones/ioapp_it.tf
@@ -110,3 +110,50 @@ resource "azurerm_dns_cname_record" "aws_cert_validation_ioweb" {
   ttl                 = var.dns_default_ttl_sec
   record              = "_2de3fc7bfdb25e6a4e58c29dc8b4dba6.zfyfvmchrl.acm-validations.aws."
 }
+
+## UAT ##
+
+resource "azurerm_dns_zone" "uat_ioapp_it" {
+  name                = "uat.ioapp.it"
+  resource_group_name = var.resource_groups.external
+
+  tags = var.tags
+}
+
+resource "azurerm_dns_caa_record" "uat_ioapp_it" {
+  name                = "@"
+  zone_name           = azurerm_dns_zone.uat_ioapp_it.name
+  resource_group_name = var.resource_groups.external
+  ttl                 = var.dns_default_ttl_sec
+
+  record {
+    flags = 0
+    tag   = "iodef"
+    value = "mailto:security+caa@pagopa.it"
+  }
+
+  tags = var.tags
+}
+
+resource "azurerm_dns_ns_record" "uat_ioapp_it_delegation" {
+  name                = "uat"
+  zone_name           = azurerm_dns_zone.ioapp_it.name
+  resource_group_name = var.resource_groups.external
+  ttl                 = 3600
+
+  records = azurerm_dns_zone.uat_ioapp_it.name_servers
+}
+
+/*
+# DMARC record for uat.ioapp.it
+resource "azurerm_dns_txt_record" "dmarc_uat_ioapp_it" {
+  name                = "_dmarc"
+  zone_name           = azurerm_dns_zone.uat_ioapp_it.name
+  resource_group_name = var.resource_groups.external
+  ttl                 = var.dns_default_ttl_sec
+  record {
+    value = "v=DMARC1; p=reject; rua=mailto:dmarc@0f1qy7b5.uriports.com; aspf=s; adkim=s"
+  }
+  tags = var.tags
+}
+*/
