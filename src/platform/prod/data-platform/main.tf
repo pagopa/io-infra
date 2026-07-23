503933e8d705010894ba652123baf995aa00f657
Matteo Anselmi <matteo.anselmi@bitrock.it>
2026-07-23 13:46:05 +0200

    add azuread provider

--- src/platform/prod/data-platform/main.tf
+++ src/platform/prod/data-platform/main.tf
@@ -13,6 +13,10 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "< 5.0.0"
     }
+    azuread = {
+      source  = "hashicorp/azuread"
+      version = "~> 3.0"
+    }
     dx = {
       source  = "pagopa-dx/azure"
       version = "~> 0.10"
