cb3f44ca95058c31e5417a3cdafe1ab4e42ae886
Matteo Anselmi <matteo.anselmi@bitrock.it>
2026-07-23 16:50:27 +0200

    add sensitive flag

--- src/platform/prod/data-platform/outputs.tf
+++ src/platform/prod/data-platform/outputs.tf
@@ -1,10 +1,12 @@
 output "redis" {
+  sensitive = true
   value = {
     weu = module.redis_weu
   }
 }
 
 output "cosmos_api" {
+  sensitive = true
   value = {
     weu = module.cosmos_api_weu
   }
