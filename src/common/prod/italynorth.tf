a6d11da97368590b7965ca65ab8bbdf87c1f4260
Matteo Anselmi <matteo.anselmi@bitrock.it>
2026-07-24 09:21:22 +0200

    remove migrated resources of data platform

--- src/common/prod/italynorth.tf
+++ src/common/prod/italynorth.tf
@@ -41,23 +41,6 @@ module "private_endpoints" {
   tags = local.tags
 }
 
-module "platform_service_bus_namespace_itn" {
-
-  source = "../_modules/platform_service_bus"
-
-  location = "italynorth"
-  project  = local.project_itn
-  prefix   = local.prefix
-
-  resource_group_internal = local.resource_groups.itn.internal
-  resource_group_event    = local.resource_groups.weu.event
-  vnet_common             = local.core.networking.itn.vnet_common
-  cidr_subnet             = "10.20.102.0/24"
-  pep_snet_id             = local.core.networking.itn.pep_snet.id
-
-  tags = local.tags
-}
-
 module "storage_accounts_itn" {
   source = "../_modules/storage_accounts"
 
