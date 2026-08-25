42fa3cdfd89bb6a2e4bbc197aa6aa1029301edb1
Matteo Anselmi <matteo.anselmi@bitrock.it>
2026-08-25 10:08:33 +0200

    add removed resources for elt

--- src/common/prod/removed.tf
+++ src/common/prod/removed.tf
@@ -2,7 +2,7 @@
 # https://pagopa.atlassian.net/browse/IOPLT-1626
 
 removed {
-  from = module.continua_app_service.azurerm_monitor_autoscale_setting.appservice_continua
+  from = module.function_app_elt.azurerm_key_vault_access_policy.function_elt_itn_kv_common
 
   lifecycle {
     destroy = false
@@ -10,7 +10,7 @@ removed {
 }
 
 removed {
-  from = module.continua_app_service.azurerm_resource_group.continua_itn_rg
+  from = module.function_app_elt.azurerm_key_vault_access_policy.function_elt_itn_slot_staging_kv_common
 
   lifecycle {
     destroy = false
@@ -18,7 +18,7 @@ removed {
 }
 
 removed {
-  from = module.continua_app_service.module.appservice_continua_itn.azurerm_linux_web_app_slot.this
+  from = module.function_app_elt.azurerm_monitor_diagnostic_setting.queue_diagnostic_setting
 
   lifecycle {
     destroy = false
@@ -26,7 +26,7 @@ removed {
 }
 
 removed {
-  from = module.continua_app_service.module.appservice_continua_itn.azurerm_linux_web_app.this
+  from = module.function_app_elt.azurerm_monitor_scheduled_query_rules_alert_v2.profile_deletion_failure_alert_rule
 
   lifecycle {
     destroy = false
@@ -34,7 +34,7 @@ removed {
 }
 
 removed {
-  from = module.continua_app_service.module.appservice_continua_itn.azurerm_private_endpoint.app_service_sites
+  from = module.function_app_elt.azurerm_monitor_scheduled_query_rules_alert_v2.profiles_failure_alert_rule
 
   lifecycle {
     destroy = false
@@ -42,7 +42,7 @@ removed {
 }
 
 removed {
-  from = module.continua_app_service.module.appservice_continua_itn.azurerm_private_endpoint.staging_app_service_sites
+  from = module.function_app_elt.azurerm_monitor_scheduled_query_rules_alert_v2.service_preferences_failure_alert_rule
 
   lifecycle {
     destroy = false
@@ -50,7 +50,7 @@ removed {
 }
 
 removed {
-  from = module.continua_app_service.module.appservice_continua_itn.azurerm_service_plan.this
+  from = module.function_app_elt.azurerm_resource_group.itn_elt
 
   lifecycle {
     destroy = false
@@ -58,7 +58,255 @@ removed {
 }
 
 removed {
-  from = module.continua_app_service.module.appservice_continua_itn.azurerm_subnet.this
+  from = module.function_app_elt.azurerm_storage_queue.pdnd-io-cosmosdb-profile-deletion-failure
+
+  lifecycle {
+    destroy = false
+  }
+}
+
+removed {
+  from = module.function_app_elt.azurerm_storage_queue.pdnd-io-cosmosdb-profile-deletion-failure-poison
+
+  lifecycle {
+    destroy = false
+  }
+}
+
+removed {
+  from = module.function_app_elt.azurerm_storage_queue.pdnd-io-cosmosdb-profiles-failure
+
+  lifecycle {
+    destroy = false
+  }
+}
+
+removed {
+  from = module.function_app_elt.azurerm_storage_queue.pdnd-io-cosmosdb-profiles-failure-poison
+
+  lifecycle {
+    destroy = false
+  }
+}
+
+removed {
+  from = module.function_app_elt.azurerm_storage_queue.pdnd-io-cosmosdb-service-preferences-failure
+
+  lifecycle {
+    destroy = false
+  }
+}
+
+removed {
+  from = module.function_app_elt.azurerm_storage_queue.pdnd-io-cosmosdb-service-preferences-failure-poison
+
+  lifecycle {
+    destroy = false
+  }
+}
+
+removed {
+  from = module.function_app_elt.azurerm_storage_queue.pdnd-io-cosmosdb-services-failure
+
+  lifecycle {
+    destroy = false
+  }
+}
+
+removed {
+  from = module.function_app_elt.azurerm_storage_queue.pdnd-io-cosmosdb-services-failure-poison
+
+  lifecycle {
+    destroy = false
+  }
+}
+
+removed {
+  from = module.function_app_elt.azurerm_storage_table.fneltcommands_itn
+
+  lifecycle {
+    destroy = false
+  }
+}
+
+removed {
+  from = module.function_app_elt.azurerm_storage_table.fnelterrors_itn
+
+  lifecycle {
+    destroy = false
+  }
+}
+
+removed {
+  from = module.function_app_elt.module.function_elt_itn.azurerm_linux_function_app_slot.this
+
+  lifecycle {
+    destroy = false
+  }
+}
+
+removed {
+  from = module.function_app_elt.module.function_elt_itn.azurerm_linux_function_app.this
+
+  lifecycle {
+    destroy = false
+  }
+}
+
+removed {
+  from = module.function_app_elt.module.function_elt_itn.azurerm_monitor_metric_alert.function_app_health_check
+
+  lifecycle {
+    destroy = false
+  }
+}
+
+removed {
+  from = module.function_app_elt.module.function_elt_itn.azurerm_monitor_metric_alert.storage_account_health_check
+
+  lifecycle {
+    destroy = false
+  }
+}
+
+removed {
+  from = module.function_app_elt.module.function_elt_itn.azurerm_private_endpoint.function_sites
+
+  lifecycle {
+    destroy = false
+  }
+}
+
+removed {
+  from = module.function_app_elt.module.function_elt_itn.azurerm_private_endpoint.st_blob
+
+  lifecycle {
+    destroy = false
+  }
+}
+
+removed {
+  from = module.function_app_elt.module.function_elt_itn.azurerm_private_endpoint.st_file
+
+  lifecycle {
+    destroy = false
+  }
+}
+
+removed {
+  from = module.function_app_elt.module.function_elt_itn.azurerm_private_endpoint.st_queue
+
+  lifecycle {
+    destroy = false
+  }
+}
+
+removed {
+  from = module.function_app_elt.module.function_elt_itn.azurerm_private_endpoint.staging_function_sites
+
+  lifecycle {
+    destroy = false
+  }
+}
+
+removed {
+  from = module.function_app_elt.module.function_elt_itn.azurerm_role_assignment.function_storage_account_contributor
+
+  lifecycle {
+    destroy = false
+  }
+}
+
+removed {
+  from = module.function_app_elt.module.function_elt_itn.azurerm_role_assignment.function_storage_blob_data_owner
+
+  lifecycle {
+    destroy = false
+  }
+}
+
+removed {
+  from = module.function_app_elt.module.function_elt_itn.azurerm_role_assignment.function_storage_queue_data_contributor
+
+  lifecycle {
+    destroy = false
+  }
+}
+
+removed {
+  from = module.function_app_elt.module.function_elt_itn.azurerm_role_assignment.staging_function_storage_account_contributor
+
+  lifecycle {
+    destroy = false
+  }
+}
+
+removed {
+  from = module.function_app_elt.module.function_elt_itn.azurerm_role_assignment.staging_function_storage_blob_data_owner
+
+  lifecycle {
+    destroy = false
+  }
+}
+
+removed {
+  from = module.function_app_elt.module.function_elt_itn.azurerm_role_assignment.staging_function_storage_queue_data_contributor
+
+  lifecycle {
+    destroy = false
+  }
+}
+
+removed {
+  from = module.function_app_elt.module.function_elt_itn.azurerm_service_plan.this
+
+  lifecycle {
+    destroy = false
+  }
+}
+
+removed {
+  from = module.function_app_elt.module.function_elt_itn.azurerm_storage_account_network_rules.st_network_rules
+
+  lifecycle {
+    destroy = false
+  }
+}
+
+removed {
+  from = module.function_app_elt.module.function_elt_itn.azurerm_storage_account.this
+
+  lifecycle {
+    destroy = false
+  }
+}
+
+removed {
+  from = module.function_app_elt.module.function_elt_itn.azurerm_subnet.this
+
+  lifecycle {
+    destroy = false
+  }
+}
+
+removed {
+  from = module.function_app_elt.module.storage_account_itn_elt.azurerm_monitor_metric_alert.storage_account_health_check
+
+  lifecycle {
+    destroy = false
+  }
+}
+
+removed {
+  from = module.function_app_elt.module.storage_account_itn_elt.azurerm_private_endpoint.this
+
+  lifecycle {
+    destroy = false
+  }
+}
+
+removed {
+  from = module.function_app_elt.module.storage_account_itn_elt.azurerm_storage_account.this
 
   lifecycle {
     destroy = false
