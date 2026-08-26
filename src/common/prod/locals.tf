96ba5da16da3fb540d6b9ff570b75e4b3e7c4b35
Matteo Anselmi <matteo.anselmi@bitrock.it>
2026-08-26 11:02:24 +0200

    remove unused local

--- src/common/prod/locals.tf
+++ src/common/prod/locals.tf
@@ -88,25 +88,4 @@ locals {
 
     apim_itn_name = "${local.project_itn}-apim-01"
   }
-
-  function_elt = {
-
-    location                        = "westeurope"
-    secondary_location_display_name = "North Europe"
-
-    vnet_common_name_itn           = "${local.project_itn}-common-vnet-01"
-    common_resource_group_name_itn = "${local.project_itn}-common-rg-01"
-
-    location_itn  = "italynorth"
-    elt_snet_cidr = "10.20.40.0/26"
-
-    tags = {
-      CostCenter     = "TS000 - Tecnologia e Servizi"
-      CreatedBy      = "Terraform"
-      Environment    = "Prod"
-      BusinessUnit   = "App IO"
-      Source         = "https://github.com/pagopa/io-infra/blob/main/src/domains/elt/prod"
-      ManagementTeam = "IO Platform"
-    }
-  }
 }
