9ab36c58aea02b3de01f065bec498e22954770c6
Matteo Anselmi <matteo.anselmi@bitrock.it>
2026-09-07 17:08:03 +0200

    remove unused variable

--- src/platform/prod/core/_modules/dns/main.tf
+++ src/platform/prod/core/_modules/dns/main.tf
@@ -14,9 +14,8 @@ module "zones" {
 
   app_gateway_public_ip = var.dns.app_gateway_public_ip
 
-  apim_private_ip                             = var.dns.apim_private_ip
-  platform_api_gateway_private_ip             = var.dns.platform_api_gateway_private_ip
-  psn_application_gateway_private_endpoint_ip = var.dns.psn_application_gateway_private_endpoint_ip
+  apim_private_ip                 = var.dns.apim_private_ip
+  platform_api_gateway_private_ip = var.dns.platform_api_gateway_private_ip
 
   tags = var.tags
 }
--- src/platform/prod/core/_modules/dns/variables.tf
+++ src/platform/prod/core/_modules/dns/variables.tf
@@ -23,8 +23,8 @@ variable "dns" {
       io                  = string
       firmaconio_selfcare = string
     })
-    app_gateway_public_ip                       = string
-    apim_private_ip                             = string
-    platform_api_gateway_private_ip             = string
+    app_gateway_public_ip           = string
+    apim_private_ip                 = string
+    platform_api_gateway_private_ip = string
   })
 }
