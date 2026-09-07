cd574232fc237aac889e69b154b9abf0936fe7d0
Matteo Anselmi <matteo.anselmi@bitrock.it>
2026-09-07 17:29:41 +0200

    fix app gateway

--- src/platform/prod/core/global.tf
+++ src/platform/prod/core/global.tf
@@ -30,7 +30,7 @@ module "dns" {
       firmaconio_selfcare = "firmaconio.selfcare"
     }
 
-    app_gateway_public_ip           = local.app_routing.application_gateway.itn.public_ip
+    app_gateway_public_ip           = local.app_routing.application_gateway.itn.public_ip.address
     apim_private_ip                 = local.app_routing.apim.itn.private_ips
     platform_api_gateway_private_ip = local.app_routing.platform_api_gateway.itn.private_ips
 
