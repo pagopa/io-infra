525f824403453068962bcbf5bdc2cca279232409
Matteo Anselmi <matteo.anselmi@bitrock.it>
2026-09-09 09:56:03 +0200

    fix import

--- src/core/prod/import.tf
+++ src/core/prod/import.tf
@@ -3,7 +3,7 @@
 # Here is the documentation which explains how to use the import code block: https://developer.hashicorp.com/terraform/language/block/import
 
 import {
-  to = module.key_vault_weu.azurerm_key_vault_access_policy.kv_common_cd["8e33ccf6-17bd-4960-a2de-e5015c06f5f6"]
+  to = module.key_vault_weu.azurerm_key_vault_access_policy.kv_common_ci["8e33ccf6-17bd-4960-a2de-e5015c06f5f6"]
   id = "/subscriptions/ec285037-c673-4f58-b594-d7c480da4e8b/resourceGroups/io-p-rg-common/providers/Microsoft.KeyVault/vaults/io-p-kv-common/objectId/8e33ccf6-17bd-4960-a2de-e5015c06f5f6"
 }
 
