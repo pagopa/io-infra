7832a4afc411b8bbdbca2a7453832c6e06f9b1a7
Matteo Anselmi <matteo.anselmi@bitrock.it>
2026-09-09 15:56:49 +0200

    add import for observability

--- src/platform/prod/observability/import.tf
+++ src/platform/prod/observability/import.tf
@@ -1,3 +1,23 @@
 # Use this file to import the wanted resources inside the state file, 
 # remember to cleanup the import code blocks with a separate PR once the import has been completed successfully.
 # Here is the documentation which explains how to use the import code block: https://developer.hashicorp.com/terraform/language/block/import
+
+import {
+  to = module.storage_accounts_westeurope.azurerm_storage_account.logs["0"]
+  id = "/subscriptions/ec285037-c673-4f58-b594-d7c480da4e8b/resourceGroups/io-p-rg-operations/providers/Microsoft.Storage/storageAccounts/iopstlogs"
+}
+
+import {
+  to = module.storage_accounts_italynorth.azurerm_storage_account.iopitnlogst01["0"]
+  id = "/subscriptions/ec285037-c673-4f58-b594-d7c480da4e8b/resourceGroups/io-p-rg-operations/providers/Microsoft.Storage/storageAccounts/iopitnlogst01"
+}
+
+import {
+  to = module.storage_accounts_italynorth.azurerm_storage_encryption_scope.iopitnlogst01["0"]
+  id = "/subscriptions/ec285037-c673-4f58-b594-d7c480da4e8b/resourceGroups/io-p-rg-operations/providers/Microsoft.Storage/storageAccounts/iopitnlogst01/encryptionScopes/logsencryption"
+}
+
+import {
+  to = module.storage_accounts_italynorth.azurerm_storage_management_policy.iopitnlogst01["0"]
+  id = "/subscriptions/ec285037-c673-4f58-b594-d7c480da4e8b/resourceGroups/io-p-rg-operations/providers/Microsoft.Storage/storageAccounts/iopitnlogst01/managementPolicies/default"
+}
