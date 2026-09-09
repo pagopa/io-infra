00e457f04a6ca5b2d9666b903ca8eeee0b42c302
Matteo Anselmi <matteo.anselmi@bitrock.it>
2026-09-09 17:17:06 +0200

    fix moved

--- src/core/prod/import.tf
+++ src/core/prod/import.tf
@@ -78,6 +78,6 @@ import {
 }
 
 moved {
-  from = module.storage_accounts_itn.azurerm_storage_account.terraform[0]
+  from = module.storage_accounts_itn.azurerm_storage_account.terraform
   to   = module.storage_accounts_itn.azurerm_storage_account.terraform_state_itn[0]
 }
