ccd5f15ec03b6a949ad988dc63406e5647e4a38f
Matteo Anselmi <matteo.anselmi@bitrock.it>
2026-09-11 10:37:35 +0200

    add outputs needed for assets cdn migration

--- src/platform/prod/observability/_modules/storage_accounts/outputs.tf
+++ src/platform/prod/observability/_modules/storage_accounts/outputs.tf
@@ -1,4 +1,16 @@
-output "logs_primary_connection_string" {
-  value     = try(azurerm_storage_account.logs[0].primary_connection_string, null)
-  sensitive = true
+output "logs" {
+  value = {
+    westeurope = {
+      id                        = try(azurerm_storage_account.logs[0].id, null)
+      name                      = try(azurerm_storage_account.logs[0].name, null)
+      primary_connection_string = try(azurerm_storage_account.logs[0].primary_connection_string, null)
+      sensitive                 = true
+    },
+    italynorth = {
+      id                        = try(azurerm_storage_account.iopitnlogst01[0].id, null)
+      name                      = try(azurerm_storage_account.iopitnlogst01[0].name, null)
+      primary_connection_string = try(azurerm_storage_account.iopitnlogst01[0].primary_connection_string, null)
+      sensitive                 = true
+    }
+  }
 }
