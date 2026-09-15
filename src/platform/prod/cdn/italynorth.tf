2783b75e34ebb0f71a13d01a813351e7ab03a633
Matteo Anselmi <matteo.anselmi@bitrock.it>
2026-09-15 14:04:38 +0200

    cleanup locals on cdn

--- src/platform/prod/cdn/italynorth.tf
+++ src/platform/prod/cdn/italynorth.tf
@@ -23,8 +23,8 @@ module "assets_locales" {
   resource_group_cdn      = local.core.resource_groups.italynorth.assets_cdn
   resource_group_external = local.core.resource_groups.westeurope.external
 
-  public_dns_zones           = local.platform_core.dns.zones.public_dns_zones
-  log_analytics_workspace_id = local.platform_observability.monitoring_italynorth.log.id
+  public_dns_zones                       = local.platform_core.dns.zones.public_dns_zones
+  log_analytics_workspace_id             = local.platform_observability.monitoring_italynorth.log.id
   diagnostic_settings_storage_account_id = local.platform_observability.storage_accounts.itn.logs.id
 
   azure_adgroups_roles = {
