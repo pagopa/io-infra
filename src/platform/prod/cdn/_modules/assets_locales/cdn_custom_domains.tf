3f33b2df9886df61809726ddf5c58987977e394d
Matteo Anselmi <matteo.anselmi@bitrock.it>
2026-09-15 12:00:47 +0200

    align custom domain dns zone id

--- src/platform/prod/cdn/_modules/assets_locales/cdn_custom_domains.tf
+++ src/platform/prod/cdn/_modules/assets_locales/cdn_custom_domains.tf
@@ -1,7 +1,6 @@
 resource "azurerm_cdn_frontdoor_custom_domain" "assets_cdn_io_italia_it" {
   name                     = "assets-cdn-io-italia-it"
   cdn_frontdoor_profile_id = azurerm_cdn_frontdoor_profile.assets_locales_frontdoor_profile.id
-  dns_zone_id              = var.public_dns_zones.io_italia_it.id
   host_name                = "assets.cdn.io.italia.it"
 
   tls {
@@ -12,7 +11,6 @@ resource "azurerm_cdn_frontdoor_custom_domain" "assets_cdn_io_italia_it" {
 resource "azurerm_cdn_frontdoor_custom_domain" "assets_cdn_io_pagopa_it" {
   name                     = "assets-cdn-io-pagopa-it"
   cdn_frontdoor_profile_id = azurerm_cdn_frontdoor_profile.assets_locales_frontdoor_profile.id
-  dns_zone_id              = var.public_dns_zones.io.id
   host_name                = "assets.cdn.io.pagopa.it"
 
   tls {
@@ -23,7 +21,6 @@ resource "azurerm_cdn_frontdoor_custom_domain" "assets_cdn_io_pagopa_it" {
 resource "azurerm_cdn_frontdoor_custom_domain" "assets_io_pagopa_it" {
   name                     = "assets-io-pagopa-it"
   cdn_frontdoor_profile_id = azurerm_cdn_frontdoor_profile.assets_locales_frontdoor_profile.id
-  dns_zone_id              = var.public_dns_zones.io.id
   host_name                = "assets.io.pagopa.it"
 
   tls {
