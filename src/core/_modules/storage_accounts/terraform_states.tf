fac0ddac2ecb35c22d64a8e7f0cbab9c21ec1850
Matteo Anselmi <matteo.anselmi@bitrock.it>
2026-09-22 13:09:49 +0200

    typo

--- src/core/_modules/storage_accounts/terraform_states.tf
+++ src/core/_modules/storage_accounts/terraform_states.tf
@@ -1,5 +1,5 @@
 # At the moment the storage account must have public access enabled due to many github runners not using private agents,
-# also a private endpoint for the storage at the moment is not implemented.
+# also a private endpoint for the storage is not implemented at the moment.
 
 #trivy:ignore:AZU-0012
 resource "azurerm_storage_account" "terraform_state_itn" {
